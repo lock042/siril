a5f3f5817a6088e91ca2f0af8ce442ce2757337b
Emmanuel Brandt <wizebt@gmail.com>
2019-04-11 14:29:33 +0000

    Update sorting.c: correct and std headers comments, use sortnet for small arrays, add max and twpqs functions to improve quickmedian perf

--- src/algos/sorting.c
+++ src/algos/sorting.c
@@ -1,7 +1,3 @@
-#include "sorting.h"
-#include <string.h>
-#include <math.h>
-
 /*
  * This file is part of Siril, an astronomy image processor.
  * Copyright (C) 2005-2011 Francois Meyer (dulle at free.fr)
@@ -20,24 +16,28 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with Siril. If not, see <http://www.gnu.org/licenses/>.
- */
-
-/* This files contains all sorting algorithms of siril.
+ *
+ * This files contains all sorting algorithms for siril.
  * See src/test/sorting.c for testing and metrics.
  */
+ 
+#include "sorting.h"
+#include <string.h>
+#include <math.h>
 
-/**
- * In-place quick sort of array of double a of size n
+/*
+ * In-place quick sort of array of double of size n
  * @param a array to sort
- * @param n size of the array
+ * @param n array size
  */
-void quicksort_d (double *a, int n) {
-	if (n < 2)
-		return;
+void quicksort_d (double *a, int n) 
+{
+	if (n < 2) return;
+	
 	double pivot = a[n / 2];
 	double *left = a;
 	double *right = a + n - 1;
-	register double t;
+	register double t; 	// temporary value used for swap
 
 	while (left <= right) {
 		if (*left < pivot) {
@@ -56,14 +56,15 @@ void quicksort_d (double *a, int n) {
 	quicksort_d(left, a + n - left);
 }
 
-/**
- * In-place quick sort of array of WORD a of size n
- * @param a array to sort
- * @param n size of the array
+/*
+ * In-place quick sort of array of WORD of size n
+ * @param a array of WORD
+ * @param n array size
  */
-void quicksort_s (WORD *a, int n) {
-	if (n < 2)
-		return;
+void quicksort_s (WORD *a, int n) 
+{
+	if (n < 2) return;
+	
 	WORD pivot = a[n / 2];
 	WORD *left = a;
 	WORD *right = a + n - 1;
@@ -86,25 +87,25 @@ void quicksort_s (WORD *a, int n) {
 	quicksort_s(left, a + n - left);
 }
 
-/* quickmedian returns the median from array of length n
- * Derived from the original quickselect algorithm from Hoare
- * warning: data are sorted in place
- * non recurssive version modified to return median value
- * @param a array of WORD to search
- * @param n size of the array
+/* quickmedian returns the median of an array of WORD of size n
+ * Non recursive version of the original quickselect algorithm from
+ * Tony Hoare modified to return the median value
+ * Warning: values are shuffled in place
+ * Written by Emmanuel Brandt 2019-02
+ * @param a array of WORD
+ * @param n array size
  * @return median as double for even size average the middle two elements
 */
-double quickmedian (WORD *a, int n) {
+double quickmedian (WORD *a, int n) 
+{	// Use fast and robust sorting network for small size array
+	if (n < 10) return sortnet_median (a, n);
+	
 	int i;
 	int k = n / 2;		// size to sort
-	int pindex;		// pivot index
+	int pindex;			// pivot index
 	int left = 0; 		// left index
 	int right = n - 1; 	// right index
 	WORD pivot, tmp;
-
-	// Use faster and robust sorting network for small size array
-	if (n < 9)
-		return sortnet_median(a, n);
 
 	while (left < right) { //we stop when our indicies have crossed
 		pindex = (left + right) / 2; // pivot selection, this can be whatever
@@ -125,26 +126,27 @@ double quickmedian (WORD *a, int n) {
 
 		if (pindex < k)
 			left = pindex + 1;
-		else
-			// pindex >= k
+		else // pindex >= k
 			right = pindex;
 	}
-	return (n % 2 == 0) ?
-			((double) a[k - 1] + (double) a[k]) / 2.0 : (double) a[k];
+	return (n % 2 == 0) ? ((double) a[k - 1] + (double) a[k]) / 2.0 : (double) a[k];
 }
 
-/* quickmedian_double returns the median from array of length n
- * Derived from the original quickselect algorithm from Hoare
- * warning: data are sorted in place
- * non recurssive version modified to return median value
- * @param a array of double to search
- * @param n size of the array
+/* 
+ * quickmedian_double returns the median of an array of double of size n
+ * Non recursive version of the original quickselect algorithm from
+ * Tony Hoare modified to return the median value
+ * Warning: values are shuffled in place
+ * Written by Emmanuel Brandt 2019-02
+ * @param a array of double 
+ * @param n array size 
  * @return median as double for even size average the middle two elements
  */
-double quickmedian_double (double *a, int n) {
+double quickmedian_double (double *a, int n) 
+{
 	int i;
 	int k = n / 2;		// size to sort
-	int pindex;		// pivot index
+	int pindex;			// pivot index
 	int left = 0; 		// left index
 	int right = n - 1; 	// right index
 	double pivot, tmp;
@@ -168,24 +170,25 @@ double quickmedian_double (double *a, int n) {
 
 		if (pindex < k)
 			left = pindex + 1;
-		else
-			// pindex >= k
+		else // pindex >= k
 			right = pindex;
 	}
 	return (n % 2 == 0) ? (a[k - 1] + a[k]) / 2 : a[k];
 }
 
 /*
- * quickmedian_int returns the median from array of int of of length n
- * Derived from original quickselect algorithm from Hoare
- * @param a array to search
- * @param n size of the array
- * @return median as double
+ * quickmedian_int returns the median from an array of int of size n
+ * Derived from original quickselect algorithm from Tony Hoare
+ * non recurssive version modified to return the median value
+ * Written by Emmanuel Brandt 2019-02
+ * @param a array of int
+ * @param n array size
+ * @return median value as double
  */
 double quickmedian_int (int *a, int n) {
 	int i;
 	int k = n / 2;		// size to sort
-	int pindex;		// pivot index
+	int pindex;			// pivot index
 	int left = 0; 		// left index
 	int right = n - 1; 	// right index
 	int pivot, tmp;
@@ -209,8 +212,7 @@ double quickmedian_int (int *a, int n) {
 
 		if (pindex < k)
 			left = pindex + 1;
-		else
-			// pindex >= k
+		else // pindex >= k
 			right = pindex;
 	}
 	return (n % 2 == 0) ?
@@ -218,14 +220,16 @@ double quickmedian_int (int *a, int n) {
 }
 
 /*
- * Optimal sorting network array of size [2,9] to retrieve median value
- * (C) Emmanuel Brandt 2019-02
- * @param a array of double to sort (warning in place sorting)
- * @param n size of the array to sort [2,9]
- * warning in-place sorting
+ * sortnet_median in-place sort an array of size [2,9] using fast and robust 
+ * sorting network and returns the median as double
+ * Written by Emmanuel Brandt 2019-02
+ * @param a array of WORD
+ * @param n array size in range [2,9]
+ * @return median value as double
  */
 #define sw(i,j) if(a[i] > a[j]) { register WORD t=a[i]; a[i]=a[j]; a[j]=t; }
-double sortnet_median (WORD *a, int n) {
+double sortnet_median (WORD *a, int n) 
+{
 	int k = n / 2;
 
 	switch (n) {
@@ -269,20 +273,21 @@ double sortnet_median (WORD *a, int n) {
 
 		default: return 0.0; break; // no sort
 	}
-	return (n % 2 == 0) ? (a[k - 1] + a[k]) / 2.0 : a[k];
+	return (n % 2 == 0) ?
+			((double) a[k - 1] + (double) a[k]) / 2.0 : (double) a[k];
 }
 #undef sw
 
 /*
- * Optimal sorting network [2,9]
- * (C) Emmanuel Brandt 2019-02
- * @param a array of double to sort (warning in place sorting)
- * @param n size of the array to sort [2,9]
- * warning in-place sorting
+ * sortnet in-place sort of an array of WORD of size [2,9] using 
+ * fast and robust sorting network.
+ * Written by Emmanuel Brandt 2019-02
+ * @param a array of WORD to sort
+ * @param n array size in range [2,9]
  */
 #define sw(i,j) if(a[i] > a[j]) { register WORD t=a[i]; a[i]=a[j]; a[j]=t; }
-void sortnet (WORD *a, int n) {
-
+void sortnet (WORD *a, int n) 
+{
 	switch (n) {
 		case 2: sw(0,1); break;
 
@@ -326,17 +331,17 @@ void sortnet (WORD *a, int n) {
 #undef sw
 
 /*
- * Histogram median for very large array of unsigned short
- * (C) Emmanuel Brandt 2019-02
+ * histogram_median returns the median of large array of unsigned short
+ * Use temp storage for histogram
+ * Written by Emmanuel Brandt 2019-02
  * @param a array of unsigned short to search
- * @param n size of the array
- * @return median as a double (for n odd)
- * Use temp storage h to build the histogram. Complexity O(2*N)
+ * @param n array size
+ * @return median as a double
  */
-double histogram_median(WORD *a, int n) {
-	// For arrays n < 10 histogram is use fast and simple sortnet_median
-	if (n < 10)
-		return sortnet_median(a, n);
+double histogram_median (WORD *a, int n) 
+{
+	// For arrays size n < 10 use sortnet_median
+	if (n < 10) return sortnet_median(a, n);
 
 	unsigned int i, j, k = n / 2;
 	size_t s = sizeof(unsigned int);
@@ -360,15 +365,17 @@ double histogram_median(WORD *a, int n) {
 }
 
 /*
- * Histogram median for very large array of double in [0,1] range
- * (C) Emmanuel Brandt 2019-02
- * @param a array of unsigned short to search
- * @param n size of the array
- * @return median as a double (for n odd)
- * Use temp storage h for the histogram. Complexity O(2*N)
- * faster than quickmedian_double for array > 20000
+ * histogram_median_double return median of an array of double of size n
+ * Developp for large array of values in range [0,1] 
+ * Use temp storage for the histogram
+ * It is faster than quickmedian_double for array > 20000
+ * Written by Emmanuel Brandt 2019-02
+ * @param a array of double
+ * @param n array size
+ * @return median as a double
  */
-double histogram_median_double(double *a, int n) {
+double histogram_median_double (double *a, int n) 
+{
 	unsigned int i, j, k = n / 2, nb_bins = 100000;
 	size_t s = sizeof(unsigned int);
 	unsigned int *h = (unsigned int *) calloc(nb_bins + 1, s);
@@ -390,13 +397,86 @@ double histogram_median_double(double *a, int n) {
 	return (n % 2 == 0) ? (double) (i + j) / ( 2.0 * (double) nb_bins) : (double) i / (double) nb_bins;
 }
 
+/*
+ * maximum returns the maximum value of an array of WORD of size n
+ * Written by Emmanuel Brandt 2019-03
+ * @param a array of WORD
+ * @param n array size
+ * @return the maximum value as WORD
+ */
+WORD maximum (WORD *a, int n) 
+{
+	WORD m = a[0];
+	
+	for (int i=1; i<n; i++) 
+		if (a[i]>m)
+			m=a[i];
+	
+	return m;
+}
+
+/*
+ * Three Way Partition Quick Select returns the median of an array of WORD of size n
+ * Combines the three way partition algorithm or "Dutch Flag" from 
+ * Edsger Dijkstra and the Quick Select from Tony Hoare.
+ * Splits the array in 3 partitions: < = > of pivot and iterate on partition 
+ * containing the median. Outperforms QuickSelect algorithm for arrays with
+ * duplicates or pre sorted
+ * Warning: in place value shuffling
+ * Written by Emmanuel Brandt 2019-04
+ * @param a array of WORD
+ * @param n array size
+ * @return median as double
+ */
+double twpqs (WORD *a, int n) 
+{
+	// For arrays size n < 10 use sortnet_median
+	if (n < 10) return sortnet_median(a, n);
+
+	int left, l, j, right, r, k;
+	WORD p, tmp;
+	left=l=j=0;
+	right=r=n-1;
+	k=n/2; // pivot index
+	p=a[k]; // pivot value
+	
+	while (left<=right) {
+		while (j<=r) { // 3 way partition < = >
+			if (a[j]<p) {
+				tmp=a[j]; a[j]=a[l]; a[l]=tmp; // swap a[j] a[l]
+				l+=1;
+				j+=1;
+			} else if (a[j]>p) {
+				tmp=a[j]; a[j]=a[r]; a[r]=tmp; // swap a[j] a[r]
+				r-=1;
+			} else 
+				j+=1;
+		}
+
+		if (k>r) {
+			left=l=j=r+1;
+			r=right;
+		} else if (k<l) {
+			r=right=l-1;
+			l=j=left;	
+		} else {
+			if (n%2==0 && k<=l)
+				return ((double) p + maximum (a,k)) / 2.0;
+			else
+				return (double) p;
+		}
+		p=a[(l+r)/2];
+	}
+}
+
 /**
  * Compares a and b like strcmp()
  * @param a a gconstpointer
  * @param b a gconstpointer
  * @return an integer less than, equal to, or greater than zero, if a is than b .
  */
-gint strcompare(gconstpointer *a, gconstpointer *b) {
+gint strcompare (gconstpointer *a, gconstpointer *b) 
+{
 	gchar *collate_key1, *collate_key2;
 	gint result;
 
