bbfdc05261a686653f48ac50e11f1556be49ab75
Vincent Hourdin <vh@free-astro.org>
2019-07-12 02:04:11 +0200

    improving the zone_compare tool, not yet working for the display of the difference after zone selection

--- src/tests/zone_compare.c
+++ src/tests/zone_compare.c
@@ -4,6 +4,8 @@
 #include "../core/proto.h"
 #include "../io/sequence.h"
 #include "../io/single_image.h"
+#include "../planetary/planetary.h"
+#include "../gui/zones.h"
 
 #define NB_DISPLAYS 3
 
@@ -21,11 +23,13 @@ cairo_surface_t *surface[NB_DISPLAYS];
 guchar *graybuf[NB_DISPLAYS];	// R=G=B 8bit version
 int surface_stride[NB_DISPLAYS];
 int changed = 1;
-double zoom = 1.0;
+double zoom[NB_DISPLAYS] = { 1.0 };
 stacking_zone zone = { .centre = {.x = -1.0 } };
 
+#define LAYER 0
 
 void remap(int image);
+void update_comparison();
 
 int main(int argc, char **argv) {
 	if (argc < 3) {
@@ -53,7 +57,6 @@ int main(int argc, char **argv) {
 	remap(0);
 	changed = 1;
 
-
 	if (!(seq = readseqfile(argv[2])))
 		exit(1);
 	if (seq_read_frame(seq, 0, &fit[1]))
@@ -77,6 +80,7 @@ int main(int argc, char **argv) {
 void remap(int image) {
 	if (changed) {
 		fprintf(stdout, "remap %d\n", image);
+		if (image > 2) return;
 		surface_stride[image] = cairo_format_stride_for_width(
 				CAIRO_FORMAT_RGB24, fit[image].rx);
 		graybuf[image] = realloc(graybuf[image],
@@ -84,7 +88,7 @@ void remap(int image) {
 		if (surface[image])
 			cairo_surface_destroy(surface[image]);
 		surface[image] = cairo_image_surface_create_for_data(graybuf[image],
-				CAIRO_FORMAT_RGB24, fit->rx, fit->ry,
+				CAIRO_FORMAT_RGB24, fit[image].rx, fit[image].ry,
 				surface_stride[image]);
 		if (cairo_surface_status(surface[image])
 				!= CAIRO_STATUS_SUCCESS) {
@@ -135,26 +139,27 @@ gboolean redraw_drawingarea(GtkWidget *widget, cairo_t *cr, gpointer data) {
 	int window_height = gtk_widget_get_allocated_height(widget);
 
 	int image = image_for_widget(widget);
-	if (image < 0 || image >= 2) return TRUE;
-	remap(image);
+	if (image < 0 || image > 2) return TRUE;
+	remap(image); // does nothing if !changed
 
 	// autofit zoom
 	double wtmp = (double)window_width / (double)fit[image].rx;
 	double htmp = (double)window_height / (double)fit[image].ry;
-	zoom = min(wtmp, htmp);
-	cairo_scale(cr, zoom, zoom);
+	zoom[image] = min(wtmp, htmp);
+	cairo_scale(cr, zoom[image], zoom[image]);
 
 	cairo_set_source_surface(cr, surface[image], 0, 0);
 	cairo_paint(cr);
 
 	if (image == 0 && zone.centre.x >= 0.0) {
+		double side = (double)get_side(&zone);
 		// draw the zone
 		cairo_set_dash(cr, NULL, 0, 0);
 		cairo_set_line_width(cr, 1.5);
 		cairo_set_source_rgba(cr, 0.0, 1.0, 1.0, 1.0);	// cyan
 		cairo_rectangle(cr, zone.centre.x - zone.half_side,
 				zone.centre.y - zone.half_side,
-				2.0 * zone.half_side, 2.0 * zone.half_side);
+				side, side);
 		cairo_stroke(cr);
 
 		// draw points at the centre
@@ -173,22 +178,81 @@ gboolean redraw_drawingarea(GtkWidget *widget, cairo_t *cr, gpointer data) {
 void on_scale_seqnumber_value_changed(GtkRange *range, gpointer user_data) {
 	int image = round_to_int(gtk_range_get_value(range));		
 	clearfits(&fit[1]);
-	if (seq_read_frame(seq, image, &fit[1]))
+	if (seq_read_frame(seq, image, &fit[1])) {
 		fprintf(stderr, "could not open image %d from sequence\n", image);
+		return;
+	}
+	seq->number = image;
 	image_find_minmax(&fit[1]);
 	changed = 1;
 	remap(1);
 	gtk_widget_queue_draw(widget_for_image(1));
+	update_comparison();
 }
 
 gboolean on_drawingarea_button_press_event(GtkWidget *widget,
 		GdkEventButton *event, gpointer user_data) {
-	zone.centre.x = event->x / zoom;
-	zone.centre.y = event->y / zoom;
+	int image = image_for_widget(widget);
+	zone.centre.x = event->x / zoom[image];
+	zone.centre.y = event->y / zoom[image];
 	GtkRange *range = GTK_RANGE(gtk_builder_get_object(builder, "scale_zonesize"));
-	zone.half_side = round_to_int(gtk_range_get_value(range));
+	zone.half_side = round_to_int(gtk_range_get_value(range)) / 2;
 	zone.mpregparam = NULL;
-	gtk_widget_queue_draw(widget);
+	gtk_widget_queue_draw(widget);	// display the selection
+	fprintf(stdout, "zone at %d, %d, half_side: %d\n", zone.centre.x, zone.centre.y, zone.half_side);
+
+	update_comparison();
 	return FALSE;
 }
 
+void update_comparison() {
+	if (zone.half_side <= 0)
+		return;
+
+	int i, side = get_side(&zone), nb_pix = side * side;
+
+	WORD *ref, *im, *diff;
+	// get the area from the ref image
+	ref = malloc(nb_pix * sizeof(WORD));
+	copy_image_zone_to_buffer(&fit[0], &zone, ref, LAYER);
+	
+	// get the area from the sequence image, with regdata
+	im = malloc(nb_pix * sizeof(WORD));
+	regdata *regparam = seq->regparam[0];
+	if (regparam) {
+		stacking_zone shifted_zone = { .centre =
+			{ .x = round_to_int(zone.centre.x - regparam[seq->number].shiftx),
+				.y = round_to_int(zone.centre.y + regparam[seq->number].shifty) },
+			.half_side = zone.half_side };
+		copy_image_zone_to_buffer(&fit[1], &shifted_zone, im, LAYER);
+	}
+	else copy_image_zone_to_buffer(&fit[1], &zone, im, LAYER);
+	// FIXME ^ apparently it's not working ^
+
+	// compute the displayed patch
+	diff = malloc(nb_pix * sizeof(WORD));
+	WORD mini = 65535, maxi = 0;
+	for (i = 0; i < nb_pix; i++) {
+		// use the abs(ref_i - im_i)
+		if (ref[i] < im[i])
+			diff[i] = im[i] - ref[i];
+		else diff[i] = ref[i] - im[i];
+		if (diff[i] < mini)
+			mini = diff[i];
+		if (diff[i] > maxi)
+			maxi = diff[i];
+	}
+	free(im);
+	free(ref);
+
+	if (fit[2].rx > 0)
+		clearfits(&fit[2]);
+	fits *newfit = &fit[2];
+	new_fit_image(&newfit, side, side, 1, diff);
+	newfit->maxi = maxi;
+	newfit->mini = mini;
+
+	changed = 1;
+	remap(2);
+	gtk_widget_queue_draw(widget_for_image(2));
+}
