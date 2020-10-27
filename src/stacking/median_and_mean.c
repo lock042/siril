ef2ecde14b0a8b2d77ae09ed722e078e484e9363
Cyril Richard <cyril.richard42@gmail.com>
2020-10-27 15:05:21 +0100

    Try to debug overflow bug

--- src/stacking/median_and_mean.c
+++ src/stacking/median_and_mean.c
@@ -990,12 +990,12 @@ static int stack_mean_or_median(struct stacking_args *args, gboolean is_mean) {
 
 	set_progress_bar_data(_("Finalizing stacking..."), (double)cur_nb/total);
 	if (is_mean) {
-		double nb_tot = (double)(naxes[0] * naxes[1] * nb_frames);
-		long channel;
-		for (channel = 0; channel < naxes[2]; channel++) {
+		double nb_tot = (double) naxes[0] * (double) naxes[1] * (double) nb_frames;
+		for (long channel = 0; channel < naxes[2]; channel++) {
+			siril_log_message("debug: nb_tot=%lf and irej[channel][0]=%ld and irej[channel][1]=%ld\n", nb_tot, irej[channel][0], irej[channel][1]);
 			siril_log_message(_("Pixel rejection in channel #%d: %.3lf%% - %.3lf%%\n"),
-					channel, irej[channel][0] / nb_tot * 100.0,
-					irej[channel][1] / nb_tot * 100.0);
+					channel, (double) irej[channel][0] / nb_tot * 100.0,
+					(double) irej[channel][1] / nb_tot * 100.0);
 		}
 	}
 
