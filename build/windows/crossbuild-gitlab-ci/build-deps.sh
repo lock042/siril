f7da1d5f0c17f6a620103d77f13afe5dfb1ceda1
Cyril Richard <cyril.richard42@gmail.com>
2020-12-08 16:15:10 +0100

    zlib must not be needed anymore

--- build/windows/crossbuild-gitlab-ci/build-deps.sh
+++ build/windows/crossbuild-gitlab-ci/build-deps.sh
@@ -9,15 +9,10 @@ crossroad install fftw \
                   opencv \
                   libheif \
                   ffms2 \
-                  zlib \
                   cfitsio
 
-# Official tarball does not compile on Windows. We take the KSTARS fork
-#wget https://indilib.org/jdownloads/wcslib/wcslib-7.3.1.tar.gz
 wget ftp://ftp.atnf.csiro.au/pub/software/wcslib/wcslib.tar.bz2
 tar xfv wcslib.tar.bz2 && cd wcslib-7.3.1
-#mkdir build && cd build 
-#crossroad cmake .. -DCMAKE_BUILD_TYPE=Release
 patch -p1 -i ../../build/windows/crossbuild-gitlab-ci/wcslib.patch
 crossroad configure LIBS=\"-pthread -lcurl -lm\" --without-pgplot --disable-fortran
 make
