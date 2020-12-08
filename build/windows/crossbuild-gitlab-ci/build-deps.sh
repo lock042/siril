705ce3bb4b417a7f07119ab05c67f0cb3fb96f95
Cyril Richard <cyril.richard42@gmail.com>
2020-12-08 15:30:03 +0100

    Try to fix build.... gggrrrrrrrrr

--- build/windows/crossbuild-gitlab-ci/build-deps.sh
+++ build/windows/crossbuild-gitlab-ci/build-deps.sh
@@ -18,7 +18,7 @@ wget ftp://ftp.atnf.csiro.au/pub/software/wcslib/wcslib.tar.bz2
 tar xfv wcslib.tar.bz2 && cd wcslib-7.3.1
 #mkdir build && cd build 
 #crossroad cmake .. -DCMAKE_BUILD_TYPE=Release
-patch -p1 -i ../build/windows/crossbuild-gitlab-ci/wcslib.patch
+patch -p1 -i ../../build/windows/crossbuild-gitlab-ci/wcslib.patch
 crossroad configure LIBS=\"-pthread -lcurl -lm\" --without-pgplot --disable-fortran
 make
 make install || exit 1
