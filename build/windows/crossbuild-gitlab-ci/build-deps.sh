6175e1bda1c3709e4bf2697fc768af29c0682bd9
Cyril Richard <cyril.richard42@gmail.com>
2020-12-08 12:44:57 +0100

    Trying to help Florian with wcslib windows integration

--- build/windows/crossbuild-gitlab-ci/build-deps.sh
+++ build/windows/crossbuild-gitlab-ci/build-deps.sh
@@ -1,22 +1,11 @@
 crossroad source msys2
+mkdir _deps && cd _deps
 
-crossroad install fftw exiv2 gtk3 libconfig gsl opencv libheif ffms2 zlib
+crossroad install fftw exiv2 gtk3 libconfig gsl opencv libheif ffms2 zlib cfitsio
 
-wget http://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio-3.48.tar.gz
-tar xfv cfitsio-3.48.tar.gz
-mkdir cfitsio-3.48/build 
-cd cfitsio-3.48
-patch -p1 -i ../build/windows/crossbuild-gitlab-ci/cfitsio-cmake.patch 
-cd build 
-crossroad cmake .. -DUSE_PTHREADS=ON -DCMAKE_BUILD_TYPE=Release
-make
-make install
-
-cd
 wget https://indilib.org/jdownloads/wcslib/wcslib-7.3.1.tar.gz
 tar xfv wcslib-7.3.1.tar.gz && cd wcslib-7.3.1
 mkdir build && cd build 
 crossroad cmake .. -DCMAKE_BUILD_TYPE=Release
 make
-make install
-
+make install || exit 1
