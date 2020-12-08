acee2c1efd7f7053df87854e7e2c4827ae4b91f8
Cyril Richard <cyril.richard42@gmail.com>
2020-12-08 14:41:18 +0100

    Trying to patch upstream wcslib

--- build/windows/crossbuild-gitlab-ci/build-deps.sh
+++ build/windows/crossbuild-gitlab-ci/build-deps.sh
@@ -1,11 +1,24 @@
 crossroad source msys2
 mkdir _deps && cd _deps
 
-crossroad install fftw exiv2 gtk3 libconfig gsl opencv libheif ffms2 zlib cfitsio
+crossroad install fftw \
+                  exiv2 \
+                  gtk3 \
+                  libconfig \
+                  gsl \
+                  opencv \
+                  libheif \
+                  ffms2 \
+                  zlib \
+                  cfitsio
 
-wget https://indilib.org/jdownloads/wcslib/wcslib-7.3.1.tar.gz
+# Official tarball does not compile on Windows. We take the KSTARS fork
+#wget https://indilib.org/jdownloads/wcslib/wcslib-7.3.1.tar.gz
+wget ftp://ftp.atnf.csiro.au/pub/software/wcslib/wcslib.tar.bz2
 tar xfv wcslib-7.3.1.tar.gz && cd wcslib-7.3.1
-mkdir build && cd build 
-crossroad cmake .. -DCMAKE_BUILD_TYPE=Release
+#mkdir build && cd build 
+#crossroad cmake .. -DCMAKE_BUILD_TYPE=Release
+patch -p1 -i ../build/windows/crossbuild-gitlab-ci/wcslib.patch
+crossroad configure LIBS=\"-pthread -lcurl -lm\" --without-pgplot --disable-fortran
 make
 make install || exit 1
