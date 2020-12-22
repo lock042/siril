6fe1de3fd7b16fc2b162235f57d7b0c6dd1d7847
Cyril Richard <cyril.richard42@gmail.com>
2020-12-22 13:42:56 +0100

    Try my_winfile

---
+++ src/io/my_winfile.h
@@ -0,0 +1,49 @@
+/*
+ * This file is part of Siril, an astronomy image processor.
+ * Copyright (C) 2005-2011 Francois Meyer (dulle at free.fr)
+ * Copyright (C) 2012-2020 team free-astro (see more in AUTHORS file)
+ * Reference site is https://free-astro.org/index.php/Siril
+ *
+ * Siril is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Siril is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Siril. If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef SRC_IO_MY_WINFILE_H_
+#define SRC_IO_MY_WINFILE_H_
+
+#ifdef _WIN32
+#include <sys/stat.h>
+/* my_winfile.c exports, should not be used outside mysys */
+extern File     my_win_open(const char *path, int oflag);
+extern int      my_win_close(File fd);
+extern size_t   my_win_read(File fd, uchar *buffer, size_t  count);
+extern size_t   my_win_write(File fd, const uchar *buffer, size_t count);
+extern size_t   my_win_pread(File fd, uchar *buffer, size_t count,
+                             my_off_t offset);
+extern size_t   my_win_pwrite(File fd, const uchar *buffer, size_t count,
+                              my_off_t offset);
+extern my_off_t my_win_lseek(File fd, my_off_t pos, int whence);
+extern int      my_win_chsize(File fd,  my_off_t newlength);
+extern FILE*    my_win_fopen(const char *filename, const char *type);
+extern File     my_win_fclose(FILE *file);
+extern File     my_win_fileno(FILE *file);
+extern FILE*    my_win_fdopen(File Filedes, const char *type);
+extern int      my_win_stat(const char *path, struct _stati64 *buf);
+extern int      my_win_fstat(File fd, struct _stati64 *buf);
+extern int      my_win_fsync(File fd);
+extern File     my_win_dup(File fd);
+extern File     my_win_sopen(const char *path, int oflag, int shflag, int perm);
+extern File     my_open_osfhandle(HANDLE handle, int oflag);
+#endif
+
+
+#endif /* SRC_IO_MY_WINFILE_H_ */
