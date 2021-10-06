58b47f7fbdeedf3f7bf10d1023ee7e4c163dca7d
Cyril Richard <cyril.richard42@gmail.com>
2021-10-06 15:59:46 +0200

    Test macOS CI

---
+++ build/macOS/120-jhbuild_install.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+#
+# SPDX-FileCopyrightText: 2021 René de Hesselle <dehesselle@web.de>
+#
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+### description ################################################################
+
+# Setup JHBuild.
+
+### includes ###################################################################
+
+# shellcheck disable=SC1090 # can't point to a single source here
+for script in "$(dirname "${BASH_SOURCE[0]}")"/0??-*.sh; do
+  source "$script";
+done
+
+### settings ###################################################################
+
+error_trace_enable
+
+### main #######################################################################
+
+jhbuild_install
+jhbuild_configure
