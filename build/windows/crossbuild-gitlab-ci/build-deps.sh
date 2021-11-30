62b77d949a89887851504e1d72edf3922fb314cf
Cyril Richard <cyril.richard42@gmail.com>
2021-11-30 10:46:24 +0100

    Handle error in win64 dep build

--- build/windows/crossbuild-gitlab-ci/build-deps.sh
+++ build/windows/crossbuild-gitlab-ci/build-deps.sh
@@ -11,12 +11,17 @@ crossroad install fftw \
                   libheif \
                   ffms2 \
                   cfitsio
+                  
+if [ $? -ne 0 ]; then
+  echo "Installation of pre-built dependencies failed.";
+  exit 1;
+fi
 
 # Build LibRaw from github
 crossroad install lcms2
 git clone --depth 1 https://github.com/LibRaw/LibRaw.git
 cd LibRaw
-autoreconf -fi
-crossroad ./configure --disable-examples --disable-static
-make install
+autoreconf -fi && \
+crossroad ./configure --disable-examples --disable-static && \
+make install || exit 1
 cd ..
