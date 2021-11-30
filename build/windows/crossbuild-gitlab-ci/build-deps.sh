583418474ce47b03b2db15ef18e3301bee8f42c9
Cyril Richard <cyril.richard42@gmail.com>
2021-11-30 22:02:41 +0100

    This CI makes me crazy

--- build/windows/crossbuild-gitlab-ci/build-deps.sh
+++ build/windows/crossbuild-gitlab-ci/build-deps.sh
@@ -1,6 +1,15 @@
 crossroad source msys2
 mkdir _deps && cd _deps
 
+# Build LibRaw from github
+crossroad install lcms2
+git clone --depth 1 https://github.com/LibRaw/LibRaw.git
+cd LibRaw
+autoreconf -fi && \
+crossroad ./configure --disable-examples --disable-static && \
+make install || exit 1
+cd ..
+
 # Install deps from crossroad
 crossroad install fftw \
                   exiv2 \
@@ -16,12 +25,3 @@ if [ $? -ne 0 ]; then
   echo "Installation of pre-built dependencies failed.";
   exit 1;
 fi
-
-# Build LibRaw from github
-crossroad install lcms2 sed
-git clone --depth 1 https://github.com/LibRaw/LibRaw.git
-cd LibRaw
-autoreconf -fi && \
-crossroad ./configure --disable-examples --disable-static && \
-make install || exit 1
-cd ..
