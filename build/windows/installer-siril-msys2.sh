6997d80cc56b0d83308ef1f7b063dca7e138ffc5
Cyril Richard <cyril.richard42@gmail.com>
2021-12-04 23:43:16 +0100

    Changing the way we get version

--- build/windows/installer-siril-msys2.sh
+++ build/windows/installer-siril-msys2.sh
@@ -3,9 +3,9 @@
 #./is.exe //SILENT //SUPPRESSMSGBOXES //CURRENTUSER //SP- //LOG="innosetup.log"
 
 # Construct now the installer.
-MAJOR_VERSION=`grep 'm4_define(\[siril_major_version' configure.ac |sed 's/m4_define(\[siril_major_version.*\[\([0-9]*\)\].*/\1/'`
-MINOR_VERSION=`grep 'm4_define(\[siril_minor_version' configure.ac |sed 's/m4_define(\[siril_minor_version.*\[\([0-9]*\)\].*/\1/'`
-MICRO_VERSION=`grep 'm4_define(\[siril_micro_version' configure.ac |sed 's/m4_define(\[siril_micro_version.*\[\([0-9]*\)\].*/\1/'`
+MAJOR_VERSION=`grep "version : " meson.build | head -n 1  | awk '{ print $3 }' | tr -d "'," | cut -d . -f 1`
+MINOR_VERSION=`grep "version : " meson.build | head -n 1  | awk '{ print $3 }' | tr -d "'," | cut -d . -f 2`
+MICRO_VERSION=`grep "version : " meson.build | head -n 1  | awk '{ print $3 }' | tr -d "'," | cut -d . -f 3`
 cd build/windows/installer
 ./compile.bat ${MAJOR_VERSION}.${MINOR_VERSION}.${MICRO_VERSION} ../../.. siril-w64 ../../.. siril-w64
 
