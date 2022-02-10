836cce317e0d34c20df268964fd085899715e85a
Cyril Richard <cyril.richard42@gmail.com>
2022-02-10 09:32:06 +0100

    Trying to improve speed of webm export

--- src/io/mp4_output.c
+++ src/io/mp4_output.c
@@ -46,7 +46,7 @@
 
 #define SCALE_FLAGS SWS_BICUBIC
 
-static double vp9_quality_to_crf[]  = { 36.0, 34.0, 31.0, 27.0, 22.0 };
+static double vp9_quality_to_crf[]  = { 32.0, 29.0, 26.0, 20.0, 17.0 };
 static double x264_quality_to_crf[] = { 29.0, 26.0, 23.0, 20.0, 17.0 };
 static double x265_quality_to_crf[] = { 34.0, 31.0, 28.0, 25.0, 22.0 };
 
@@ -108,26 +108,29 @@ static int add_stream(struct mp4_struct *ost, AVCodec **codec,
 	}
 	ost->enc = c;
 
+	c->thread_count = com.max_thread;
+
 	c->codec_id = codec_id;
 	int retval;
-	double crf;
+	int crf;
 	switch (codec_id) {
 		case AV_CODEC_ID_VP9:
 			c->bit_rate = 0;
-			/* for this codec, quality depends on image size (like bit rate) */
-			float size_factor = 5.0f * logf(w * h / (1920 * 1080));
-			if (size_factor < -4.0f) size_factor = -4.0f;
-			crf = vp9_quality_to_crf[ost->quality-1] - roundf_to_int(size_factor);
-			siril_debug_print("VP9 constant quality value: %lf\n", crf);
-			retval = av_opt_set_double(c->priv_data, "crf", crf, 0); // For integer values
+			crf = vp9_quality_to_crf[ost->quality - 1];
+			siril_debug_print("VP9 constant quality value: %d\n", crf);
+			retval = av_opt_set_int(c->priv_data, "crf", crf, 0); // For integer values
+			CHECK_OPT_SET_RETVAL;
+			retval = av_opt_set_int(c->priv_data, "speed", 1, 0);
+			CHECK_OPT_SET_RETVAL;
+			retval = av_opt_set_int(c->priv_data, "frame-parallel", 1, 0);
 			CHECK_OPT_SET_RETVAL;
 			break;
 		case AV_CODEC_ID_H264:
 			/* The range of the CRF scale is 0–51, where 0 is lossless, 23 is the
 			 * default, and 51 is worst quality possible. A subjectively sane range
 			 * is 17–28. Consider 17 or 18 to be visually lossless or nearly so. */
-			crf = x264_quality_to_crf[ost->quality-1];
-			siril_debug_print("x264 constant quality value: %lf\n", crf);
+			crf = x264_quality_to_crf[ost->quality - 1];
+			siril_debug_print("x264 constant quality value: %d\n", crf);
 			retval = av_opt_set_int(c->priv_data, "crf", crf, 0); // For integer values
 			CHECK_OPT_SET_RETVAL;
 			retval = av_opt_set(c->priv_data, "preset", "fast", 0);
@@ -137,8 +140,8 @@ static int add_stream(struct mp4_struct *ost, AVCodec **codec,
 			break;
 		case AV_CODEC_ID_H265:
 			// default is 28, it should visually correspond to libx264 video at CRF 23
-			crf = x265_quality_to_crf[ost->quality-1];
-			siril_debug_print("x265 constant quality value: %lf\n", crf);
+			crf = x265_quality_to_crf[ost->quality - 1];
+			siril_debug_print("x265 constant quality value: %d\n", crf);
 			retval = av_opt_set_int(c->priv_data, "crf", crf, 0); // For integer values
 			CHECK_OPT_SET_RETVAL;
 			retval = av_opt_set(c->priv_data, "preset", "fast", 0);
