47afbe0a671503cea7aced893eef638d3ce7ce08
cissou8 <cissou8@gmail.com>
2022-02-18 07:35:20 +0100

    try with posh

---
+++ build/windows/native-gitlab-ci/siril-build.ps1
@@ -0,0 +1,4 @@
+git submodule update --init
+
+C:\msys64\mingw64\bin\meson.exe _build
+C:\msys64\mingw64\bin\ninja.exe -C _build install
--- build/windows/native-gitlab-ci/siril-build.sh
+++
@@ -1,6 +0,0 @@
-git clone https://gitlab.com/free-astro/siril.git
-cd siril
-git submodule update --init
-
-meson _build
-ninja -C _build install
