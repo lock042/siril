746884c4de06dd06359bda76cb3335d063660991
cissou8 <cissou8@gmail.com>
2022-02-18 16:26:12 +0100

    go back to bash

--- build/windows/native-gitlab-ci/siril-build.ps1
+++
@@ -1,4 +0,0 @@
-git submodule update --init
-
-C:\msys64\mingw64\bin\meson.exe _build
-C:\msys64\mingw64\bin\ninja.exe -C _build install
---
+++ build/windows/native-gitlab-ci/siril-build.sh
@@ -0,0 +1,4 @@
+git submodule update --init
+
+meson _build
+ninja.exe -C _build install
