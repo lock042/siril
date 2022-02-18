b9ec40f649a972b42511aa112ebab91945d02aec
cissou8 <cissou8@gmail.com>
2022-02-18 01:14:12 +0100

    test with msys2

---
+++ build/windows/native-gitlab-ci/siril-build.sh
@@ -0,0 +1,19 @@
+pacman --noconfirm -S --needed base-devel \
+mingw-w64-x86_64-toolchain \
+mingw-w64-x86_64-cmake \
+git \
+automake \
+mingw-w64-x86_64-meson \
+mingw-w64-x86_64-ninja \
+mingw-w64-x86_64-gtk3 \
+mingw-w64-x86_64-cfitsio \
+mingw-w64-x86_64-fftw \
+mingw-w64-x86_64-gsl \
+mingw-w64-x86_64-libconfig \
+mingw-w64-x86_64-opencv \
+mingw-w64-x86_64-exiv2 \
+mingw-w64-x86_64-json-glib \
+mingw-w64-x86_64-libraw \
+mingw-w64-x86_64-libheif \
+mingw-w64-x86_64-ffms2 \
+mingw-w64-x86_64-curl
