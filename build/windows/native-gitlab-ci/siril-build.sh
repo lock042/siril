6f1c639231977d4c5536b5072c265aa31bcd5a11
cissou8 <cissou8@gmail.com>
2022-02-19 00:34:12 +0100

    try packaging

--- build/windows/native-gitlab-ci/siril-build.sh
+++ build/windows/native-gitlab-ci/siril-build.sh
@@ -1,4 +1,5 @@
 git submodule update --init
 
-meson _build
-ninja.exe -C _build install
+meson _build --buildtype=release --prefix="${INSTALL_PREFIX}"
+ninja -C _build install
+cp -fr /c/msys64/mingw64 ../${W64_OUT}
