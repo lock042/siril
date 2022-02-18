ddfcb683a7f22a8dd5bd05e9cd6660a7817f4d15
cissou8 <cissou8@gmail.com>
2022-02-18 07:04:53 +0100

    split deps and build

--- build/windows/native-gitlab-ci/siril-build.sh
+++ build/windows/native-gitlab-ci/siril-build.sh
@@ -1,24 +1,3 @@
-pacman --noconfirm -S --needed base-devel \
-mingw-w64-x86_64-toolchain \
-mingw-w64-x86_64-cmake \
-git \
-automake \
-mingw-w64-x86_64-meson \
-mingw-w64-x86_64-ninja \
-mingw-w64-x86_64-gtk3 \
-mingw-w64-x86_64-cfitsio \
-mingw-w64-x86_64-fftw \
-mingw-w64-x86_64-gsl \
-mingw-w64-x86_64-libconfig \
-mingw-w64-x86_64-opencv \
-mingw-w64-x86_64-exiv2 \
-mingw-w64-x86_64-json-glib \
-mingw-w64-x86_64-libraw \
-mingw-w64-x86_64-libheif \
-mingw-w64-x86_64-ffms2 \
-mingw-w64-x86_64-curl
-
-
 git clone https://gitlab.com/free-astro/siril.git
 cd siril
 git submodule update --init
