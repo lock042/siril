710f49fc9a9f266280efcbe6ad795d646338cda0
cissou8 <cissou8@gmail.com>
2022-02-19 11:18:15 +0100

    try compiling libraw

--- build/windows/native-gitlab-ci/siril-deps.sh
+++ build/windows/native-gitlab-ci/siril-deps.sh
@@ -3,17 +3,27 @@ mingw-w64-x86_64-toolchain \
 mingw-w64-x86_64-cmake \
 git \
 automake \
+mingw-w64-lcms2 \
+mingw-w64-x86_64-curl \
+mingw-w64-x86_64-json-glib \
 mingw-w64-x86_64-meson \
 mingw-w64-x86_64-ninja \
-mingw-w64-x86_64-gtk3 \
-mingw-w64-x86_64-cfitsio \
 mingw-w64-x86_64-fftw \
-mingw-w64-x86_64-gsl \
-mingw-w64-x86_64-libconfig \
-mingw-w64-x86_64-opencv \
 mingw-w64-x86_64-exiv2 \
-mingw-w64-x86_64-json-glib \
-mingw-w64-x86_64-libraw \
+mingw-w64-x86_64-gtk3 \
+mingw-w64-x86_64-libconfig \
+mingw-w64-x86_64-gsl \
+mingw-w64-x86_64-opencv \
 mingw-w64-x86_64-libheif \
 mingw-w64-x86_64-ffms2 \
-mingw-w64-x86_64-curl
+mingw-w64-x86_64-cfitsio \
+
+# Build LibRaw from github
+mkdir _deps && cd _deps
+git clone --depth 1 https://github.com/LibRaw/LibRaw.git
+cd LibRaw
+autoreconf -fi && \
+./configure --disable-examples --disable-static && \
+make install || exit 1
+cd ..
+
