1c8bf8481604d4968392f8e45725693874989ad6
cissou8 <cissou8@gmail.com>
2022-03-01 00:52:02 +0100

    fix

--- build/windows/crossbuild-gitlab-ci/build-deps.sh
+++ build/windows/crossbuild-gitlab-ci/build-deps.sh
@@ -9,7 +9,7 @@ crossroad install lcms2 \
                   exiv2 \
                   libconfig \
                   gsl \
-                  opencv \
+                  opencv
 crossroad uninstall crt-git
 
 # git clone --depth 1 https://github.com/LibRaw/LibRaw.git
@@ -27,8 +27,7 @@ cd ../../..
 
 cd _deps
 # Install deps from crossroad
-crossroad install opencv \
-                  libheif \
+crossroad install libheif \
                   ffms2 \
                   cfitsio \
 
