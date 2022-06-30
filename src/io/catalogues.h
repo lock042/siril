5cc4ab2a16e8c873ea7e316e557a0cb6d54d3edb
Vincent Hourdin <vh@free-astro.org>
2022-06-30 10:39:59 +0200

    fix forgotten include

--- src/io/catalogues.h
+++ src/io/catalogues.h
@@ -1,8 +1,10 @@
 #ifndef _CATALOGUES_H
 #define _CATALOGUES_H
 
+#include <glib.h>
 #include "core/siril.h"
 #include "core/siril_world_cs.h"
+#include "algos/photometry.h"
 
 int get_stars_from_local_catalogues(double ra, double dec, double radius, fits *fit, float max_mag, pcc_star **stars, int *nb_stars);
 
