8a08bdaa1401ce66bafb3a95175e135c97d3832a
Adrian Knagg-Baugh <aje.baugh@gmail.com>
2022-07-21 20:19:55 +0000

    Star recomposition tool

--- src/gui/histogram.h
+++ src/gui/histogram.h
@@ -44,4 +44,8 @@ void on_histoHighEntry_changed(GtkEditable *editable, gpointer user_data);
 void apply_mtf_to_sequence(struct mtf_data *mtf_args);
 void apply_ght_to_sequence(struct ght_data *ght_args);
 
+void erase_histo_display(cairo_t *cr, int width, int height);
+void draw_grid(cairo_t *cr, int width, int height);
+void display_scale(cairo_t *cr, int width, int height);
+void display_histo(gsl_histogram *histo, cairo_t *cr, int layer, int width, int height, double zoomH, double zoomV, gboolean isOrig);
 #endif
