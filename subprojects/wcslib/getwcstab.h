f73b25b333663f116b91b61ceeaa9ae01d0e059f
Cyril Richard <cyril.richard42@gmail.com>
2022-08-23 23:04:19 +0200

    Try to update wcslib

--- subprojects/wcslib/getwcstab.h
+++ subprojects/wcslib/getwcstab.h
@@ -1,6 +1,6 @@
 /*============================================================================
-  WCSLIB 7.7 - an implementation of the FITS WCS standard.
-  Copyright (C) 1995-2021, Mark Calabretta
+  WCSLIB 7.11 - an implementation of the FITS WCS standard.
+  Copyright (C) 1995-2022, Mark Calabretta
 
   This file is part of WCSLIB.
 
@@ -19,10 +19,10 @@
 
   Author: Mark Calabretta, Australia Telescope National Facility, CSIRO.
   http://www.atnf.csiro.au/people/Mark.Calabretta
-  $Id: getwcstab.h,v 7.7 2021/07/12 06:36:49 mcalabre Exp $
+  $Id: getwcstab.h,v 7.11 2022/04/26 06:13:52 mcalabre Exp $
 *=============================================================================
 *
-* WCSLIB 7.7 - C routines that implement the FITS World Coordinate System
+* WCSLIB 7.11 - C routines that implement the FITS World Coordinate System
 * (WCS) standard.  Refer to the README file provided with WCSLIB for an
 * overview of the library.
 *
