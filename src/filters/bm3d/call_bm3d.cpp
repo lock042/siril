8dc789c2f26bc80a671bda92c0de03c42f3537da
Adrian Knagg-Baugh <aje.baugh@gmail.com>
2022-09-09 12:38:41 +0100

    Code tidyup

--- src/filters/bm3d/call_bm3d.cpp
+++ src/filters/bm3d/call_bm3d.cpp
@@ -29,36 +29,6 @@ extern "C" {
 
 using namespace std;
 
-/*
-float* cutchunk (float *img, unsigned x0, unsigned y0, unsigned xlen, unsigned ylen, unsigned width, unsigned nchans) {
-	// img stored as bgrbgrbgr
-	float *chunk = (float*) calloc(xlen * ylen * nchans, sizeof(float));
-	for (unsigned i = y0, ii = 0; i < (y0 + ylen); i++, ii++) {
-		for (unsigned j = x0, jj = 0; j < (x0 + xlen); j++, jj++) {
-			for (unsigned chan=0; chan < nchans ; chan++) {
-				chunk[chan + (jj + ii * xlen) * nchans] = img[chan + (j + i * width) * nchans];
-			}
-		}
-	}
-	return chunk;
-}
-
-void pastechunk (float *img, float *chunk, unsigned x0, unsigned y0, unsigned xlen, unsigned ylen, unsigned pad, unsigned width, unsigned nchans) {
-	// pad is the half-window size
-	for (unsigned i = y0, ii = pad; i < (y0 + ylen); i++, ii++) {
-		for (unsigned j = x0, jj = pad; j < (x0 + xlen); j++, jj++) {
-			for (unsigned chan=0; chan < nchans; chan++) {
-				img[chan + (j + i * width) * nchans] = chunk[chan + (jj + ii * xlen) * nchans];
-			}
-		}
-	}
-}
-
-unsigned round_up(unsigned num, unsigned factor) {
-	return (num - 1 - (num - 1) % factor + factor);
-}
-*/
-
 void bgrbgr_float_to_fits(fits *image, float *bgrbgr) {
 	size_t ndata = image->rx * image->ry * 3;
 	for (size_t i = 0, j = 0; i < ndata; i += 3, j++) {
@@ -101,12 +71,7 @@ extern "C" int do_bm3d(fits *fit) {
       norm = USHRT_MAX_SINGLE;
     float invnorm = 1 / norm;
 
-//    float ratio = 0.5f; // This will become a user parameter
-//    if (nchans == 1)
-//      ratio *= 0.75f;
-
-
-// Measure background noise
+    // Measure background noise
     float fSigma = 0.f;
     for (size_t chan = 0 ; chan < nchans ; chan++) {
       imstats* stat = statistics(NULL, -1, fit, chan, NULL, STATS_SIGMEAN, MULTI_THREADED);
@@ -115,8 +80,6 @@ extern "C" int do_bm3d(fits *fit) {
     }
     fSigma /= nchans;
     siril_log_message(_("Auto parametrisation: measured background noise level is %f\n"),fSigma);
-//    fSigma *= ratio; // Replace 1.0f with a user-specified parameter. Reasonable default seems to be
-                     // 1.f for colour images and 0.75f for mono.
 
 	// The algorithm parameters set below are the optimal parameters discussed in
 	// (Lebrun, 2012): http://www.ipol.im/pub/art/2012/l-bm3d/
@@ -176,7 +139,7 @@ extern "C" int do_bm3d(fits *fit) {
           w_table[i], h_table[i], nchans, useSD_1, useSD_2, tau_2D_hard, tau_2D_wien, color_space, patch_size, nb_threads, verbose) != EXIT_SUCCESS)
         return EXIT_FAILURE;
 
-      set_progress_bar_data("BM3D denoising...", ((double) i / (double) numchunks));
+      set_progress_bar_data("BM3D denoising...", ((double) (i+1) / (double) numchunks));
     }
 
     // Reassemble chunks
@@ -203,86 +166,3 @@ extern "C" int do_bm3d(fits *fit) {
     }
 	return EXIT_SUCCESS;
 }
-
-
-/*
-int fits_apply_bm3d(fits *fit) {
-    // Parameters
-	float fSigma = 2; // Replace this with getting the nosie level from statistics
-    const bool useSD_1 = FALSE;
-    const bool useSD_2 = FALSE;
-    const unsigned tau_2D_hard = DCT;
-    const unsigned tau_2D_wien = BIOR;
-    const unsigned color_space = OPP;
-    const int patch_size = 0;
-    const int nb_threads = 0;
-    const bool verbose = FALSE;
-
-	//! Declarations
-	vector<float> strip_basic, strip_denoised;
-    const unsigned width = fit->naxes[0], height = fit->naxes[1], chnls = fit->naxes[2];
-    const unsigned npixels = width * height;
-    const unsigned num_strips = min(npixels / 1e6, 1);
-    const unsigned ndata = npixels * chnls;
-    const unsigned strip_height = height % num_strips;
-    // Ensure strip height is at least big enough for the minimum dimension for the denoiser
-    const unsigned strip_npixels = strip_height * width;
-    const unsigned strip_ndata = strip_height * width * chnls;
-    const unsigned last_strip_height = height - (num_strips - 1) * strip_height;
-    // Ensure last_strip_height is at least big enough for the minimum dimension for the denoiser
-    const unsigned last_strip_npixels = last_strip_height * width;
-    const unsigned last_strip_ndata = last_strip_npixels * chnls;
-
-    //! Load image
-    float *bgr_f = fits_to_bgrbgr_float(fit);
-
-   //! Denoising
-   // Split image into strips
-    float *buf_strip[num_strips];
-    float *denoised_strip;
-    for (unsigned strip = 0; strip < num_strips - 1 ; strip ++) { // All except the last strip
-      buf_strip[strip] = bgr_f + (strip_ndata * strip);
-      vector<float> strip_noisy {buf_strip[strip] , buf_strip[strip] + strip_ndata};
-      if (run_bm3d(fSigma, strip_noisy, strip_basic, strip_denoised,
-            width, strip_height, chnls, useSD_1, useSD_2, tau_2D_hard, tau_2D_wien, color_space, patch_size, nb_threads, verbose)
-            != EXIT_SUCCESS)
-        return EXIT_FAILURE;
-      // Put the denoised strips back into the fits
-      denoised_strip = strip_denoised.data();
-      for (unsigned i=0; i<strip_npixels;i++) {
-        fit->fpdata[RLAYER][(strip * strip_npixels) + i] = denoised_strip[3*i];
-        fit->fpdata[GLAYER][(strip * strip_npixels) + i] = denoised_strip[3*i+1];
-        fit->fpdata[BLAYER][(strip * strip_npixels) + i] = denoised_strip[3*i+2];
-      }
-      // Add a progress bar update here
-    }
-    // Do the last strip
-    unsigned laststrip = num_strips - 1;
-    buf_strip[num_strips - 1] = bgr_f + (strip_ndata * (laststrip));
-    vector<float> strip_noisy {buf_strip[laststrip] , buf_strip[laststrip] + last_strip_ndata};
-     if (run_bm3d(fSigma, strip_noisy, strip_basic, strip_denoised,
-          width, strip_height, chnls, useSD_1, useSD_2, tau_2D_hard, tau_2D_wien, color_space, patch_size, nb_threads, verbose)
-          != EXIT_SUCCESS)
-      return EXIT_FAILURE;
-    denoised_strip = strip_denoised.data();
-    for (unsigned i=0; i<last_strip_npixels;i++) {
-      fit->fpdata[RLAYER][(laststrip * strip_npixels) + i] = denoised_strip[3*i];
-      fit->fpdata[GLAYER][(laststrip * strip_npixels) + i] = denoised_strip[3*i+1];
-      fit->fpdata[BLAYER][(laststrip * strip_npixels) + i] = denoised_strip[3*i+2];
-    }
-    free(bgr_f);
-/*   //! save noisy, denoised and differences images
-   cout << endl << "Save images...";
-
-   if (argc > 4)
-   if (save_image(argv[4], img_basic, width, height, chnls) != EXIT_SUCCESS)
-      return EXIT_FAILURE;
-
-	if (save_image(argv[3], img_denoised, width, height, chnls) != EXIT_SUCCESS)
-		return EXIT_FAILURE;
-
-    cout << "done." << endl;
-
-	return EXIT_SUCCESS;
-}
-*/
