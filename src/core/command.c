lock042/siril

commit 5f4507fa46fe1a71c4907712265b8c0795960ac0

Allow applying GHT or MTF stretches to only selected colour channels.

src/core/command.c
@@ -657,6 +657,9 @@ int process_wrecons(int nb) {
 	return CMD_OK;
 }
 int process_linstretch(int nb) {
+	gboolean do_red = TRUE;
+	gboolean do_green = TRUE;
+	gboolean do_blue = TRUE;
 	if (nb <= 0)
 	return CMD_ARG_ERROR;
 
@@ -665,8 +668,31 @@ int process_linstretch(int nb) {
 	if ((BP < 0.0) || (BP > 1.0)) {
 		siril_log_message(_("Black Point BP must be between 0 and 1\n"));
 	}
+	if (word[2]) {
+		if (!strcmp(word[2], "R")) {
+			do_green = FALSE;
+			do_blue = FALSE;
+		}
+		if (!strcmp(word[2], "G")) {
+			do_red = FALSE;
+			do_blue = FALSE;
+		}
+		if (!strcmp(word[2], "B")) {
+			do_green = FALSE;
+			do_red = FALSE;
+		}
+		if (!strcmp(word[2], "RG")) {
+			do_blue = FALSE;
+		}
+		if (!strcmp(word[2], "RB")) {
+			do_green = FALSE;
+		}
+		if (!strcmp(word[2], "GB")) {
+			do_red = FALSE;
+		}
+	}
 	set_cursor_waiting(TRUE);
-	ght_params params = {0.0, 0.0, 0.0, 0.0, 0.0, BP, STRETCH_LINEAR, COL_INDEP};
+	ght_params params = {0.0, 0.0, 0.0, 0.0, 0.0, BP, STRETCH_LINEAR, COL_INDEP, do_red, do_green, do_blue};
 	ght_compute_params compute_params;
 	GHTsetup(&compute_params, 0.0, 0.0, 0.0, 0.0, 0.0, STRETCH_LINEAR);
 	apply_linked_ght_to_fits(&gfit, &gfit, params, compute_params, TRUE);
@@ -678,6 +704,9 @@ int process_linstretch(int nb) {
 int process_ght(int nb) {
 	int stretch_colourmodel = COL_INDEP;
 	int arg_offset = 0;
+	gboolean do_red = TRUE;
+	gboolean do_green = TRUE;
+	gboolean do_blue = TRUE;
 	if (!strcmp(word[1], "-human")) {
 		stretch_colourmodel = COL_HUMANLUM;
 		arg_offset = 1;
@@ -723,8 +752,31 @@ int process_ght(int nb) {
 		return CMD_ARG_ERROR;
 	}
 
+	if (word[6 + arg_offset]) {
+		if (!strcmp(word[6 + arg_offset], "R")) {
+			do_green = FALSE;
+			do_blue = FALSE;
+		}
+		if (!strcmp(word[6 + arg_offset], "G")) {
+			do_red = FALSE;
+			do_blue = FALSE;
+		}
+		if (!strcmp(word[6 + arg_offset], "B")) {
+			do_green = FALSE;
+			do_red = FALSE;
+		}
+		if (!strcmp(word[6 + arg_offset], "RG")) {
+			do_blue = FALSE;
+		}
+		if (!strcmp(word[6 + arg_offset], "RB")) {
+			do_green = FALSE;
+		}
+		if (!strcmp(word[6 + arg_offset], "GB")) {
+			do_red = FALSE;
+		}
+	}
 	set_cursor_waiting(TRUE);
-	ght_params params = {B, D, LP, SP, HP, 0.0, STRETCH_PAYNE_NORMAL, stretch_colourmodel};
+	ght_params params = {B, D, LP, SP, HP, 0.0, STRETCH_PAYNE_NORMAL, stretch_colourmodel, do_red, do_green, do_blue};
 	ght_compute_params compute_params;
 	GHTsetup(&compute_params, B, D, LP, SP, HP, STRETCH_PAYNE_NORMAL);
 	apply_linked_ght_to_fits(&gfit, &gfit, params, compute_params, TRUE);
@@ -734,6 +786,9 @@ int process_ght(int nb) {
 }
 
 int process_invght(int nb) {
+	gboolean do_red = TRUE;
+	gboolean do_green = TRUE;
+	gboolean do_blue = TRUE;
 	int stretch_colourmodel = COL_INDEP;
 	int arg_offset = 0;
 	if (!strcmp(word[1], "-human")) {
@@ -781,9 +836,32 @@ int process_invght(int nb) {
 		siril_log_message(_("Headroom preservation point HP must be between stretch focal point and 1\n"));
 		return CMD_ARG_ERROR;
 	}
+	if (word[6 + arg_offset]) {
+		if (!strcmp(word[6 + arg_offset], "R")) {
+			do_green = FALSE;
+			do_blue = FALSE;
+		}
+		if (!strcmp(word[6 + arg_offset], "G")) {
+			do_red = FALSE;
+			do_blue = FALSE;
+		}
+		if (!strcmp(word[6 + arg_offset], "B")) {
+			do_green = FALSE;
+			do_red = FALSE;
+		}
+		if (!strcmp(word[6 + arg_offset], "RG")) {
+			do_blue = FALSE;
+		}
+		if (!strcmp(word[6 + arg_offset], "RB")) {
+			do_green = FALSE;
+		}
+		if (!strcmp(word[6 + arg_offset], "GB")) {
+			do_red = FALSE;
+		}
+	}
 
 	set_cursor_waiting(TRUE);
-	ght_params params = {B, D, LP, SP, HP, 0.0, STRETCH_PAYNE_INVERSE, stretch_colourmodel};
+	ght_params params = {B, D, LP, SP, HP, 0.0, STRETCH_PAYNE_INVERSE, stretch_colourmodel, do_red, do_green, do_blue};
 	ght_compute_params compute_params;
 	GHTsetup(&compute_params, B, D, LP, SP, HP, STRETCH_PAYNE_INVERSE);
 	apply_linked_ght_to_fits(&gfit, &gfit, params, compute_params, TRUE);
@@ -793,6 +871,9 @@ int process_invght(int nb) {
 }
 
 int process_modasinh(int nb) {
+	gboolean do_red = TRUE;
+	gboolean do_green = TRUE;
+	gboolean do_blue = TRUE;
 	int stretch_colourmodel = COL_INDEP;
 	int arg_offset = 0;
 	if (!strcmp(word[1], "-human")) {
@@ -833,9 +914,32 @@ int process_modasinh(int nb) {
 		siril_log_message(_("HP must be between stretch focal point and 1\n"));
 		return CMD_ARG_ERROR;
 	}
+	if (word[5 + arg_offset]) {
+		if (!strcmp(word[5 + arg_offset], "R")) {
+			do_green = FALSE;
+			do_blue = FALSE;
+		}
+		if (!strcmp(word[5 + arg_offset], "G")) {
+			do_red = FALSE;
+			do_blue = FALSE;
+		}
+		if (!strcmp(word[5 + arg_offset], "B")) {
+			do_green = FALSE;
+			do_red = FALSE;
+		}
+		if (!strcmp(word[5 + arg_offset], "RG")) {
+			do_blue = FALSE;
+		}
+		if (!strcmp(word[5 + arg_offset], "RB")) {
+			do_green = FALSE;
+		}
+		if (!strcmp(word[5 + arg_offset], "GB")) {
+			do_red = FALSE;
+		}
+	}
 
 	set_cursor_waiting(TRUE);
-	ght_params params = {0.0, D, LP, SP, HP, 0.0, STRETCH_ASINH, stretch_colourmodel};
+	ght_params params = {0.0, D, LP, SP, HP, 0.0, STRETCH_ASINH, stretch_colourmodel, do_red, do_green, do_blue};
 	ght_compute_params compute_params;
 	GHTsetup(&compute_params, 0.0, D, LP, SP, HP, STRETCH_ASINH);
 	apply_linked_ght_to_fits(&gfit, &gfit, params, compute_params, TRUE);
@@ -845,6 +949,9 @@ int process_modasinh(int nb) {
 }
 
 int process_invmodasinh(int nb) {
+	gboolean do_red = TRUE;
+	gboolean do_green = TRUE;
+	gboolean do_blue = TRUE;
 	int stretch_colourmodel = COL_INDEP;
 	int arg_offset = 0;
 	if (!strcmp(word[1], "-human")) {
@@ -885,9 +992,32 @@ int process_invmodasinh(int nb) {
 		siril_log_message(_("HP must be between stretch focal point and 1\n"));
 		return CMD_ARG_ERROR;
 	}
+	if (word[5 + arg_offset]) {
+		if (!strcmp(word[5 + arg_offset], "R")) {
+			do_green = FALSE;
+			do_blue = FALSE;
+		}
+		if (!strcmp(word[5 + arg_offset], "G")) {
+			do_red = FALSE;
+			do_blue = FALSE;
+		}
+		if (!strcmp(word[5 + arg_offset], "B")) {
+			do_green = FALSE;
+			do_red = FALSE;
+		}
+		if (!strcmp(word[5 + arg_offset], "RG")) {
+			do_blue = FALSE;
+		}
+		if (!strcmp(word[5 + arg_offset], "RB")) {
+			do_green = FALSE;
+		}
+		if (!strcmp(word[5 + arg_offset], "GB")) {
+			do_red = FALSE;
+		}
+	}
 
 	set_cursor_waiting(TRUE);
-	ght_params params = {0.0, D, LP, SP, HP, 0.0, STRETCH_INVASINH, stretch_colourmodel};
+	ght_params params = {0.0, D, LP, SP, HP, 0.0, STRETCH_INVASINH, stretch_colourmodel, do_red, do_green, do_blue};
 	ght_compute_params compute_params;
 	GHTsetup(&compute_params, 0.0, D, LP, SP, HP, STRETCH_INVASINH);
 	apply_linked_ght_to_fits(&gfit, &gfit, params, compute_params, TRUE);
@@ -1383,11 +1513,37 @@ int process_mtf(int nb) {
 	params.shadows = g_ascii_strtod(word[1], &end1);
 	params.midtones = g_ascii_strtod(word[2], &end2);
 	params.highlights = g_ascii_strtod(word[3], &end3);
+	params.do_red = TRUE;
+	params.do_green = TRUE;
+	params.do_blue = TRUE;
 	if (end1 == word[1] || end2 == word[2] || end3 == word[3] ||
 			params.shadows < 0.0 || params.midtones <= 0.0 || params.highlights <= 0.0 ||
 			params.shadows >= 1.0 || params.midtones >= 1.0 || params.highlights > 1.0) {
 		siril_log_message(_("Invalid argument to %s, aborting.\n"), word[0]);
 		return CMD_ARG_ERROR;
+	}
+	if (word[4]) {
+		if (!strcmp(word[4], "R")) {
+			params.do_green = FALSE;
+			params.do_blue = FALSE;
+		}
+		if (!strcmp(word[4], "G")) {
+			params.do_red = FALSE;
+			params.do_blue = FALSE;
+		}
+		if (!strcmp(word[4], "B")) {
+			params.do_green = FALSE;
+			params.do_red = FALSE;
+		}
+		if (!strcmp(word[4], "RG")) {
+			params.do_blue = FALSE;
+		}
+		if (!strcmp(word[4], "RB")) {
+			params.do_green = FALSE;
+		}
+		if (!strcmp(word[4], "GB")) {
+			params.do_red = FALSE;
+		}
 	}
 
 	apply_linked_mtf_to_fits(&gfit, &gfit, params, TRUE);
@@ -3428,6 +3584,9 @@ int process_seq_mtf(int nb) {
 	args->params.shadows = g_ascii_strtod(word[2], &end1);
 	args->params.midtones = g_ascii_strtod(word[3], &end2);
 	args->params.highlights = g_ascii_strtod(word[4], &end3);
+	args->params.do_red = TRUE;
+	args->params.do_green = TRUE;
+	args->params.do_blue = TRUE;
 	if (end1 == word[2] || end2 == word[3] || end3 == word[4] ||
 			args->params.shadows < 0.0 || args->params.midtones <= 0.0 || args->params.highlights <= 0.0 ||
 			args->params.shadows >= 1.0 || args->params.midtones >= 1.0 || args->params.highlights > 1.0) {
@@ -3440,6 +3599,27 @@ int process_seq_mtf(int nb) {
 	if (nb > startoptargs) {
 		for (int i = startoptargs; i < nb; i++) {
 			if (word[i]) {
+				if (!strcmp(word[i], "R")) {
+					args->params.do_green = FALSE;
+					args->params.do_blue = FALSE;
+				}
+				if (!strcmp(word[i], "G")) {
+					args->params.do_red = FALSE;
+					args->params.do_blue = FALSE;
+				}
+				if (!strcmp(word[i], "B")) {
+					args->params.do_green = FALSE;
+					args->params.do_red = FALSE;
+				}
+				if (!strcmp(word[i], "RG")) {
+					args->params.do_blue = FALSE;
+				}
+				if (!strcmp(word[i], "RB")) {
+					args->params.do_green = FALSE;
+				}
+				if (!strcmp(word[i], "GB")) {
+					args->params.do_red = FALSE;
+				}
 				if (g_str_has_prefix(word[i], "-prefix=")) {
 					char *current = word[i], *value;
 					value = current + 8;

src/filters/mtf.c
@@ -26,30 +26,43 @@
 
 void apply_linked_mtf_to_fits(fits *from, fits *to, struct mtf_params params, gboolean multithreaded) {
 
+	const gboolean do_channel[3] = {params.do_red, params.do_green, params.do_blue};
+
 	g_assert(from->naxes[2] == 1 || from->naxes[2] == 3);
-	const size_t ndata = from->naxes[0] * from->naxes[1] * from->naxes[2];
+	const size_t layersize = from->naxes[0] * from->naxes[1];
 	g_assert(from->type == to->type);
 
 	siril_log_message(_("Applying MTF with values %f, %f, %f\n"),
 			params.shadows, params.midtones, params.highlights);
+
 	if (from->type == DATA_USHORT) {
 		float norm = (float)get_normalized_value(from);
 		float invnorm = 1.0f / norm;
 #ifdef _OPENMP
 #pragma omp parallel for num_threads(com.max_thread) schedule(static) if(multithreaded)
 #endif
-		for (size_t i = 0; i < ndata; i++) {
-			float pxl = from->data[i] * invnorm;
-			float mtf = MTFp(pxl, params);
-			to->data[i] = roundf_to_WORD(mtf * norm);
+		for (size_t j = 0; j < from->naxes[2] ; j++) {
+			if (do_channel[j]) {
+				for (size_t i = 0; i < layersize; i++) {
+					float pxl = from->pdata[j][i] * invnorm;
+					float mtf = MTFp(pxl, params);
+					to->pdata[j][i] = roundf_to_WORD(mtf * norm);
+				}
+			} else
+				memcpy(to->pdata[j], from->pdata[j], layersize * sizeof(WORD));
 		}
 	}
 	else if (from->type == DATA_FLOAT) {
 #ifdef _OPENMP
 #pragma omp parallel for num_threads(com.max_thread) schedule(static) if(multithreaded)
 #endif
-		for (size_t i = 0; i < ndata; i++) {
-			to->fdata[i] = MTFp(from->fdata[i], params);
+		for (size_t j = 0; j < from->naxes[2] ; j++) {
+			if (do_channel[j]) {
+				for (size_t i = 0; i < layersize; i++) {
+					to->fpdata[j][i] = MTFp(from->fpdata[j][i], params);
+				}
+			} else
+				memcpy(to->fpdata[j], from->fpdata[j], layersize * sizeof(float));
 		}
 	}
 	else return;
@@ -80,7 +93,8 @@ float MTFp(float x, struct mtf_params params) {
 }
 
 void apply_linked_pseudoinverse_mtf_to_fits(fits *from, fits *to, struct mtf_params params) {
-
+// This is for use in reversing the pre-stretch applied to linear images for starnet++ input.
+// It does not support selected channels.
 	g_assert(from->naxes[2] == 1 || from->naxes[2] == 3);
 	const size_t ndata = from->naxes[0] * from->naxes[1] * from->naxes[2];
 	g_assert(from->type == to->type);
@@ -187,6 +201,7 @@ int find_linked_midtones_balance_default(fits *fit, struct mtf_params *result) {
 }
 
 void apply_unlinked_mtf_to_fits(fits *from, fits *to, struct mtf_params *params) {
+	const gboolean do_channel[3] = {params->do_red, params->do_green, params->do_blue};
 	g_assert(from->naxes[2] == 1 || from->naxes[2] == 3);
 	const size_t ndata = from->naxes[0] * from->naxes[1];
 	g_assert(from->type == to->type);
@@ -196,36 +211,45 @@ void apply_unlinked_mtf_to_fits(fits *from, fits *to, struct mtf_params *params)
 		float invnorm = 1.0f / norm;
 #ifdef _OPENMP
 		int threads = com.max_thread >= 3 ? 3 : com.max_thread;
-#pragma omp parallel for num_threads(threads) schedule(static) if (threads > 1)
 #endif
 		for (int chan = 0; chan < (int)from->naxes[2]; chan++) {
-			siril_log_message(_("Applying MTF to channel %d with values %f, %f, %f\n"), chan,
+			if (do_channel[chan]) {
+				siril_log_message(_("Applying MTF to channel %d with values %f, %f, %f\n"), chan,
 				params[chan].shadows, params[chan].midtones, params[chan].highlights);
-			for (size_t i = 0; i < ndata; i++) {
-				float pxl = (float)from->pdata[chan][i] * invnorm;
-				float mtf = MTFp(pxl, params[chan]);
-				to->pdata[chan][i] = roundf_to_WORD(mtf * norm);
-			}
+#ifdef _OPENMP
+#pragma omp parallel for num_threads(threads) schedule(static) if (threads > 1)
+#endif
+				for (size_t i = 0; i < ndata; i++) {
+					float pxl = (float)from->pdata[chan][i] * invnorm;
+					float mtf = MTFp(pxl, params[chan]);
+					to->pdata[chan][i] = roundf_to_WORD(mtf * norm);
+				}
+			} else
+				memcpy(to->pdata[chan], from->pdata[chan], ndata * sizeof(WORD));
 		}
 	}
 	else if (from->type == DATA_FLOAT) {
 #ifdef _OPENMP
 		int threads = com.max_thread >= 3 ? 3 : com.max_thread;
-#pragma omp parallel for num_threads(threads) schedule(static) if (threads > 1)
 #endif
 		for (int chan = 0; chan < (int)from->naxes[2]; chan++) {
-			siril_log_message(_("Applying MTF to channel %d with values %f, %f, %f\n"), chan,
+			if (do_channel[chan]) {
+				siril_log_message(_("Applying MTF to channel %d with values %f, %f, %f\n"), chan,
 				params[chan].shadows, params[chan].midtones, params[chan].highlights);
-			for (size_t i = 0; i < ndata; i++) {
-				to->fpdata[chan][i] = MTFp(from->fpdata[chan][i], params[chan]);
-			}
+#ifdef _OPENMP
+#pragma omp parallel for num_threads(threads) schedule(static) if (threads > 1)
+#endif
+				for (size_t i = 0; i < ndata; i++) {
+					to->fpdata[chan][i] = MTFp(from->fpdata[chan][i], params[chan]);
+				}
+			} else
+				memcpy(to->fpdata[chan], from->fpdata[chan], ndata * sizeof(float));
 		}
 	}
 	else return;
 
 	invalidate_stats_from_fit(to);
 }
-
 
 int find_unlinked_midtones_balance(fits *fit, float shadows_clipping, float target_bg, struct mtf_params *results) {
 	int i, invertedChannels = 0;

commit 76f618791714b215013520e01ddacaaccb850878

fixing autostretch command after regression from 5f4507fa

src/core/command.c
@@ -1586,6 +1586,9 @@ int process_autostretch(int nb) {
 	if (linked) {
 		struct mtf_params params;
 		find_linked_midtones_balance(&gfit, shadows_clipping, target_bg, &params);
+		params.do_red = TRUE;
+		params.do_green = TRUE;
+		params.do_blue = TRUE;
 		apply_linked_mtf_to_fits(&gfit, &gfit, params, TRUE);
 	} else {
 		struct mtf_params params[3];

src/filters/mtf.c
@@ -23,10 +23,9 @@
 #include "core/proto.h"
 #include "algos/statistics.h"
 
-
 void apply_linked_mtf_to_fits(fits *from, fits *to, struct mtf_params params, gboolean multithreaded) {
 
-	const gboolean do_channel[3] = {params.do_red, params.do_green, params.do_blue};
+	const gboolean do_channel[3] = { params.do_red, params.do_green, params.do_blue };
 
 	g_assert(from->naxes[2] == 1 || from->naxes[2] == 3);
 	const size_t layersize = from->naxes[0] * from->naxes[1];
@@ -201,7 +200,6 @@ int find_linked_midtones_balance_default(fits *fit, struct mtf_params *result) {
 }
 
 void apply_unlinked_mtf_to_fits(fits *from, fits *to, struct mtf_params *params) {
-	const gboolean do_channel[3] = {params->do_red, params->do_green, params->do_blue};
 	g_assert(from->naxes[2] == 1 || from->naxes[2] == 3);
 	const size_t ndata = from->naxes[0] * from->naxes[1];
 	g_assert(from->type == to->type);
@@ -213,19 +211,16 @@ void apply_unlinked_mtf_to_fits(fits *from, fits *to, struct mtf_params *params)
 		int threads = com.max_thread >= 3 ? 3 : com.max_thread;
 #endif
 		for (int chan = 0; chan < (int)from->naxes[2]; chan++) {
-			if (do_channel[chan]) {
-				siril_log_message(_("Applying MTF to channel %d with values %f, %f, %f\n"), chan,
-				params[chan].shadows, params[chan].midtones, params[chan].highlights);
+			siril_log_message(_("Applying MTF to channel %d with values %f, %f, %f\n"), chan,
+					params[chan].shadows, params[chan].midtones, params[chan].highlights);
 #ifdef _OPENMP
 #pragma omp parallel for num_threads(threads) schedule(static) if (threads > 1)
 #endif
-				for (size_t i = 0; i < ndata; i++) {
-					float pxl = (float)from->pdata[chan][i] * invnorm;
-					float mtf = MTFp(pxl, params[chan]);
-					to->pdata[chan][i] = roundf_to_WORD(mtf * norm);
-				}
-			} else
-				memcpy(to->pdata[chan], from->pdata[chan], ndata * sizeof(WORD));
+			for (size_t i = 0; i < ndata; i++) {
+				float pxl = (float)from->pdata[chan][i] * invnorm;
+				float mtf = MTFp(pxl, params[chan]);
+				to->pdata[chan][i] = roundf_to_WORD(mtf * norm);
+			}
 		}
 	}
 	else if (from->type == DATA_FLOAT) {
@@ -233,17 +228,14 @@ void apply_unlinked_mtf_to_fits(fits *from, fits *to, struct mtf_params *params)
 		int threads = com.max_thread >= 3 ? 3 : com.max_thread;
 #endif
 		for (int chan = 0; chan < (int)from->naxes[2]; chan++) {
-			if (do_channel[chan]) {
-				siril_log_message(_("Applying MTF to channel %d with values %f, %f, %f\n"), chan,
-				params[chan].shadows, params[chan].midtones, params[chan].highlights);
+			siril_log_message(_("Applying MTF to channel %d with values %f, %f, %f\n"), chan,
+					params[chan].shadows, params[chan].midtones, params[chan].highlights);
 #ifdef _OPENMP
 #pragma omp parallel for num_threads(threads) schedule(static) if (threads > 1)
 #endif
-				for (size_t i = 0; i < ndata; i++) {
-					to->fpdata[chan][i] = MTFp(from->fpdata[chan][i], params[chan]);
-				}
-			} else
-				memcpy(to->fpdata[chan], from->fpdata[chan], ndata * sizeof(float));
+			for (size_t i = 0; i < ndata; i++) {
+				to->fpdata[chan][i] = MTFp(from->fpdata[chan][i], params[chan]);
+			}
 		}
 	}
 	else return;