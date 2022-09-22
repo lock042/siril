db2085e03e56de1f2e96e9b153efd80ae17665ac
Vincent Hourdin <vh@free-astro.org>
2022-09-22 12:30:16 +0200

    adding files from previous commits

---
+++ src/gui/colors.c
@@ -0,0 +1,407 @@
+/*
+ * This file is part of Siril, an astronomy image processor.
+ * Copyright (C) 2005-2011 Francois Meyer (dulle at free.fr)
+ * Copyright (C) 2012-2020 team free-astro (see more in AUTHORS file)
+ * Reference site is https://free-astro.org/index.php/Siril
+ *
+ * Siril is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Siril is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Siril. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <assert.h>
+#include <math.h>
+
+#include "core/siril.h"
+#include "core/proto.h"
+#include "core/processing.h"
+#include "core/undo.h"
+#include "core/OS_utils.h"
+#include "io/single_image.h"
+#include "io/image_format_fits.h"
+#include "algos/colors.h"
+#include "algos/statistics.h"
+
+#include "gui/progress_and_log.h"
+#include "gui/callbacks.h"
+#include "gui/image_display.h"
+#include "gui/image_interactions.h"
+#include "gui/registration_preview.h"
+#include "gui/message_dialog.h"
+#include "gui/histogram.h"
+#include "gui/dialogs.h"
+#include "gui/utils.h"
+#include "gui/colors.h"
+
+void on_button_bkg_selection_clicked(GtkButton *button, gpointer user_data) {
+	static GtkSpinButton *selection_black_value[4] = { NULL, NULL, NULL, NULL };
+
+	if ((!com.selection.h) || (!com.selection.w)) {
+		siril_message_dialog(GTK_MESSAGE_WARNING, _("There is no selection"),
+				_("Make a selection of the background area"));
+		return;
+	}
+
+	if (!selection_black_value[0]) {
+		selection_black_value[0] = GTK_SPIN_BUTTON(
+				gtk_builder_get_object(gui.builder, "spin_bkg_x"));
+		selection_black_value[1] = GTK_SPIN_BUTTON(
+				gtk_builder_get_object(gui.builder, "spin_bkg_y"));
+		selection_black_value[2] = GTK_SPIN_BUTTON(
+				gtk_builder_get_object(gui.builder, "spin_bkg_w"));
+		selection_black_value[3] = GTK_SPIN_BUTTON(
+				gtk_builder_get_object(gui.builder, "spin_bkg_h"));
+	}
+
+	gtk_spin_button_set_value(selection_black_value[0], com.selection.x);
+	gtk_spin_button_set_value(selection_black_value[1], com.selection.y);
+	gtk_spin_button_set_value(selection_black_value[2], com.selection.w);
+	gtk_spin_button_set_value(selection_black_value[3], com.selection.h);
+}
+
+void initialize_calibration_interface() {
+	static GtkAdjustment *selection_black_adjustment[4] = { NULL, NULL, NULL,
+		NULL };
+	static GtkAdjustment *selection_white_adjustment[4] = { NULL, NULL, NULL,
+		NULL };
+
+	if (!selection_black_adjustment[0]) {
+		selection_black_adjustment[0] = GTK_ADJUSTMENT(
+				gtk_builder_get_object(gui.builder, "adjustment_bkg_x"));
+		selection_black_adjustment[1] = GTK_ADJUSTMENT(
+				gtk_builder_get_object(gui.builder, "adjustment_bkg_y"));
+		selection_black_adjustment[2] = GTK_ADJUSTMENT(
+				gtk_builder_get_object(gui.builder, "adjustment_bkg_w"));
+		selection_black_adjustment[3] = GTK_ADJUSTMENT(
+				gtk_builder_get_object(gui.builder, "adjustment_bkg_h"));
+	}
+	if (!selection_white_adjustment[0]) {
+		selection_white_adjustment[0] = GTK_ADJUSTMENT(
+				gtk_builder_get_object(gui.builder, "adjustment_white_x"));
+		selection_white_adjustment[1] = GTK_ADJUSTMENT(
+				gtk_builder_get_object(gui.builder, "adjustment_white_y"));
+		selection_white_adjustment[2] = GTK_ADJUSTMENT(
+				gtk_builder_get_object(gui.builder, "adjustment_white_w"));
+		selection_white_adjustment[3] = GTK_ADJUSTMENT(
+				gtk_builder_get_object(gui.builder, "adjustment_white_h"));
+	}
+	gtk_adjustment_set_upper(selection_black_adjustment[0], gfit.rx);
+	gtk_adjustment_set_upper(selection_black_adjustment[1], gfit.ry);
+	gtk_adjustment_set_upper(selection_black_adjustment[2], gfit.rx);
+	gtk_adjustment_set_upper(selection_black_adjustment[3], gfit.ry);
+	gtk_adjustment_set_value(selection_black_adjustment[0], 0);
+	gtk_adjustment_set_value(selection_black_adjustment[1], 0);
+	gtk_adjustment_set_value(selection_black_adjustment[2], 0);
+	gtk_adjustment_set_value(selection_black_adjustment[3], 0);
+
+	gtk_adjustment_set_upper(selection_white_adjustment[0], gfit.rx);
+	gtk_adjustment_set_upper(selection_white_adjustment[1], gfit.ry);
+	gtk_adjustment_set_upper(selection_white_adjustment[2], gfit.rx);
+	gtk_adjustment_set_upper(selection_white_adjustment[3], gfit.ry);
+	gtk_adjustment_set_value(selection_white_adjustment[0], 0);
+	gtk_adjustment_set_value(selection_white_adjustment[1], 0);
+	gtk_adjustment_set_value(selection_white_adjustment[2], 0);
+	gtk_adjustment_set_value(selection_white_adjustment[3], 0);
+}
+
+void on_button_bkg_neutralization_clicked(GtkButton *button, gpointer user_data) {
+	static GtkSpinButton *selection_black_value[4] = { NULL, NULL, NULL, NULL };
+	rectangle black_selection;
+	int width, height;
+
+	if (!selection_black_value[0]) {
+		selection_black_value[0] = GTK_SPIN_BUTTON(
+				gtk_builder_get_object(gui.builder, "spin_bkg_x"));
+		selection_black_value[1] = GTK_SPIN_BUTTON(
+				gtk_builder_get_object(gui.builder, "spin_bkg_y"));
+		selection_black_value[2] = GTK_SPIN_BUTTON(
+				gtk_builder_get_object(gui.builder, "spin_bkg_w"));
+		selection_black_value[3] = GTK_SPIN_BUTTON(
+				gtk_builder_get_object(gui.builder, "spin_bkg_h"));
+	}
+	width = (int) gtk_spin_button_get_value(selection_black_value[2]);
+	height = (int) gtk_spin_button_get_value(selection_black_value[3]);
+
+	if ((!width) || (!height)) {
+		siril_message_dialog( GTK_MESSAGE_WARNING, _("There is no selection"),
+				_("Make a selection of the background area"));
+		return;
+	}
+	black_selection.x = gtk_spin_button_get_value(selection_black_value[0]);
+	black_selection.y = gtk_spin_button_get_value(selection_black_value[1]);
+	black_selection.w = gtk_spin_button_get_value(selection_black_value[2]);
+	black_selection.h = gtk_spin_button_get_value(selection_black_value[3]);
+
+	undo_save_state(&gfit, _("Background neutralization"));
+
+	set_cursor_waiting(TRUE);
+	background_neutralize(&gfit, black_selection);
+	delete_selected_area();
+
+	update_gfit_histogram_if_needed();
+	redraw(REMAP_ALL);
+	redraw_previews();
+	set_cursor_waiting(FALSE);
+}
+
+void on_button_white_selection_clicked(GtkButton *button, gpointer user_data) {
+	static GtkSpinButton *selection_white_value[4] = { NULL, NULL, NULL, NULL };
+
+	if (!selection_white_value[0]) {
+		selection_white_value[0] = GTK_SPIN_BUTTON(
+				gtk_builder_get_object(gui.builder, "spin_white_x"));
+		selection_white_value[1] = GTK_SPIN_BUTTON(
+				gtk_builder_get_object(gui.builder, "spin_white_y"));
+		selection_white_value[2] = GTK_SPIN_BUTTON(
+				gtk_builder_get_object(gui.builder, "spin_white_w"));
+		selection_white_value[3] = GTK_SPIN_BUTTON(
+				gtk_builder_get_object(gui.builder, "spin_white_h"));
+	}
+
+	if ((!com.selection.h) || (!com.selection.w)) {
+		siril_message_dialog( GTK_MESSAGE_WARNING, _("There is no selection"),
+				_("Make a selection of the white reference area"));
+		return;
+	}
+
+	gtk_spin_button_set_value(selection_white_value[0], com.selection.x);
+	gtk_spin_button_set_value(selection_white_value[1], com.selection.y);
+	gtk_spin_button_set_value(selection_white_value[2], com.selection.w);
+	gtk_spin_button_set_value(selection_white_value[3], com.selection.h);
+}
+
+static void white_balance(fits *fit, gboolean is_manual, rectangle white_selection,
+		rectangle black_selection) {
+	int chan;
+	double norm, low, high;
+	double kw[3] = { 0.0, 0.0, 0.0 };
+	double bg[3] = { 0.0, 0.0, 0.0 };
+	static GtkRange *scale_white_balance[3] = { NULL, NULL, NULL };
+	static GtkRange *scaleLimit[2] = { NULL, NULL };
+
+	if (scale_white_balance[RLAYER] == NULL) {
+		scale_white_balance[RLAYER] = GTK_RANGE(lookup_widget("scale_r"));
+		scale_white_balance[GLAYER] = GTK_RANGE(lookup_widget("scale_g"));
+		scale_white_balance[BLAYER] = GTK_RANGE(lookup_widget("scale_b"));
+
+		scaleLimit[0] = GTK_RANGE(lookup_widget("lowWhiteColorCalibScale"));
+		scaleLimit[1] = GTK_RANGE(lookup_widget("upWhiteColorCalibScale"));
+	}
+
+	assert(fit->naxes[2] == 3);
+	norm = get_normalized_value(fit);
+
+	if (is_manual) {
+		kw[RLAYER] = gtk_range_get_value(scale_white_balance[RLAYER]);
+		kw[GLAYER] = gtk_range_get_value(scale_white_balance[GLAYER]);
+		kw[BLAYER] = gtk_range_get_value(scale_white_balance[BLAYER]);
+
+	} else {
+		low = gtk_range_get_value(scaleLimit[0]);
+		high = gtk_range_get_value(scaleLimit[1]);
+		get_coeff_for_wb(fit, white_selection, black_selection, kw, bg, norm, low, high);
+	}
+#ifdef _OPENMP
+#pragma omp parallel for num_threads(com.max_thread) private(chan) schedule(static)
+#endif
+	for (chan = 0; chan < 3; chan++) {
+		if (kw[chan] == 1.0) continue;
+		calibrate(fit, chan, kw[chan], bg[chan], norm);
+	}
+
+	invalidate_stats_from_fit(fit);
+	invalidate_gfit_histogram();
+}
+
+void on_calibration_apply_button_clicked(GtkButton *button, gpointer user_data) {
+	rectangle black_selection, white_selection;
+	static GtkSpinButton *selection_black_value[4] = { NULL, NULL, NULL, NULL };
+	static GtkSpinButton *selection_white_value[4] = { NULL, NULL, NULL, NULL };
+	struct timeval t_start, t_end;
+
+	siril_log_color_message(_("Color Calibration: processing...\n"), "green");
+	gettimeofday(&t_start, NULL);
+
+	GtkToggleButton *manual = GTK_TOGGLE_BUTTON(lookup_widget("checkbutton_manual_calibration"));
+	gboolean is_manual = gtk_toggle_button_get_active(manual);
+
+	if (!selection_black_value[0]) {
+		selection_black_value[0] = GTK_SPIN_BUTTON(lookup_widget("spin_bkg_x"));
+		selection_black_value[1] = GTK_SPIN_BUTTON(lookup_widget("spin_bkg_y"));
+		selection_black_value[2] = GTK_SPIN_BUTTON(lookup_widget("spin_bkg_w"));
+		selection_black_value[3] = GTK_SPIN_BUTTON(lookup_widget("spin_bkg_h"));
+	}
+
+	if (!selection_white_value[0]) {
+		selection_white_value[0] = GTK_SPIN_BUTTON(lookup_widget("spin_white_x"));
+		selection_white_value[1] = GTK_SPIN_BUTTON(lookup_widget("spin_white_y"));
+		selection_white_value[2] = GTK_SPIN_BUTTON(lookup_widget("spin_white_w"));
+		selection_white_value[3] = GTK_SPIN_BUTTON(lookup_widget("spin_white_h"));
+	}
+
+	black_selection.x = gtk_spin_button_get_value(selection_black_value[0]);
+	black_selection.y = gtk_spin_button_get_value(selection_black_value[1]);
+	black_selection.w = gtk_spin_button_get_value(selection_black_value[2]);
+	black_selection.h = gtk_spin_button_get_value(selection_black_value[3]);
+
+	if ((!black_selection.w || !black_selection.h) && !is_manual) {
+		siril_message_dialog( GTK_MESSAGE_WARNING, _("There is no selection"),
+				_("Make a selection of the background area"));
+		return;
+	}
+
+	white_selection.x = gtk_spin_button_get_value(selection_white_value[0]);
+	white_selection.y = gtk_spin_button_get_value(selection_white_value[1]);
+	white_selection.w = gtk_spin_button_get_value(selection_white_value[2]);
+	white_selection.h = gtk_spin_button_get_value(selection_white_value[3]);
+
+	if ((!white_selection.w || !white_selection.h) && !is_manual) {
+		siril_message_dialog( GTK_MESSAGE_WARNING, _("There is no selection"),
+				_("Make a selection of the white reference area"));
+		return;
+	}
+
+	set_cursor_waiting(TRUE);
+	undo_save_state(&gfit, _("Color Calibration"));
+	white_balance(&gfit, is_manual, white_selection, black_selection);
+
+	gettimeofday(&t_end, NULL);
+
+	show_time(t_start, t_end);
+
+	delete_selected_area();
+
+	redraw(REMAP_ALL);
+	redraw_previews();
+	update_gfit_histogram_if_needed();
+	set_cursor_waiting(FALSE);
+}
+
+void on_calibration_close_button_clicked(GtkButton *button, gpointer user_data) {
+	siril_close_dialog("color_calibration");
+}
+
+void on_checkbutton_manual_calibration_toggled(GtkToggleButton *togglebutton,
+		gpointer user_data) {
+	GtkWidget *cc_box_red = lookup_widget("cc_box_red");
+	GtkWidget *scale_r = lookup_widget("scale_r");
+	GtkWidget *cc_box_green = lookup_widget("cc_box_green");
+	GtkWidget *scale_g = lookup_widget("scale_g");
+	GtkWidget *cc_box_blue = lookup_widget("cc_box_blue");
+	GtkWidget *scale_b = lookup_widget("scale_b");
+	gtk_widget_set_sensitive(cc_box_red, gtk_toggle_button_get_active(togglebutton));
+	gtk_widget_set_sensitive(scale_r, gtk_toggle_button_get_active(togglebutton));
+	gtk_widget_set_sensitive(cc_box_green, gtk_toggle_button_get_active(togglebutton));
+	gtk_widget_set_sensitive(scale_g, gtk_toggle_button_get_active(togglebutton));
+	gtk_widget_set_sensitive(cc_box_blue, gtk_toggle_button_get_active(togglebutton));
+	gtk_widget_set_sensitive(scale_b, gtk_toggle_button_get_active(togglebutton));
+}
+
+void negative_processing() {
+	set_cursor_waiting(TRUE);
+	undo_save_state(&gfit, _("Negative Transformation"));
+	pos_to_neg(&gfit);
+	invalidate_stats_from_fit(&gfit);
+	invalidate_gfit_histogram();
+	update_gfit_histogram_if_needed();
+	redraw(REMAP_ALL);
+	redraw_previews();
+	set_cursor_waiting(FALSE);
+}
+/**********************************************************************/
+
+void on_extract_channel_button_close_clicked(GtkButton *button,
+		gpointer user_data) {
+	siril_close_dialog("extract_channel_dialog");
+}
+
+void on_combo_extract_colors_changed(GtkComboBox *box, gpointer user_data) {
+	switch(gtk_combo_box_get_active(box)) {
+		default:
+		case 0: // RGB
+			gtk_label_set_text(GTK_LABEL(lookup_widget("label_extract_c1")), _("Red: "));
+			gtk_label_set_text(GTK_LABEL(lookup_widget("label_extract_c2")), _("Green: "));
+			gtk_label_set_text(GTK_LABEL(lookup_widget("label_extract_c3")), _("Blue: "));
+			break;
+		case 1: // HSL
+			gtk_label_set_text(GTK_LABEL(lookup_widget("label_extract_c1")), _("Hue: "));
+			gtk_label_set_text(GTK_LABEL(lookup_widget("label_extract_c2")), _("Saturation: "));
+			gtk_label_set_text(GTK_LABEL(lookup_widget("label_extract_c3")), _("Lightness: "));
+			break;
+		case 2: // HSV
+			gtk_label_set_text(GTK_LABEL(lookup_widget("label_extract_c1")), _("Hue: "));
+			gtk_label_set_text(GTK_LABEL(lookup_widget("label_extract_c2")), _("Saturation: "));
+			gtk_label_set_text(GTK_LABEL(lookup_widget("label_extract_c3")), _("Value: "));
+			break;
+		case 3: // CIE L*a*b*
+			gtk_label_set_text(GTK_LABEL(lookup_widget("label_extract_c1")), "L*: ");
+			gtk_label_set_text(GTK_LABEL(lookup_widget("label_extract_c2")), "a*: ");
+			gtk_label_set_text(GTK_LABEL(lookup_widget("label_extract_c3")), "b*: ");
+	}
+}
+
+void on_extract_channel_button_ok_clicked(GtkButton *button, gpointer user_data) {
+	static GtkEntry *channel_extract_entry[3] = { NULL, NULL, NULL };
+	static GtkComboBox *combo_extract_channel = NULL;
+
+	if (get_thread_run()) {
+		PRINT_ANOTHER_THREAD_RUNNING;
+		return;
+	}
+
+	struct extract_channels_data *args = malloc(sizeof(struct extract_channels_data));
+	if (!args) {
+		PRINT_ALLOC_ERR;
+		return;
+	}
+
+	if (combo_extract_channel == NULL) {
+		combo_extract_channel = GTK_COMBO_BOX(
+				lookup_widget("combo_extract_colors"));
+		channel_extract_entry[0] = GTK_ENTRY(
+				lookup_widget("Ch1_extract_channel_entry"));
+		channel_extract_entry[1] = GTK_ENTRY(
+				lookup_widget("Ch2_extract_channel_entry"));
+		channel_extract_entry[2] = GTK_ENTRY(
+				lookup_widget("Ch3_extract_channel_entry"));
+	}
+
+	args->type = gtk_combo_box_get_active(combo_extract_channel);
+	args->str_type = gtk_combo_box_get_active_id(combo_extract_channel);
+
+	args->channel[0] = g_strdup_printf("%s%s", gtk_entry_get_text(channel_extract_entry[0]), com.pref.ext);
+	args->channel[1] = g_strdup_printf("%s%s", gtk_entry_get_text(channel_extract_entry[1]), com.pref.ext);
+	args->channel[2] = g_strdup_printf("%s%s", gtk_entry_get_text(channel_extract_entry[2]), com.pref.ext);
+
+	if ((args->channel[0][0] != '\0') && (args->channel[1][0] != '\0')
+			&& (args->channel[2][0] != '\0')) {
+		args->fit = calloc(1, sizeof(fits));
+		set_cursor_waiting(TRUE);
+		if (copyfits(&gfit, args->fit, CP_ALLOC | CP_COPYA | CP_FORMAT, -1)) {
+			siril_log_message(_("Could not copy the input image, aborting.\n"));
+			free(args->fit);
+			free(args->channel[0]);
+			free(args->channel[1]);
+			free(args->channel[2]);
+			free(args);
+		} else {
+			copy_fits_metadata(&gfit, args->fit);
+			start_in_new_thread(extract_channels, args);
+		}
+	}
+	else {
+		free(args->channel[0]);
+		free(args->channel[1]);
+		free(args->channel[2]);
+		free(args);
+	}
+}
---
+++ src/gui/menu_gray_geometry.c
@@ -0,0 +1,283 @@
+/*
+ * This file is part of Siril, an astronomy image processor.
+ * Copyright (C) 2005-2011 Francois Meyer (dulle at free.fr)
+ * Copyright (C) 2012-2020 team free-astro (see more in AUTHORS file)
+ * Reference site is https://free-astro.org/index.php/Siril
+ *
+ * Siril is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Siril is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Siril. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "core/siril.h"
+#include "algos/geometry.h"
+#include "core/undo.h"
+#include "core/processing.h"
+#include "io/single_image.h"
+#include "algos/astrometry_solver.h"
+
+#include "gui/callbacks.h"
+#include "gui/dialogs.h"
+#include "gui/image_display.h"
+#include "gui/image_interactions.h"
+#include "gui/message_dialog.h"
+#include "gui/progress_and_log.h"
+#include "gui/PSF_list.h"
+#include "gui/registration_preview.h"
+#include "gui/siril_preview.h"
+#include "gui/utils.h"
+#include "menu_gray_geometry.h"
+
+/**
+ *  ROTATION
+ */
+static void rotate_gui(fits *fit) {
+	if (com.selection.w == 0 || com.selection.h == 0) return;
+	static GtkToggleButton *crop_rotation = NULL;
+	double angle = gtk_spin_button_get_value(
+			GTK_SPIN_BUTTON(lookup_widget("spinbutton_rotation")));
+	int interpolation = gtk_combo_box_get_active(
+			GTK_COMBO_BOX(lookup_widget("combo_interpolation_rotation")));
+	int cropped;
+
+	if (crop_rotation == NULL) {
+		crop_rotation = GTK_TOGGLE_BUTTON(
+				lookup_widget("checkbutton_rotation_crop"));
+	}
+	cropped = gtk_toggle_button_get_active(crop_rotation);
+	if ((!cropped) & (com.selection.w < gfit.rx || com.selection.h < gfit.ry)) {
+		cropped = siril_confirm_dialog(_("Crop confirmation"), ("A selection is active and its size is smaller than the original image. Do you want to crop to current selection?"), _("Crop"));
+		if (cropped)
+			gtk_toggle_button_set_active(crop_rotation, TRUE);
+	}
+
+	set_cursor_waiting(TRUE);
+	undo_save_state(fit, _("Rotation (%.1lfdeg, cropped=%s)"), angle,
+			cropped ? "TRUE" : "FALSE");
+	verbose_rotate_image(fit, com.selection, angle, interpolation, cropped);
+
+	// the UI is still opened, need to reset selection
+	// to current image size and reset rotation
+	rectangle area = {0, 0, fit->rx, fit->ry};
+	memcpy(&com.selection, &area, sizeof(rectangle));
+	gtk_spin_button_set_value(
+			GTK_SPIN_BUTTON(lookup_widget("spinbutton_rotation")), 0.);
+	new_selection_zone();
+
+	update_zoom_label();
+	redraw(REMAP_ALL);
+	redraw_previews();
+	set_cursor_waiting(FALSE);
+}
+void siril_rotate90() {
+	set_cursor_waiting(TRUE);
+	undo_save_state(&gfit, _("Rotation (90 deg)"));
+	verbose_rotate_fast(&gfit, 90); // fast rotation, no interpolation, no crop
+	update_zoom_label();
+	redraw(REMAP_ALL);
+	redraw_previews();
+	set_cursor_waiting(FALSE);
+}
+
+void siril_rotate270() {
+	set_cursor_waiting(TRUE);
+	undo_save_state(&gfit, _("Rotation (-90 deg)"));
+	verbose_rotate_fast(&gfit, -90); // fast rotation, no interpolation, no crop
+	update_zoom_label();
+	redraw(REMAP_ALL);
+	redraw_previews();
+	set_cursor_waiting(FALSE);
+}
+
+void on_button_rotation_close_clicked(GtkButton *button, gpointer user_data) {
+	delete_selected_area();
+	siril_close_dialog("rotation_dialog");
+}
+
+void on_button_rotation_ok_clicked(GtkButton *button, gpointer user_data) {
+	rotate_gui(&gfit);
+}
+
+void on_spin_rotation_value_changed(GtkSpinButton *button, gpointer user_data) {
+	if (com.selection.w != 0 && com.selection.h != 0) {
+		gui.rotation = gtk_spin_button_get_value(button);
+		redraw(REDRAW_OVERLAY);
+	}
+}
+
+void on_checkbutton_rotation_crop_toggled(GtkToggleButton *button, gpointer user_data) {
+	if (!gtk_toggle_button_get_active(button)) {
+		rectangle area = {0, 0, gfit.rx, gfit.ry};
+		memcpy(&com.selection, &area, sizeof(rectangle));
+		new_selection_zone();
+	}
+}
+
+/******
+ * MIRROR
+ */
+
+void on_menuitem_mirrorx_activate(GtkMenuItem *menuitem, gpointer user_data) {
+	mirrorx_gui(&gfit);
+}
+
+void on_mirrorx_button_clicked(GtkToolButton *button, gpointer user_data) {
+	mirrorx_gui(&gfit);
+}
+
+void on_menuitem_mirrory_activate(GtkMenuItem *menuitem, gpointer user_data) {
+	mirrory_gui(&gfit);
+}
+
+void on_mirrory_button_clicked(GtkToolButton *button, gpointer user_data) {
+	mirrory_gui(&gfit);
+}
+
+void mirrorx_gui(fits *fit) {
+	set_cursor_waiting(TRUE);
+	undo_save_state(fit, _("Mirror X"));
+	mirrorx(fit, TRUE);
+	redraw(REMAP_ALL);
+	redraw_previews();
+	set_cursor_waiting(FALSE);
+}
+
+void mirrory_gui(fits *fit) {
+	set_cursor_waiting(TRUE);
+	undo_save_state(fit, _("Mirror Y"));
+	mirrory(fit, TRUE);
+	redraw(REMAP_ALL);
+	redraw_previews();
+	set_cursor_waiting(FALSE);
+}
+
+/*************
+ * RESAMPLE
+ */
+void on_button_resample_ok_clicked(GtkButton *button, gpointer user_data) {
+	if (confirm_delete_wcs_keywords(&gfit)) {
+		double sample[2];
+		sample[0] = gtk_spin_button_get_value(
+				GTK_SPIN_BUTTON(lookup_widget("spinbutton_resample_X")));
+		sample[1] = gtk_spin_button_get_value(
+				GTK_SPIN_BUTTON(lookup_widget("spinbutton_resample_Y")));
+		int interpolation = gtk_combo_box_get_active(
+				GTK_COMBO_BOX(lookup_widget("combo_interpolation")));
+
+		set_cursor_waiting(TRUE);
+		int toX = round_to_int((sample[0] / 100.0) * gfit.rx);
+		int toY = round_to_int((sample[1] / 100.0) * gfit.ry);
+		undo_save_state(&gfit, _("Resample (%g - %g)"), sample[0] / 100.0,
+				sample[1] / 100.0);
+		verbose_resize_gaussian(&gfit, toX, toY, interpolation);
+
+		redraw(REMAP_ALL);
+		redraw_previews();
+		update_MenuItem();
+		set_cursor_waiting(FALSE);
+	}
+}
+
+void on_button_resample_close_clicked(GtkButton *button, gpointer user_data) {
+	siril_close_dialog("resample_dialog");
+}
+
+void on_spinbutton_resample_X_value_changed(GtkSpinButton *spinbutton,
+		gpointer user_data) {
+	GtkToggleButton *ratio = GTK_TOGGLE_BUTTON(
+			lookup_widget("button_sample_ratio"));
+	double xvalue = gtk_spin_button_get_value(
+			GTK_SPIN_BUTTON(lookup_widget("spinbutton_resample_X")));
+
+	if (gtk_toggle_button_get_active(ratio))
+		gtk_spin_button_set_value(
+				GTK_SPIN_BUTTON(lookup_widget("spinbutton_resample_Y")),
+				xvalue);
+}
+
+void on_spinbutton_resample_Y_value_changed(GtkSpinButton *spinbutton,
+		gpointer user_data) {
+	GtkToggleButton *ratio = GTK_TOGGLE_BUTTON(
+			lookup_widget("button_sample_ratio"));
+	double yvalue = gtk_spin_button_get_value(
+			GTK_SPIN_BUTTON(lookup_widget("spinbutton_resample_Y")));
+
+	if (gtk_toggle_button_get_active(ratio))
+		gtk_spin_button_set_value(
+				GTK_SPIN_BUTTON(lookup_widget("spinbutton_resample_X")),
+				yvalue);
+}
+
+void on_button_sample_ratio_toggled(GtkToggleButton *button, gpointer user_data) {
+	double xvalue = gtk_spin_button_get_value(
+			GTK_SPIN_BUTTON(lookup_widget("spinbutton_resample_X")));
+
+	if (gtk_toggle_button_get_active(button))
+		gtk_spin_button_set_value(
+				GTK_SPIN_BUTTON(lookup_widget("spinbutton_resample_Y")),
+				xvalue);
+}
+/**************
+ * CROP
+ */
+void siril_crop() {
+	undo_save_state(&gfit, _("Crop (x=%d, y=%d, w=%d, h=%d)"),
+			com.selection.x, com.selection.y, com.selection.w,
+			com.selection.h);
+	if (is_preview_active()) {
+		siril_message_dialog(GTK_MESSAGE_INFO, _("A live preview session is active"),
+				_("It is impossible to crop the image when a filter with preview session is active. "
+						"Please consider to close the filter dialog first."));
+		return;
+	}
+	crop(&gfit, &com.selection);
+	delete_selected_area();
+	reset_display_offset();
+	update_zoom_label();
+	adjust_cutoff_from_updated_gfit();
+	redraw(REMAP_ALL);
+	redraw_previews();
+}
+
+void on_crop_Apply_clicked(GtkButton *button, gpointer user_data) {
+	if (get_thread_run()) {
+		PRINT_ANOTHER_THREAD_RUNNING;
+		return;
+	}
+
+#ifdef HAVE_FFMS2
+	if (com.seq.type == SEQ_AVI) {
+		siril_log_message(_("Crop does not work with "
+				"avi film. Please, convert your file to SER first.\n"));
+		return;
+	}
+#endif
+	if (com.seq.type == SEQ_INTERNAL) {
+		siril_log_message(_("Not a valid sequence for cropping.\n"));
+	}
+
+	struct crop_sequence_data *args = malloc(sizeof(struct crop_sequence_data));
+
+	GtkEntry *cropped_entry = GTK_ENTRY(lookup_widget("cropped_entry"));
+
+	args->seq = &com.seq;
+	memcpy(&args->area, &com.selection, sizeof(rectangle));
+	args->prefix = gtk_entry_get_text(cropped_entry);
+
+	set_cursor_waiting(TRUE);
+	crop_sequence(args);
+}
+
+void on_crop_close_clicked(GtkButton *button, gpointer user_data) {
+	siril_close_dialog("crop_dialog");
+}
+
