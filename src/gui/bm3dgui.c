83356d0b2cbe54384625ef8bc16543c3ffcf534e
Adrian Knagg-Baugh <aje.baugh@gmail.com>
2022-09-26 10:16:45 +0100

    Restored undo functionality for the GUI

--- src/gui/bm3dgui.c
+++ src/gui/bm3dgui.c
@@ -9,6 +9,7 @@
 #include "gui/siril_preview.h"
 #include "gui/dialogs.h"
 #include "core/processing.h"
+#include "core/undo.h"
 #include "core/command.h"
 #include "io/single_image.h"
 
@@ -137,23 +138,51 @@ void on_denoise_apply_clicked(GtkButton *button, gpointer user_data) {
 		free(args);
 		return;
 	}
-	siril_log_message(_("Modulation: %f\n"),args->modulation);
-	if (args->do_cosme)
-		siril_log_message(_("Will apply salt and pepper noise removal.\n"));
+	char *msg1 = NULL, *msg2 = NULL, *msg3 = NULL, *log_msg = NULL;
+	int n = 0, m = 0, q = 0;
+	n = snprintf(NULL, 0, _("NL-Bayes denoise (mod=%.3f"), args->modulation);
+	msg1 = malloc(n + 1);
+	n = snprintf(msg1, n + 1, _("NL-Bayes denoise (mod=%.3f"), args->modulation);
+	if(args->da3d) {
+		m = snprintf(NULL, 0, _(", DA3D enabled"));
+		msg2 = malloc(m + 1);
+		m = snprintf(msg2, m + 1, _(", DA3D enabled"));
+	} else if (args->sos > 1) {
+		m = snprintf(NULL, 0, _(", SOS enabled (iters=%d, rho=%.3f)"), args->sos, args->rho);
+		msg2 = malloc(m + 1);
+		m = snprintf(msg2, m + 1, _(", SOS enabled (iters=%d, rho=%.3f)"), args->sos, args->rho);
+	} else if (args->do_anscombe) {
+		m = snprintf(NULL, 0, _(", VST enabled"));
+		msg2 = malloc(m + 1);
+		m = snprintf(msg2, m + 1, _(", VST enabled"));
+	}
+	if (args->do_cosme) {
+		q = snprintf(NULL, 0, _(", CC enabled)"));
+		msg3 = malloc(q + 1);
+		q = snprintf(msg3, q + 1, _(", CC enabled)"));
+	} else {
+		q = 1;
+		msg3 = malloc(q + 1);
+		q = snprintf(msg3, q + 1, _(")"));
+	}
+	log_msg = malloc(n + m + q + 1);
+	if (m == 0 && q == 0)
+		snprintf(log_msg, n + 1, "%s", msg1);
+	else if (m > 0 && q == 0)
+		snprintf(log_msg, n + m + 1, "%s%s", msg1, msg2);
+	else if(m == 0 && q > 0)
+		snprintf(log_msg, n + q + 1, "%s%s", msg1, msg3);
+	else if (m > 0 && q > 0)
+		snprintf(log_msg, n + m + q + 1, "%s%s%s", msg1, msg2, msg3);
 	else
-		siril_log_message(_("Salt and pepper noise removal disabled.\n"));
-	if (args->do_anscombe)
-		siril_log_message(_("Will apply Anscombe variance stabilising transform.\n"));
-	else
-		siril_log_message(_("Anscombe variance stabilising transform disabled.\n"));
-	if (args->da3d)
-		siril_log_message(_("Will apply final stage DA3D denoising.\n"));
-	else
-		siril_log_message(_("Final stage DA3D denoising disabled.\n"));
-	if (sos)
-		siril_log_message(_("Will apply SOS iterative denoise booster.\n"));
-	else
-		siril_log_message(_("SOS denoise booster disabled.\n"));
+		snprintf(log_msg, 26, "Error, this can't happen!");
+
+	if (msg1) free(msg1);
+	if (msg2) free(msg2);
+	if (msg3) free(msg3);
+	undo_save_state(&gfit, "%s", log_msg);
+	free(log_msg);
+
 	control_window_switch_to_tab(OUTPUT_LOGS);
 	start_in_new_thread(run_nlbayes_on_fit, args);
 	siril_close_dialog("denoise_dialog");
