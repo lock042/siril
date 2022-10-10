0135a2beeb498e49d1f6038e1b60d3a20760d0b0
Adrian Knagg-Baugh <aje.baugh@gmail.com>
2022-10-10 13:36:34 +0100

    Improvements to clamping - now works for 16bit and 32bit images but note the odd brightness increase on repeated operations

--- src/opencv/opencv.cpp
+++ src/opencv/opencv.cpp
@@ -482,61 +482,35 @@ int cvTransformImage(fits *image, unsigned int width, unsigned int height, Homog
 	// OpenCV function
 	warpPerspective(in, out, H, Size(target_rx, target_ry), interpolation, BORDER_TRANSPARENT);
 	if ((interpolation == OPENCV_LANCZOS4 || interpolation == OPENCV_CUBIC) && clamp) {
-		Mat guide, tmp1, tmp2;
+		Mat guide, guide16, tmp1, tmp2;
 		// Create guide image
 		warpPerspective(in, guide, H, Size(target_rx, target_ry), OPENCV_AREA, BORDER_TRANSPARENT);
 		// Compare the two, replace out pixels with guide pixels if too far out
-		Sobel(guide, tmp1, CV_16U, 2, 0, 3, 3, 1, BORDER_DEFAULT);
-		Sobel(guide, tmp2, CV_16U, 0, 2, 3, 3, 1, BORDER_DEFAULT);
+		if (guide.type() == CV_16UC1 || guide.type() == CV_16UC3) {
+			Sobel(guide, tmp1, guide.type(), 2, 0, 3, 3, 1, BORDER_DEFAULT);
+			Sobel(guide, tmp2, guide.type(), 0, 2, 3, 3, 1, BORDER_DEFAULT);
+		} else {
+			Mat guide16;
+			guide.convertTo(guide16, CV_16U, 65535);
+			Sobel(guide16, tmp1, guide16.type(), 2, 0, 3, 3, 1, BORDER_DEFAULT);
+			Sobel(guide16, tmp2, guide16.type(), 0, 2, 3, 3, 1, BORDER_DEFAULT);
+			guide16.release();
+		}
 		tmp1 = (tmp1 / 2 + tmp2 / 2);
 		// tmp2 is the mask for the areas of high gradient change
 		tmp2 = (tmp1 > 4000);
-
-		// Detect neighbourhoods around undershot pixels
-
-// Disabled at the moment, not this means the GUI parameter currently does
-// nothing. It might be useful to tune the gradient mask cutoff though
-
-//		compare(out, (guide * clamping_factor), tmp1, CMP_LT);
-//		dilate(tmp1, tmp1, Mat(), Point(-1,-1),1);
-		// And with the Sobel mask, this makes tmp2 the mask for the guide data
-//		bitwise_and(tmp1, tmp2, tmp2);
-
-		// This makes tmp1 the mask for the original lanczos4 data
+		tmp2.convertTo(tmp2, CV_8U);
 		bitwise_not(tmp2, tmp1);
-		// Convert masks to the same bitformat as the data
-		switch(out.type()) {
-			case CV_16UC1:
-				tmp1.convertTo(tmp1, CV_16UC1, 65535);
-				tmp2.convertTo(tmp2, CV_16UC1, 65535);
-				bitwise_and(tmp2, guide, guide);
-				bitwise_and(tmp1, out, out);
-				break;
-			case CV_16UC3:
-				tmp1.convertTo(tmp1, CV_16UC3, 65535);
-				tmp2.convertTo(tmp2, CV_16UC3, 65535);
-				bitwise_and(tmp2, guide, guide);
-				bitwise_and(tmp1, out, out);
-				break;
-			case CV_32FC1:
-				tmp1.convertTo(tmp1, CV_32FC1);
-				tmp2.convertTo(tmp2, CV_32FC1);
-				guide = guide.mul(tmp2);
-				out = out.mul(tmp1);
-				break;
-			case CV_32FC3:
-				tmp1.convertTo(tmp1, CV_32FC3);
-				tmp2.convertTo(tmp2, CV_32FC3);
-				guide = guide.mul(tmp2);
-				out = out.mul(tmp1);
-				break;
-		}
+
+		copyTo(guide, out, tmp2);
+		copyTo(out, out, tmp1);
+
 		out = out + guide;
-		if (out.type() == CV_32FC1 || out.type() == CV_32FC3) {
+/*		if (out.type() == CV_32FC1 || out.type() == CV_32FC3) {
 			double min, max;
 			minMaxIdx(out, &min, &max);
 			out = out / max;
-		}
+		} */
 		guide.release();
 		tmp1.release();
 		tmp2.release();
