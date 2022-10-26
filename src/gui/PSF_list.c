57289a83b6cdddea6b2ef401e685393e8b407401
Cyril Richard <cyril.richard42@gmail.com>
2022-10-26 15:15:09 +0200

    Cleaning a bit

--- src/gui/PSF_list.c
+++ src/gui/PSF_list.c
@@ -366,14 +366,13 @@ void set_iter_of_clicked_psf(double x, double y) {
 		if (distsq < psflimsq) {
 			gtk_tree_selection_select_iter(selection, &iter);
 			GtkTreePath *path = gtk_tree_model_get_path(model, &iter);
+			if (!path) return;
 			gtk_tree_selection_select_path(selection, path);
 			gtk_tree_view_scroll_to_cell(treeview, path, NULL, TRUE, 0.5, 0.0);
+			gtk_tree_path_free(path);
 			gui.selected_star = get_index_of_selected_star(xpos, ypos);
 			display_status();
 			redraw(REDRAW_OVERLAY);
-			if (path) {
-				gtk_tree_path_free(path);
-			}
 			return;
 		}
 		valid = gtk_tree_model_iter_next(model, &iter);
