9710c7e5a5dd03b26c40f7b93c3371dcb0a2c112
cissou8 <cissou8@gmail.com>
2022-11-10 23:38:01 +0100

    add seqfixbanding command

--- src/core/command.h
+++ src/core/command.h
@@ -129,6 +129,7 @@ int	process_seq_extractHa(int nb);
 int	process_seq_extractGreen(int nb);
 int	process_seq_extractHaOIII(int nb);
 int	process_seq_findstar(int nb);
+int process_seq_fixbanding(int nb);
 int	process_seq_header(int nb);
 int	process_seq_mtf(int nb);
 int	process_seq_psf(int nb);
