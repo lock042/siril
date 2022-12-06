lock042/siril

commit 0ba9047382eb466ea7ece8604aa4c81898c15449

Fixing issue #951

src/core/command.c
@@ -6594,8 +6594,16 @@ struct preprocessing_data *parse_preprocess_args(int nb, sequence *seq) {
 				args->cc_from_dark = TRUE;
 				//either we use the default sigmas or we try to read the next two checking for sigma low and high
 				gchar *end1, *end2;
-				if (word[i + 1] && word[i + 2] && ((args->sigma[0] = g_ascii_strtod(word[i + 1], &end1)) < 0.0 && end1 != word[i + 1])
-						&& (args->sigma[1] = g_ascii_strtod(word[i + 2], &end2)) < 0.0 && end2 != word[i + 2]) {
+
+				if (word[i + 1] && word[i + 2]) {
+					args->sigma[0] = g_ascii_strtod(word[i + 1], &end1);
+					args->sigma[1] = g_ascii_strtod(word[i + 2], &end2);
+
+					if (end1 == word[i + 1] || end2 == word[i + 2]) {
+						args->sigma[0] = 0;
+						args->sigma[1] = 0;
+						continue;
+					}
 					i+= 2;
 				}
 				if (args->sigma[0] == 0) args->sigma[0] = -1.00;

commit 8de6d9f39c4025d05c390172cb9c5bfa6ad27e0c

Fixing bug introduced by 0ba90473

src/core/command.c
@@ -6631,18 +6631,14 @@ struct preprocessing_data *parse_preprocess_args(int nb, sequence *seq) {
 				args->cc_from_dark = TRUE;
 				//either we use the default sigmas or we try to read the next two checking for sigma low and high
 				gchar *end1, *end2;
+				args->sigma[0] = g_ascii_strtod(word[i + 1], &end1);
+				args->sigma[1] = g_ascii_strtod(word[i + 2], &end2);
 
-				if (word[i + 1] && word[i + 2]) {
-					args->sigma[0] = g_ascii_strtod(word[i + 1], &end1);
-					args->sigma[1] = g_ascii_strtod(word[i + 2], &end2);
-
-					if (end1 == word[i + 1] || end2 == word[i + 2]) {
-						args->sigma[0] = 0;
-						args->sigma[1] = 0;
-						continue;
-					}
+				if (word[i + 1] && word[i + 2] && word[i + 1] != end1
+						&& word[i + 2] != end2) {
 					i+= 2;
 				}
+
 				if (args->sigma[0] == 0) args->sigma[0] = -1.00;
 				if (args->sigma[1] == 0) args->sigma[1] = -1.00;
 				if (args->sigma[0] > 0)