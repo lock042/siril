06036913132b3420f6048355411cb7ea3f49e4c9
aje.baugh <aje.baugh@gmail.com>
2023-01-19 12:26:43 +0000

    Correct copyright details for 2 files, typo

--- src/filters/deconvolution/utils.hpp
+++ src/filters/deconvolution/utils.hpp
@@ -1,10 +1,25 @@
 /*
+MIT License
 
-From https://ipolcore.ipol.im/demo/clientApp/demo.html?id=77777000086
-Anger, Jérémy, Mauricio Delbracio, and Gabriele Facciolo. "Efficient Blind Deblurring under High Noise Levels.", Published at International Symposium on Image and Signal Processing and Analysis (ISPA 2019).
+Copyright (c) 2018 Jérémy Anger, Gabriele Facciolo, Mauricio Delbracio
 
-Licenced under the GNU AFFERO GENERAL PUBLIC LICENSE, Version 3, 19 November 2007
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 */
 #pragma once
 
