298256aa46481e27123d7dd18d4f65c2d9955049
aje.baugh <aje.baugh@gmail.com>
2023-02-02 22:45:12 +0000

    Removed some debugging code from the generic sequence worker

--- src/core/processing.c
+++ src/core/processing.c
@@ -158,7 +158,6 @@ gpointer generic_sequence_worker(gpointer p) {
 	if (have_seqwriter)
 		omp_set_schedule(omp_sched_dynamic, 1);
 	else omp_set_schedule(omp_sched_guided, 0);
-	printf("number of frames: %d\n", nb_frames);
 #ifdef HAVE_FFMS2
 	// we don't want to enable parallel processing for films, as ffms2 is not thread-safe
 #pragma omp parallel for num_threads(args->max_parallel_images) private(input_idx) schedule(runtime) \
@@ -276,14 +275,12 @@ gpointer generic_sequence_worker(gpointer p) {
 			if (args->save_hook)
 				retval = args->save_hook(args, frame, input_idx, fit);
 			else retval = generic_save(args, frame, input_idx, fit);
-			printf("retval from save hook: %d\n", retval);
 			if (retval) {
 				abort = 1;
 				clearfits(fit);
 				free(fit);
 				continue;
 			}
-			printf("cleared the retval check\n");
 
 		} else {
 			/* save stats that may have been computed for the first
