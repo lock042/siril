46f996fa580cb636f8b7f66ddbecd8ad9898efa5
Cyril Richard <cyril.richard42@gmail.com>
2023-02-27 15:25:31 +0100

    Updating some progressbar

--- src/algos/photometric_cc.c
+++ src/algos/photometric_cc.c
@@ -482,7 +482,7 @@ gpointer photometric_cc_standalone(gpointer p) {
 	free(stars);
 	args = NULL;
 
-	set_progress_bar_data("", PROGRESS_RESET);
+	set_progress_bar_data(PROGRESS_TEXT_RESET, PROGRESS_RESET);
 
 	if (!retval && image_is_gfit) {
 		siril_log_color_message(_("Photometric Color Calibration suceeded.\n"), "green");
