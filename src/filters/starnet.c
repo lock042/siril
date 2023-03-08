cf53ce29df427f70136e0ba47a7964d8add60e75
aje.baugh <aje.baugh@gmail.com>
2023-03-08 12:24:17 +0000

    Remove the G_SPAWN_STDERR_TO_DEV_NULL flag from starnet_executablecheck()

--- src/filters/starnet.c
+++ src/filters/starnet.c
@@ -207,7 +207,7 @@ starnet_version starnet_executablecheck(gchar* executable) {
 	// g_spawn handles wchar so not need to convert
 	g_spawn_async_with_pipes(NULL, test_argv, NULL,
 			G_SPAWN_DO_NOT_REAP_CHILD | G_SPAWN_SEARCH_PATH |
-			G_SPAWN_LEAVE_DESCRIPTORS_OPEN | G_SPAWN_STDERR_TO_DEV_NULL,
+			G_SPAWN_LEAVE_DESCRIPTORS_OPEN,
 			NULL, NULL, &child_pid, NULL, &child_stdout,
 			NULL, &error);
 
