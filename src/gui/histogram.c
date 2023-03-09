1ad29e00e52585df56728deb43070366247eaa54
aje.baugh <aje.baugh@gmail.com>
2023-03-09 13:03:01 +0000

    Add missing include

--- src/gui/histogram.c
+++ src/gui/histogram.c
@@ -32,6 +32,7 @@
 #include "io/image_format_fits.h"
 #include "io/sequence.h"
 #include "gui/image_display.h"
+#include "gui/callbacks.h"
 #include "gui/utils.h"	// for lookup_widget()
 #include "gui/progress_and_log.h"
 #include "gui/dialogs.h"
