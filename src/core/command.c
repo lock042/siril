836acdf43c0c595aa8fbcd386992c6e0b1a0c5a4
Cyril Richard <cyril.richard42@gmail.com>
2023-05-22 22:07:32 +0200

    Fixing small error

--- src/core/command.c
+++ src/core/command.c
@@ -7986,7 +7986,7 @@ int process_capabilities(int nb) {
 	siril_log_message("Can read and write TIFF and Astro-TIFF files\n");
 #endif
 #ifdef HAVE_LIBXISF
-	siril_log_message("Can read and write XISF files\n");
+	siril_log_message("Can read XISF files\n");
 #endif
 #ifdef HAVE_LIBHEIF
 	siril_log_message("Can read HEIF files\n");
