ae0de8bcddb3cd4924e38c14d1c8e7b9feabe91f
Adrian Knagg-Baugh <aje.baugh@gmail.com>
2023-05-30 15:41:21 +0000

    Update 2 files
    - /src/algos/lcms_fast_float/lcms2_fast_float.h
    - /meson.build

--- src/algos/lcms_fast_float/lcms2_fast_float.h
+++ src/algos/lcms_fast_float/lcms2_fast_float.h
@@ -22,9 +22,6 @@
 #ifndef _LCMS2_FAST_FLOAT_H
 #define _LCMS2_FAST_FLOAT_H
 
-#ifdef HAVE_CONFIG_H
-#include "config.h"
-#endif
 #include "lcms2_plugin.h"
 
 /* There is an issue with version 2.15 on Windows platforms: some symbols
