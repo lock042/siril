ade10c33b8e2ad09c665bb401276f9a32976aad3
cissou8 <cissou8@gmail.com>
2023-07-14 17:15:49 +0200

    replace formatting of y axis for light curves

--- src/algos/photometry.c
+++ src/algos/photometry.c
@@ -456,6 +456,7 @@ int new_light_curve(sequence *seq, const char *filename, const char *target_desc
 			init_siril_plot_data(spl_data);
 			siril_plot_set_title(spl_data, title);
 			siril_plot_set_xlabel(spl_data, xlabel);
+			siril_plot_set_yfmt(spl_data, "%g");
 			spl_data->revertY = TRUE;
 			double *date0 = malloc(nb_valid_images * sizeof(double));
 			for (int i = 0; i < nb_valid_images; i++)
