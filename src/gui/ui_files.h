71a2c85f7201b4f13102cc9e4632b4383ecfde85
aje.baugh <aje.baugh@gmail.com>
2023-11-10 23:22:30 +0000

    More progress
    All GtkDialogs have their own UI file, also the filefilters which
    need to be loaded first.

--- src/gui/ui_files.h
+++ src/gui/ui_files.h
@@ -2,6 +2,7 @@
 #define UI_FILES_H
 
 const char* ui_files[] = {
+	"filefilters.ui",
 	"cosmetic_dialog.ui",
 	"binxy_dialog.ui",
 	"CLAHE_dialog.ui",
@@ -17,7 +18,33 @@ const char* ui_files[] = {
 	"background_extraction_dialog.ui",
 	"savepopup.ui",
 	"satu_dialog.ui",
+	"SCNR_dialog.ui",
+	"bdeconv_dialog.ui",
+	"crop_dialog.ui",
+	"cut_spectroscopy_dialog.ui",
+	"denoise_dialog.ui",
+	"extract_channel_dialog.ui",
+	"file_information_dialog.ui",
+	"composition_dialog.ui",
+	"dialog_FFT.ui",
+	"dialog_star_remix.ui",
+	"linearmatch_dialog.ui",
+	"icc_dialog.ui",
+	"icc_gamut_dialog.ui",
+	"IPS_dialog.ui",
+	"StatWindow.ui",
+	"stars_list_window.ui",
+	"merge_cfa_dialog.ui",
+	"histogram_dialog.ui",
+	"resample_dialog.ui",
+	"rotation_dialog.ui",
+	"script_contents_dialog.ui",
+	"split_cfa_dialog.ui",
+	"color_calibration.ui",
+	"settings_window.ui",
 	"siril.ui",
+	"data_dialog.ui",
+
 // Add entries here as required
 	"" // Must be terminated by a NULL string
 };
