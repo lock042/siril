056a59479c7780f3c49019416e9cef8ae63dd639
aje.baugh <aje.baugh@gmail.com>
2023-11-10 15:11:20 +0000

    Work in progress splitting the glade file

--- src/gui/ui_files.h
+++ src/gui/ui_files.h
@@ -2,7 +2,22 @@
 #define UI_FILES_H
 
 const char* ui_files[] = {
-	"siril3.glade",
+	"cosmetic_dialog.ui",
+	"binxy_dialog.ui",
+	"CLAHE_dialog.ui",
+	"cut_dialog.ui",
+	"cut_coords_dialog.ui",
+	"extract_wavelet_layers_dialog.ui",
+	"canon_fixbanding_dialog.ui",
+	"Median_dialog.ui",
+	"rgradient_dialog.ui",
+	"starnet_dialog.ui",
+	"wavelets_dialog.ui",
+	"asinh_dialog.ui",
+	"background_extraction_dialog.ui",
+	"savepopup.ui",
+	"satu_dialog.ui",
+	"siril.ui",
 // Add entries here as required
 	"" // Must be terminated by a NULL string
 };
