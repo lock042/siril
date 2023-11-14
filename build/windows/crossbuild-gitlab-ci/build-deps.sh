0479b3abbd06efa2215b9cc16967c4ae156ed625
cissou8 <cissou8@gmail.com>
2023-11-14 20:39:37 +0100

    yet another win build test

--- build/windows/crossbuild-gitlab-ci/build-deps.sh
+++ build/windows/crossbuild-gitlab-ci/build-deps.sh
@@ -12,8 +12,6 @@ crossroad install lcms2 \
                   ffms2 \
                   cfitsio \
                   libgit2 \
-                  glib2 \
-                  glib2-devel
 # need to uninstall crt-git
 # probably same root cause as https://github.com/msys2/MINGW-packages/issues/10837
 # otherwise, it's messing up all the subsequent builds
