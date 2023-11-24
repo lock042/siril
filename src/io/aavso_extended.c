cf619be5d8703ce903e06723c85d11671a8610c9
Cyril Richard <cyril.richard42@gmail.com>
2023-11-24 15:01:51 +0100

    Cleaning code

--- src/io/aavso_extended.c
+++ src/io/aavso_extended.c
@@ -51,109 +51,93 @@ static void generate_aavso_header(const aavso_parameters *data, char *result) {
 }
 
 // save the data to a txt file
-static gboolean siril_plot_save_aavso(siril_plot_data *spl_data, const char *datfilename, const char *head_data, aavso_data *adata) {
-    GString *header = NULL;
-    FILE *fileout = NULL;
-    gboolean retval = TRUE;
+static gboolean siril_plot_save_aavso(siril_plot_data *spl_data,
+		const char *datfilename, const char *head_data, aavso_data *adata) {
+	GString *header = g_string_new(head_data);
+	gboolean retval = TRUE;
 
-    if (g_list_length(spl_data->plots) > 1) {
-        siril_debug_print("Light curve should not hold more than one data series, aborting\n");
-        return FALSE;
-    }
+	if (g_list_length(spl_data->plots) > 1) {
+		siril_debug_print("AAVSO should not hold more than one data series, aborting\n");
+		g_string_free(header, TRUE);
+		return FALSE;
+	}
 
-    int nbpoints = 0, nbcols = 15; // aavso has 15 columns
+	int nbcols = 15; // aavso has 15 columns
 
-    header = g_string_new(head_data);
-
-	// Check if the data series is available
 	if (spl_data->plots != NULL && spl_data->plot) {
 		splxyerrdata *plots = (splxyerrdata*) spl_data->plots->data;
-		nbpoints = plots->nb;
+		int nbpoints = plots->nb;
 
 		GList *list = spl_data->plot;
 
-		/* Get cstar data */
 		splxydata *cplot = (splxydata*) list->data;
-		if (nbpoints != cplot->nb) {
-			retval = FALSE;
-			goto clean_and_exit;
-		}
-		/* move to kstar data */
-		list = list->next;
-		splxydata *kplot = (splxydata*) list->data;
-		if (nbpoints != kplot->nb) {
-			retval = FALSE;
-			goto clean_and_exit;
+		splxydata *kplot = (splxydata*) list->next->data;
+		splxydata *airmass = (splxydata*) list->next->next->data;
+
+		if (nbpoints != cplot->nb || nbpoints != kplot->nb
+				|| nbpoints != airmass->nb) {
+			g_string_free(header, TRUE);
+			return FALSE;
 		}
 
-		/* move to airmass data */
-		list = list->next;
-		splxydata *airmass = (splxydata*) list->data;
-		if (nbpoints != airmass->nb) {
-			retval = FALSE;
-			goto clean_and_exit;
-		}
-
-		// Allocate memory for data array
 		gchar **data = g_new(gchar*, nbpoints * nbcols);
 		if (!data) {
 			PRINT_ALLOC_ERR;
+			g_string_free(header, TRUE);
+			return FALSE;
+		}
+
+		int index = 0;
+		for (int i = 0; i < nbpoints; i++) {
+			data[index++] = g_strdup(adata->starid); // NAME
+			data[index++] = g_strdup_printf("%12.6lf",
+					plots->plots[0]->data[i].x); // DATE
+			for (int k = 0; k < 2; k++) // MAG, MAGERR
+				data[index++] = g_strdup_printf("%8.6lf", plots->plots[k]->data[i].y);
+			data[index++] = g_strdup(adata->filter); // FILT
+			data[index++] = g_strdup("NO"); // TRANS
+			data[index++] = g_strdup("STD"); // MTYPE
+			data[index++] = g_strdup(adata->cname); // CNAME
+			data[index++] = g_strdup_printf("%8.6lf", cplot->data[i].y); // CMAG
+			data[index++] = g_strdup(adata->kname); // KNAME
+			data[index++] = g_strdup_printf("%8.6lf", kplot->data[i].y); // KMAG
+			data[index++] = g_strdup_printf("%.3lf", airmass->data[i].y); // AMASS
+			data[index++] = g_strdup(_NA_); // GROUP
+			data[index++] = g_strdup(adata->chart); // CHART
+			data[index++] = g_strdup(adata->notes); // NOTES
+		}
+
+		FILE *fileout = g_fopen(datfilename, "w");
+		if (fileout == NULL) {
+			siril_log_message(_("Could not create %s, aborting\n"), datfilename);
 			retval = FALSE;
-            goto clean_and_exit;
-        }
+		} else {
+			fprintf(fileout, "%s", header->str);
+			index = 0;
+			for (int r = 0; r < nbpoints; r++) {
+				fprintf(fileout, "\n%s", data[index++]); // print newline and x
+				for (int c = 1; c < nbcols; c++) {
+					if (index < nbpoints * nbcols) {
+						fprintf(fileout, ",%s", data[index++]);
+					}
+				}
+			}
 
-        // Fill the data array
-        int index = 0;
-        for (int i = 0; i < nbpoints; i++) {
-            data[index++] = g_strdup(adata->starid); // NAME
-            data[index++] = g_strdup_printf("%12.6lf", plots->plots[0]->data[i].x); // DATE
-            for (int k = 0; k < 2; k++) // MAG, MAGERR
-                data[index++] = g_strdup_printf("%8.6lf", plots->plots[k]->data[i].y);
-            data[index++] = g_strdup(adata->filter); // FILT
-            data[index++] = g_strdup("NO"); // TRANS
-            data[index++] = g_strdup("STD"); // MTYPE
-            data[index++] = g_strdup(adata->cname); // CNAME
-            data[index++] = g_strdup_printf("%8.6lf", cplot->data[i].y); // CMAG
-            data[index++] = g_strdup(adata->kname); // KNAME
-            data[index++] = g_strdup_printf("%8.6lf", kplot->data[i].y); // KMAG
-            data[index++] = g_strdup_printf("%.3lf", airmass->data[i].y); // AMASS
-            data[index++] = g_strdup(_NA_); // GROUP
-            data[index++] = g_strdup(adata->chart); // CHART
-            data[index++] = g_strdup(adata->notes); // NOTES
-        }
+			fclose(fileout);
+			siril_log_message(_("%s has been saved.\n"), datfilename);
+		}
 
-        // Write data to the file
-        fileout = g_fopen(datfilename, "w");
-        if (fileout == NULL) {
-            siril_log_message(_("Could not create %s, aborting\n"), datfilename);
-            retval = FALSE;
-            goto clean_and_exit;
-        }
+		// Free memory
+		g_string_free(header, TRUE);
+		for (int i = 0; i < nbpoints * nbcols; i++) {
+			g_free(data[i]);
+		}
+		g_free(data);
+	}
 
-        fprintf(fileout, "%s", header->str);
-        index = 0;
-        for (int r = 0; r < nbpoints; r++) {
-            fprintf(fileout, "\n%s", data[index++]); // print newline and x
-            for (int c = 1; c < nbcols; c++)
-                fprintf(fileout, ",%s", data[index++]);
-        }
-
-        fclose(fileout);
-        siril_log_message(_("%s has been saved.\n"), datfilename);
-
-clean_and_exit:
-        // Free memory
-        g_string_free(header, TRUE);
-        if (data != NULL) {
-            for (int i = 0; i < nbpoints * nbcols; i++) {
-                g_free(data[i]);
-            }
-            g_free(data);
-        }
-    }
-
-    return retval;
+	return retval;
 }
+
 
 gboolean export_to_aavso_extended(siril_plot_data *data, aavso_dlg *aavso_ptr, const char *datfilename) {
 	aavso_parameters header;
