56692d2156dc83676c68ba1efa7729bd9b8ad1c0
FRed_Shift <f4eop@wanadoo.fr>
2024-02-06 15:06:53 +0000

    UI simplified

--- src/gui/compstars.c
+++ src/gui/compstars.c
@@ -26,6 +26,7 @@
 #include "algos/comparison_stars.h"
 #include "gui/message_dialog.h"
 #include "gui/utils.h"
+#include "gui/PSF_list.h"
 
 static GtkWidget *dialog = NULL;	// the window, a GtkDialog
 static GtkWidget *delta_vmag_entry = NULL;
@@ -34,8 +35,17 @@ static GtkWidget *emag_entry = NULL;
 static GtkWidget *target_entry = NULL;
 static GtkWidget *apass_radio = NULL;
 static GtkWidget *check_narrow = NULL;
+static GtkWidget *auto_mode, *mode_grp, *manual_mode;
+static GtkWidget *auto_data_grp;
 
 static void on_compstars_response(GtkDialog* self, gint response_id, gpointer user_data);
+
+static void output_state (GtkToggleButton *source, gpointer user_data) {
+	if (gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(manual_mode)))
+		gtk_widget_set_sensitive (auto_data_grp, FALSE);
+	if (gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(auto_mode)))
+		gtk_widget_set_sensitive (auto_data_grp, TRUE);
+}
 
 static void build_the_dialog() {
 	dialog = gtk_dialog_new_with_buttons(_("Create a comparison stars list"), NULL,
@@ -47,40 +57,72 @@ static void build_the_dialog() {
 	g_signal_connect(G_OBJECT(dialog), "delete-event", G_CALLBACK(gtk_widget_hide_on_delete), NULL);
 	g_signal_connect(G_OBJECT(dialog), "response", G_CALLBACK(on_compstars_response), NULL);
 
-	GtkWidget *label = gtk_label_new(_("Find comparison stars for the currently loaded image and the given star name"));
-	gtk_label_set_line_wrap(GTK_LABEL(label), TRUE);
-	g_object_set(G_OBJECT(label), "margin", 15, NULL);
 
+
+
+
+	/* Mode (Auto/Manu) choice */
+	mode_grp = gtk_box_new(GTK_ORIENTATION_VERTICAL, 2);
+	gtk_box_set_homogeneous(GTK_BOX(mode_grp), TRUE);
+	gtk_widget_set_tooltip_text(mode_grp, _("Toggle Manual mode or Auromatic mode for Comparison stars list"));
+
+	manual_mode = gtk_radio_button_new_with_label_from_widget(NULL, _("Use the stars selected in the currently loaded image"));
+	g_signal_connect (manual_mode, "toggled",G_CALLBACK (output_state), NULL);
+	gtk_container_add(GTK_CONTAINER(mode_grp), manual_mode);
+
+	auto_mode = gtk_radio_button_new_with_label_from_widget (GTK_RADIO_BUTTON(manual_mode), _("Find comparison stars from catalogue requests"));
+	g_signal_connect (auto_mode, "toggled",G_CALLBACK (output_state), NULL);
+	gtk_container_add(GTK_CONTAINER(mode_grp), auto_mode);
+
+	gtk_widget_set_halign(mode_grp, GTK_ALIGN_START);
+	g_object_set(G_OBJECT(mode_grp), "margin-left", 15, NULL);
+	g_object_set(G_OBJECT(mode_grp), "margin-top", 20, NULL);
+	g_object_set(G_OBJECT(mode_grp), "margin-bottom", 20, NULL);
+
+	// Defines the group for the auto mode parameters
+	auto_data_grp = gtk_box_new(GTK_ORIENTATION_VERTICAL, 9);
+	gtk_box_set_homogeneous(GTK_BOX(auto_data_grp), TRUE);
+	gtk_widget_set_tooltip_text(auto_data_grp, _("Variable star data and sorting parameters for catalogue request"));
+
+	// Defines the parameters of the automatic mode
 	target_entry = gtk_entry_new();
 	gtk_entry_set_placeholder_text(GTK_ENTRY(target_entry), "Target star name");
 	gtk_widget_set_tooltip_text(target_entry, _("Enter the target star name to search in cataloges"));
 	g_object_set(G_OBJECT(target_entry), "margin", 15, NULL);
+	g_object_set(G_OBJECT(target_entry), "margin-top", 0, NULL);
+	g_object_set(G_OBJECT(target_entry), "margin-bottom", 0, NULL);
+	gtk_container_add(GTK_CONTAINER(auto_data_grp), target_entry);
 
 	check_narrow = gtk_check_button_new_with_label(_("Narrow field of view"));
 	gtk_widget_set_tooltip_text(check_narrow, _("Tick this box to use a narrow field of view centered about the target star"));
 	gtk_widget_set_halign(check_narrow, GTK_ALIGN_START);
 	g_object_set(G_OBJECT(check_narrow), "margin-left", 15, NULL);
-	g_object_set(G_OBJECT(check_narrow), "margin-top", 5, NULL);
+	g_object_set(G_OBJECT(check_narrow), "margin-top", 0, NULL);
 	g_object_set(G_OBJECT(check_narrow), "margin-bottom", 0, NULL);
+	gtk_container_add(GTK_CONTAINER(auto_data_grp), check_narrow);
 
 	GtkWidget *labelvmag = gtk_label_new(_("Allowed visual magnitude range:"));
 	gtk_widget_set_halign(labelvmag, GTK_ALIGN_START);
 	g_object_set(G_OBJECT(labelvmag), "margin-left", 15, NULL);
-	g_object_set(G_OBJECT(labelvmag), "margin-top", 10, NULL);
+	g_object_set(G_OBJECT(labelvmag), "margin-top", 0, NULL);
 	g_object_set(G_OBJECT(labelvmag), "margin-bottom", 0, NULL);
-
+	gtk_container_add(GTK_CONTAINER(auto_data_grp), labelvmag);
+	
 	delta_vmag_entry = gtk_entry_new();
 	gtk_entry_set_text(GTK_ENTRY(delta_vmag_entry), "3.0");
 	gtk_widget_set_tooltip_text(delta_vmag_entry, _("Allowed range of visual magnitude between the target star and the comparison stars"));
 	g_object_set(G_OBJECT(delta_vmag_entry), "margin-left", 15, NULL);
 	g_object_set(G_OBJECT(delta_vmag_entry), "margin-right", 15, NULL);
 	g_object_set(G_OBJECT(delta_vmag_entry), "margin-top", 0, NULL);
+	g_object_set(G_OBJECT(delta_vmag_entry), "margin-bottom", 0, NULL);
+	gtk_container_add(GTK_CONTAINER(auto_data_grp), delta_vmag_entry);
 
 	GtkWidget *labelbv = gtk_label_new(_("Allowed B-V index range:"));
 	gtk_widget_set_halign(labelbv, GTK_ALIGN_START);
 	g_object_set(G_OBJECT(labelbv), "margin-left", 15, NULL);
 	g_object_set(G_OBJECT(labelbv), "margin-top", 10, NULL);
 	g_object_set(G_OBJECT(labelbv), "margin-bottom", 0, NULL);
+	gtk_container_add(GTK_CONTAINER(auto_data_grp), labelbv);
 
 	delta_bv_entry = gtk_entry_new();
 	gtk_entry_set_text(GTK_ENTRY(delta_bv_entry), "0.5");
@@ -88,12 +130,15 @@ static void build_the_dialog() {
 	g_object_set(G_OBJECT(delta_bv_entry), "margin-left", 15, NULL);
 	g_object_set(G_OBJECT(delta_bv_entry), "margin-right", 15, NULL);
 	g_object_set(G_OBJECT(delta_bv_entry), "margin-top", 0, NULL);
+	g_object_set(G_OBJECT(delta_bv_entry), "margin-bottom", 0, NULL);
+	gtk_container_add(GTK_CONTAINER(auto_data_grp), delta_bv_entry);
 
 	GtkWidget *labelemag = gtk_label_new(_("Allowed magnitude error:"));
 	gtk_widget_set_halign(labelemag, GTK_ALIGN_START);
 	g_object_set(G_OBJECT(labelemag), "margin-left", 15, NULL);
-	g_object_set(G_OBJECT(labelemag), "margin-top", 10, NULL);
+	g_object_set(G_OBJECT(labelemag), "margin-top", 0, NULL);
 	g_object_set(G_OBJECT(labelemag), "margin-bottom", 0, NULL);
+	gtk_container_add(GTK_CONTAINER(auto_data_grp), labelemag);
 
 	emag_entry = gtk_entry_new();
 	gtk_entry_set_text(GTK_ENTRY(emag_entry), "0.03");
@@ -101,6 +146,8 @@ static void build_the_dialog() {
 	g_object_set(G_OBJECT(emag_entry), "margin-left", 15, NULL);
 	g_object_set(G_OBJECT(emag_entry), "margin-right", 15, NULL);
 	g_object_set(G_OBJECT(emag_entry), "margin-top", 0, NULL);
+	g_object_set(G_OBJECT(emag_entry), "margin-bottom", 0, NULL);
+	gtk_container_add(GTK_CONTAINER(auto_data_grp), emag_entry);
 
 	/* catalogue choice */
 	GtkWidget *radio2, *radiobox;
@@ -113,21 +160,20 @@ static void build_the_dialog() {
 			_("NOMAD catalogue"));
 	gtk_container_add(GTK_CONTAINER(radiobox), apass_radio);
 	gtk_container_add(GTK_CONTAINER(radiobox), radio2);
-	g_object_set(G_OBJECT(radiobox), "margin", 15, NULL);
+	gtk_container_add(GTK_CONTAINER(auto_data_grp), radiobox);
+	g_object_set(G_OBJECT(radiobox), "margin-left", 15, NULL);
+	g_object_set(G_OBJECT(radiobox), "margin-top", 0, NULL);
+	g_object_set(G_OBJECT(radiobox), "margin-bottom", 0, NULL);
 
+	// Gather the graphic items
 	GtkWidget *content_area = gtk_dialog_get_content_area(GTK_DIALOG(dialog));
 	gtk_box_set_spacing(GTK_BOX(content_area), 15);
-	gtk_container_add(GTK_CONTAINER(content_area), label);
-	gtk_container_add(GTK_CONTAINER(content_area), target_entry);
-	gtk_container_add(GTK_CONTAINER(content_area), check_narrow);
-	gtk_container_add(GTK_CONTAINER(content_area), labelvmag);
-	gtk_container_add(GTK_CONTAINER(content_area), delta_vmag_entry);
-	gtk_container_add(GTK_CONTAINER(content_area), labelbv);
-	gtk_container_add(GTK_CONTAINER(content_area), delta_bv_entry);
-	gtk_container_add(GTK_CONTAINER(content_area), labelemag);
-	gtk_container_add(GTK_CONTAINER(content_area), emag_entry);
-	gtk_container_add(GTK_CONTAINER(content_area), radiobox);
+	gtk_container_add(GTK_CONTAINER(content_area), mode_grp);
+	gtk_container_add(GTK_CONTAINER(content_area), auto_data_grp);
+	gtk_widget_set_sensitive (auto_data_grp, FALSE);
 	gtk_widget_show_all(GTK_WIDGET(content_area));
+
+
 }
 
 // the public getter
@@ -137,59 +183,133 @@ GtkWidget *get_compstars_dialog() {
 	return dialog;
 }
 
+static void manual_photometry_data (sequence *seq) {
+	struct compstars_arg *args = calloc(1, sizeof(struct compstars_arg));
+	siril_catalogue *comp_sta = calloc(1, sizeof(siril_catalogue));
+	cat_item *sel_item = calloc(MAX_SEQPSF + 1, sizeof(cat_item));
+
+	comp_sta->cat_index = CAT_COMPSTARS;
+	double ra, dec;
+	// Gather the selected stars by hand
+	int nb_ref_stars = 0;
+	if (!seq->photometry[0] || !seq->photometry[1]) {
+		siril_log_color_message(_("One Variable star and one comparison star at least are required. Cannot create any file\n"), "salmon");
+		siril_message_dialog(GTK_MESSAGE_ERROR, _("Error"), _("One Variable star and one comparison star at least are required. Cannot create any file"));
+		return;
+	}
+	for (int r = 0; r < MAX_SEQPSF && seq->photometry[r]; r++) {
+		if (get_ra_and_dec_from_star_pos(seq->photometry[r][seq->reference_image], &ra, &dec)) siril_log_color_message(_("Problem with convertion\n"), "salmon"); // PB in the conversion pix->wcs
+		sel_item[r].ra = ra;
+		sel_item[r].dec = dec;
+		nb_ref_stars++;
+	}
+
+	// Header for the console display
+	siril_log_message(_("-> %i comparison stars selected\n"), nb_ref_stars - 1);
+	siril_log_message("Star type        RA      DEC\n");
+	// Preparing the output catalog
+	comp_sta->columns = siril_catalog_columns(CAT_COMPSTARS); // we add mag to write it in the output file (it is not a mandatory field at readout)
+	// Allocating final sorted list to the required size
+	cat_item *result = calloc(nb_ref_stars + 1, sizeof(cat_item));
+	// Write the target star
+	fill_compstar_item(&result[0], sel_item[0].ra, sel_item[0].dec, 0.0, "V", "Target");
+	siril_log_message(_("Target star  : %4.3lf, %+4.3lf\n"),
+			sel_item[0].ra,
+			sel_item[0].dec);
+	// And write the selected comparison stars
+	for (int i = 0; i < nb_ref_stars; i++) {
+		cat_item *item = &sel_item[i];
+		if (i == 0) continue;
+		gchar *name = g_strdup_printf("%d", i);
+		fill_compstar_item(&result[i], item->ra, item->dec, 0.0, name, "Comp1");
+		g_free(name);
+		siril_log_message(_("Comp star %3d: %4.3lf, %+4.3lf\n"),
+				i, 
+				item->ra,
+				item->dec);
+	}
+
+	// Fill the catalogue structure
+	comp_sta->cat_items = result;
+	comp_sta->nbitems = nb_ref_stars;
+	comp_sta->nbincluded = nb_ref_stars;
+	// Fill the other catalogue  structure
+	args->comp_stars = comp_sta;
+	args->nina_file = g_strdup("V_SirilstarList_user.csv");
+	args->target_star = &result[0];
+	args->delta_Vmag = 0.0;		// Explicitely set these three variables
+	args->delta_BV = 0.0;
+	args->max_emag = 0.0;
+	args->cat = CAT_COMPSTARS;
+	// Finally create the csv file
+	write_nina_file(args);
+	// and free the stuff
+	siril_catalog_free (comp_sta);
+	siril_catalog_free_item (sel_item);
+	free(args);
+}
+
 static void on_compstars_response(GtkDialog* self, gint response_id, gpointer user_data) {
+//	auto_manu =gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(auto_mode))
 	siril_debug_print("got response event\n");
 	if (response_id != GTK_RESPONSE_ACCEPT) {
 		gtk_widget_hide(dialog);
 		return;
 	}
-	if (!has_wcs(&gfit)) {
-		siril_message_dialog(GTK_MESSAGE_ERROR, _("Error"), _("The currently loaded image must be plate solved"));
-		gtk_widget_hide(dialog);
-		return;
+
+	if (gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(manual_mode))){
+		manual_photometry_data(&com.seq);
 	}
 
-	const gchar *entered_target_name = gtk_entry_get_text(GTK_ENTRY(target_entry));
-	gchar *target_name = g_strdup(entered_target_name);
-	g_strstrip(target_name);
-	if (target_name[0] == '\0') {
-		siril_message_dialog(GTK_MESSAGE_ERROR, _("Error"), _("Enter the name of the target star"));
-		return;
-	}
+	if (gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(auto_mode))){
+		if (!has_wcs(&gfit)) {
+			siril_message_dialog(GTK_MESSAGE_ERROR, _("Error"), _("The currently loaded image must be plate solved"));
+			gtk_widget_hide(dialog);
+			return;
+		}
 
-	gchar *end;
-	const gchar *text = gtk_entry_get_text(GTK_ENTRY(delta_vmag_entry));
-	double delta_Vmag = g_ascii_strtod(text, &end);
-	if (text == end || delta_Vmag <= 0.0 || delta_Vmag > 6.0) {
-		siril_message_dialog(GTK_MESSAGE_ERROR, _("Error"), _("Vmag range not accepted (should be ]0, 6])"));
-		return;
-	}
-	text = gtk_entry_get_text(GTK_ENTRY(delta_bv_entry));
-	double delta_BV = g_ascii_strtod(text, &end);
-	if (text == end || delta_BV <= 0.0 || delta_BV > 0.7) {
-		siril_message_dialog(GTK_MESSAGE_ERROR, _("Error"), _("BV range not accepted (should be ]0, 0.7]"));
-		return;
-	}
-	text = gtk_entry_get_text(GTK_ENTRY(emag_entry));
-	double emag = g_ascii_strtod(text, &end);
-	if (text == end || emag <= 0.0 || emag > 0.1) {
-		siril_message_dialog(GTK_MESSAGE_ERROR, _("Error"), _("Magnitude error not accepted (should be ]0, 0.1["));
-		return;
-	}
+		const gchar *entered_target_name = gtk_entry_get_text(GTK_ENTRY(target_entry));
+		gchar *target_name = g_strdup(entered_target_name);
+		g_strstrip(target_name);
+		if (target_name[0] == '\0') {
+			siril_message_dialog(GTK_MESSAGE_ERROR, _("Error"), _("Enter the name of the target star"));
+			return;
+		}
 
-	gboolean use_apass = gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(apass_radio));
-	gboolean narrow = gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(check_narrow));
-	control_window_switch_to_tab(OUTPUT_LOGS);
+		gchar *end;
+		const gchar *text = gtk_entry_get_text(GTK_ENTRY(delta_vmag_entry));
+		double delta_Vmag = g_ascii_strtod(text, &end);
+		if (text == end || delta_Vmag <= 0.0 || delta_Vmag > 6.0) {
+			siril_message_dialog(GTK_MESSAGE_ERROR, _("Error"), _("Vmag range not accepted (should be ]0, 6])"));
+			return;
+		}
+		text = gtk_entry_get_text(GTK_ENTRY(delta_bv_entry));
+		double delta_BV = g_ascii_strtod(text, &end);
+		if (text == end || delta_BV <= 0.0 || delta_BV > 0.7) {
+			siril_message_dialog(GTK_MESSAGE_ERROR, _("Error"), _("BV range not accepted (should be ]0, 0.7]"));
+			return;
+		}
+		text = gtk_entry_get_text(GTK_ENTRY(emag_entry));
+		double emag = g_ascii_strtod(text, &end);
+		if (text == end || emag <= 0.0 || emag > 0.1) {
+			siril_message_dialog(GTK_MESSAGE_ERROR, _("Error"), _("Magnitude error not accepted (should be ]0, 0.1["));
+			return;
+		}
 
-	struct compstars_arg *args = calloc(1, sizeof(struct compstars_arg));
-	args->fit = &gfit;
-	args->target_name = g_strdup(target_name);
-	args->narrow_fov = narrow;
-	args->cat = use_apass ? CAT_APASS : CAT_NOMAD;
-	args->delta_Vmag = delta_Vmag;
-	args->delta_BV = delta_BV;
-	args->max_emag = emag;
-	args->nina_file = g_strdup("auto");
+		gboolean use_apass = gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(apass_radio));
+		gboolean narrow = gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(check_narrow));
+		control_window_switch_to_tab(OUTPUT_LOGS);
 
-	start_in_new_thread(compstars_worker, args);
+		struct compstars_arg *args = calloc(1, sizeof(struct compstars_arg));
+		args->fit = &gfit;
+		args->target_name = g_strdup(target_name);
+		args->narrow_fov = narrow;
+		args->cat = use_apass ? CAT_APASS : CAT_NOMAD;
+		args->delta_Vmag = delta_Vmag;
+		args->delta_BV = delta_BV;
+		args->max_emag = emag;
+		args->nina_file = g_strdup("auto");
+
+		start_in_new_thread(compstars_worker, args);
+	}
 }
