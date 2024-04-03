8df952797aea72b8f32a7764b294af7e03e5e56f
cissou8 <cissou8@gmail.com>
2024-04-03 22:05:13 +0200

    clean

--- src/opencv/opencv.cpp
+++ src/opencv/opencv.cpp
@@ -1198,7 +1198,6 @@ int cvWarp_fromKR(fits *image, astrometric_roi *roi_in, Homography K, Homography
 		Mat roiin = aux(Rect(xi, yi, outr.size().width, outr.size().height));
 		Mat roiout = out(Rect(xo, yo, outr.size().width, outr.size().height));
 		roiin.copyTo(roiout);
-		aux.release();
 
 		return Mat_to_image(image, &in, &out, bgr, roi_in->w, roi_in->h);
 
