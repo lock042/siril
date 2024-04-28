348f8d36e8606a6cf620ce8f6844630f3ce30114
aje.baugh <aje.baugh@gmail.com>
2024-04-28 21:37:20 +0100

    Add IAU observatory code preference
    This is used in Solar System object search to improve accuracy.
    If the preference is set it is always used by the GUI in preference
    to the geocentric observer code;
    it is used by the conesearch command -cat=solsys unless overridden
    with the -obs= option.

--- src/core/siril_actions.c
+++ src/core/siril_actions.c
@@ -402,7 +402,13 @@ void search_object_activate(GSimpleAction *action, GVariant *parameter, gpointer
 void search_object_solar_activate(GSimpleAction *action, GVariant *parameter, gpointer user_data) {
 	if (has_wcs(&gfit)) {
 		siril_catalogue *siril_cat = siril_catalog_fill_from_fit(&gfit, CAT_IMCCE, -1.f);
-		siril_cat->IAUcode = g_strdup("500");
+		if (com.pref.astrometry.default_obscode != NULL) {
+			siril_cat->IAUcode = g_strdup(com.pref.astrometry.default_obscode);
+			siril_log_message(_("Using preferred observatory code %s\n"), siril_cat->IAUcode);
+		} else {
+			siril_cat->IAUcode = g_strdup("500");
+			siril_log_message(_("Using default geocentric observer code\n"));
+		}
 		conesearch_args *args = init_conesearch();
 		args->fit = &gfit;
 		args->has_GUI = TRUE;
