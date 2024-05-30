52226f3fee2c65a3778986ab3ddf14a346405cf0
Cyril Richard <cyril.richard42@gmail.com>
2024-05-30 22:20:31 +0200

    Using new python tool to declare static variables

--- src/gui/annotate.c
+++ src/gui/annotate.c
@@ -42,9 +42,68 @@ enum {
 	SEARCH_OBJECT_PAGE
 };
 
+// Statics declarations
+
+GtkButton *annotate_close = NULL, *annotate_apply = NULL, *annotate_save_as_button = NULL, *show_button = NULL;
+GtkSearchEntry *search_objects_entry = NULL;
+GtkWindow *annotate_dialog = NULL;
+GtkLabel *show_ra = NULL, *show_dec = NULL;
+GtkEntry *conesearch_save_entry = NULL, *annotate_obscode_entry = NULL, *show_file_entry = NULL, *show_ra_entry = NULL, *show_dec_entry = NULL, *show_name_entry = NULL;
+GtkComboBoxText *conesearch_combo = NULL;
+GtkAdjustment *adj_mag_limit = NULL;
+GtkSpinButton *conesearch_maglimit = NULL;
+GtkStack *stack_show = NULL;
+GtkNotebook *notebook_annotate = NULL;
+GtkToggleButton *conesearch_photometric = NULL, *conesearch_tag = NULL, *conesearch_log = NULL, *show_tag = NULL, *show_log = NULL, *show_clear = NULL;
+
+
+// Statics init
+
+static void annotate_dialog_init_statics() {
+	if (adj_mag_limit == NULL) {
+		// GtkButton
+		annotate_close = GTK_BUTTON(lookup_widget("annotate_close"));
+		annotate_apply = GTK_BUTTON(lookup_widget("annotate_apply"));
+		annotate_save_as_button = GTK_BUTTON(lookup_widget("annotate_save_as_button"));
+		show_button = GTK_BUTTON(lookup_widget("show_button"));
+		// GtkSearchEntry
+		search_objects_entry = GTK_SEARCH_ENTRY(lookup_widget("search_objects_entry"));
+		// GtkWindow
+		annotate_dialog = GTK_WINDOW(lookup_widget("annotate_dialog"));
+		// GtkLabel
+		show_ra = GTK_LABEL(lookup_widget("show_ra"));
+		show_dec = GTK_LABEL(lookup_widget("show_dec"));
+		// GtkEntry
+		conesearch_save_entry = GTK_ENTRY(lookup_widget("conesearch_save_entry"));
+		annotate_obscode_entry = GTK_ENTRY(lookup_widget("annotate_obscode_entry"));
+		show_file_entry = GTK_ENTRY(lookup_widget("show_file_entry"));
+		show_ra_entry = GTK_ENTRY(lookup_widget("show_ra_entry"));
+		show_dec_entry = GTK_ENTRY(lookup_widget("show_dec_entry"));
+		show_name_entry = GTK_ENTRY(lookup_widget("show_name_entry"));
+		// GtkComboBoxText
+		conesearch_combo = GTK_COMBO_BOX_TEXT(lookup_widget("conesearch_combo"));
+		// GtkAdjustment
+		adj_mag_limit = GTK_ADJUSTMENT(gtk_builder_get_object(gui.builder, "adj_mag_limit"));
+		// GtkSpinButton
+		conesearch_maglimit = GTK_SPIN_BUTTON(lookup_widget("conesearch_maglimit"));
+		// GtkStack
+		stack_show = GTK_STACK(lookup_widget("stack_show"));
+		// GtkNotebook
+		notebook_annotate = GTK_NOTEBOOK(lookup_widget("notebook_annotate"));
+		// GtkToggleButton
+		conesearch_photometric = GTK_TOGGLE_BUTTON(lookup_widget("conesearch_photometric"));
+		conesearch_tag = GTK_TOGGLE_BUTTON(lookup_widget("conesearch_tag"));
+		conesearch_log = GTK_TOGGLE_BUTTON(lookup_widget("conesearch_log"));
+		show_tag = GTK_TOGGLE_BUTTON(lookup_widget("show_tag"));
+		show_log = GTK_TOGGLE_BUTTON(lookup_widget("show_log"));
+		show_clear = GTK_TOGGLE_BUTTON(lookup_widget("show_clear"));
+	}
+}
+
+
 static siril_cat_index get_cat_index_from_combo() {
 	siril_cat_index cat = CAT_AUTO;
-	const gchar *cat_char = gtk_combo_box_text_get_active_text(GTK_COMBO_BOX_TEXT(lookup_widget("conesearch_combo")));
+	const gchar *cat_char = gtk_combo_box_text_get_active_text(conesearch_combo);
 	if (!g_strcmp0(cat_char, "tycho2"))
 		cat = CAT_TYCHO2;
 	else if (!g_strcmp0(cat_char, "nomad"))
@@ -92,10 +151,13 @@ static void set_filter(GtkFileChooser *dialog) {
 	gtk_file_chooser_set_filter(dialog, f);
 }
 
+void on_annotate_dialog_show(GtkWidget *widget, gpointer user_data) {
+	annotate_dialog_init_statics();
+}
+
 void on_annotate_save_as_button_clicked(GtkButton *button, gpointer user_data) {
 	SirilWidget *widgetdialog;
 	GtkFileChooser *dialog = NULL;
-	GtkWindow *annotate_dialog = GTK_WINDOW(lookup_widget("annotate_dialog"));
 	gint res;
 	gchar *filename;
 
@@ -113,9 +175,8 @@ void on_annotate_save_as_button_clicked(GtkButton *button, gpointer user_data) {
 	res = siril_dialog_run(widgetdialog);
 	if (res == GTK_RESPONSE_ACCEPT) {
 		gchar *file = siril_file_chooser_get_filename(dialog);
-		GtkEntry* entry = GTK_ENTRY(lookup_widget("conesearch_save_entry"));
-		gtk_entry_set_text(entry, file);
-		gtk_editable_set_position(GTK_EDITABLE(entry), -1);
+		gtk_entry_set_text(conesearch_save_entry, file);
+		gtk_editable_set_position(GTK_EDITABLE(conesearch_save_entry), -1);
 		g_free(file);
 	}
 	siril_widget_destroy(widgetdialog);
@@ -125,7 +186,6 @@ void on_annotate_save_as_button_clicked(GtkButton *button, gpointer user_data) {
 void on_show_button_clicked(GtkButton *button, gpointer user_data) {
 	SirilWidget *widgetdialog;
 	GtkFileChooser *dialog = NULL;
-	GtkWindow *annotate_dialog = GTK_WINDOW(lookup_widget("annotate_dialog"));
 	gint res;
 
 
@@ -139,9 +199,8 @@ void on_show_button_clicked(GtkButton *button, gpointer user_data) {
 	res = siril_dialog_run(widgetdialog);
 	if (res == GTK_RESPONSE_ACCEPT) {
 		gchar *file = siril_file_chooser_get_filename(dialog);
-		GtkEntry* entry = GTK_ENTRY(lookup_widget("show_file_entry"));
-		gtk_entry_set_text(entry, file);
-		gtk_editable_set_position(GTK_EDITABLE(entry), -1);
+		gtk_entry_set_text(show_file_entry, file);
+		gtk_editable_set_position(GTK_EDITABLE(show_file_entry), -1);
 		g_free(file);
 	}
 	siril_widget_destroy(widgetdialog);
@@ -160,24 +219,22 @@ static conesearch_params* parse_conesearch_ui() {
 	params->outfilename = NULL;
 
 	gboolean local_cat = local_catalogues_available();
-	GtkEntry *save_entry = GTK_ENTRY(lookup_widget("conesearch_save_entry"));
-	GtkEntry *obscode_entry = GTK_ENTRY(lookup_widget("annotate_obscode_entry"));
 
 	if (com.pref.astrometry.default_obscode != NULL) {
 		params->obscode = g_strdup(com.pref.astrometry.default_obscode);
 		params->default_obscode_used = TRUE;
 	}
 	params->cat = get_cat_index_from_combo();
-	params->display_log = gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(lookup_widget("conesearch_log"))) ? BOOL_TRUE : BOOL_FALSE;
-	params->display_tag = gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(lookup_widget("conesearch_tag"))) ? BOOL_TRUE : BOOL_FALSE;
-	params->photometric = gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(lookup_widget("conesearch_photometric")));
-	params->limit_mag = gtk_spin_button_get_value(GTK_SPIN_BUTTON(lookup_widget("conesearch_maglimit")));
-    const gchar *output = gtk_entry_get_text(save_entry);
+	params->display_log = gtk_toggle_button_get_active(conesearch_log) ? BOOL_TRUE : BOOL_FALSE;
+	params->display_tag = gtk_toggle_button_get_active(conesearch_tag) ? BOOL_TRUE : BOOL_FALSE;
+	params->photometric = gtk_toggle_button_get_active(conesearch_photometric);
+	params->limit_mag = gtk_spin_button_get_value(conesearch_maglimit);
+    const gchar *output = gtk_entry_get_text(conesearch_save_entry);
 
     if (output != NULL && strlen(output) != 0) {
         params->outfilename = g_strdup(output);
     }
-    const gchar *obscode = gtk_entry_get_text(obscode_entry);
+    const gchar *obscode = gtk_entry_get_text(annotate_obscode_entry);
     if (obscode != NULL && strlen(obscode) != 0) {
     	params->default_obscode_used = FALSE;
         params->obscode = g_strdup(obscode);
@@ -240,10 +297,10 @@ static show_params* parse_show_ui() {
 	params->display_tag = BOOL_NOT_SET;
 	params->name = NULL;
 
-	const gchar *visible_child_name = gtk_stack_get_visible_child_name(GTK_STACK(lookup_widget("stack_show")));
+	const gchar *visible_child_name = gtk_stack_get_visible_child_name(stack_show);
 
 	if (!g_strcmp0("page0", visible_child_name)) {
-	    const gchar *input = gtk_entry_get_text(GTK_ENTRY(lookup_widget("show_file_entry")));
+	    const gchar *input = gtk_entry_get_text(show_file_entry);
 		if (input != NULL && strlen(input) != 0) {
 			params->file = g_strdup(input);
 		}
@@ -251,23 +308,23 @@ static show_params* parse_show_ui() {
 		params->name = NULL;
 	} else {
 		params->file = NULL;
-		const gchar *ra = gtk_entry_get_text(GTK_ENTRY(lookup_widget("show_ra_entry")));
-		const gchar *dec = gtk_entry_get_text(GTK_ENTRY(lookup_widget("show_dec_entry")));
+		const gchar *ra = gtk_entry_get_text(show_ra_entry);
+		const gchar *dec = gtk_entry_get_text(show_dec_entry);
 
 		if (is_string_numeric(ra) && is_string_numeric(dec)) {
 			params->coords = siril_world_cs_new_from_objct_ra_dec((gchar *)ra, (gchar *)dec);
 		} else {
 			params->coords = NULL;
 		}
-	    const gchar *name_entry = gtk_entry_get_text(GTK_ENTRY(lookup_widget("show_name_entry")));
+	    const gchar *name_entry = gtk_entry_get_text(show_name_entry);
 		if (name_entry != NULL && strlen(name_entry) != 0) {
 			params->name = g_strdup(name_entry);
 		}
 	}
 
-    params->clear = gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(lookup_widget("show_clear")));
-    params->display_log = gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(lookup_widget("show_log")));
-    params->display_tag = gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(lookup_widget("show_tag")));
+    params->clear = gtk_toggle_button_get_active(show_clear);
+    params->display_log = gtk_toggle_button_get_active(show_log);
+    params->display_tag = gtk_toggle_button_get_active(show_tag);
 
 
 	return params;
@@ -336,7 +393,6 @@ int execute_show_command(show_params *params) {
 }
 
 void on_annotate_apply_clicked(GtkButton *button, gpointer user_data) {
-	GtkNotebook *notebook_annotate = GTK_NOTEBOOK(lookup_widget("notebook_annotate"));
 	GtkEntry *entry = NULL;
 	conesearch_params *params_cone = NULL;
 	show_params *params_show = NULL;
@@ -363,7 +419,7 @@ void on_annotate_apply_clicked(GtkButton *button, gpointer user_data) {
 		g_free(params_show);
 		break;
 	case SEARCH_OBJECT_PAGE:
-		entry = GTK_ENTRY(GTK_SEARCH_ENTRY(lookup_widget("search_objects_entry")));
+		entry = GTK_ENTRY(search_objects_entry);
 		search_object(entry);
 		break;
 	}
