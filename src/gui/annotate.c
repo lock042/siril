4bd4fe4571d34219b32aa01142edff12bd670f56
Cyril Richard <cyril.richard42@gmail.com>
2024-05-30 12:26:03 +0200

    moving function

--- src/gui/annotate.c
+++ src/gui/annotate.c
@@ -232,36 +232,6 @@ int execute_conesearch(conesearch_params *params) {
 	return CMD_OK;
 }
 
-gboolean is_string_numeric(const gchar *str) {
-	// Check if the string is NULL or empty
-	if (str == NULL || *str == '\0') {
-		return FALSE;
-	}
-
-	gboolean has_decimal_point = FALSE;
-	const gchar *p = str;
-
-	// Check for an optional leading sign
-	if (*p == '-' || *p == '+') {
-		p++;
-	}
-
-	// Check each character in the string
-	for (; *p != '\0'; p++) {
-		if (*p == '.') {
-			if (has_decimal_point) {
-				// More than one decimal point
-				return FALSE;
-			}
-			has_decimal_point = TRUE;
-		} else if (!g_ascii_isdigit(*p)) {
-			return FALSE;
-		}
-	}
-
-	return (p > str && (g_ascii_isdigit(*(p - 1)) || has_decimal_point));
-}
-
 static show_params* parse_show_ui() {
 	show_params *params = g_new0(show_params, 1);
 	params->display_log = BOOL_NOT_SET;
