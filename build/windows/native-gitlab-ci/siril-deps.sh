3633921834d6ac7ad844ed7cda3d1b67c89f4950
cissou8 <cissou8@gmail.com>
2024-06-15 22:14:15 +0200

    fix pacman install

--- build/windows/native-gitlab-ci/siril-deps.sh
+++ build/windows/native-gitlab-ci/siril-deps.sh
@@ -3,7 +3,6 @@ mingw-w64-x86_64-toolchain \
 mingw-w64-x86_64-cmake \
 git \
 automake \
-wget \
 mingw-w64-x86_64-autotools \
 mingw-w64-x86_64-lcms2 \
 mingw-w64-x86_64-curl \
@@ -19,16 +18,18 @@ mingw-w64-x86_64-opencv \
 mingw-w64-x86_64-libheif \
 mingw-w64-x86_64-ffms2 \
 
-# Build LibRaw from github
 mkdir _deps && cd _deps
+
+# Fetch older version of cfitsio (4.3.1-1)
+wget https://repo.msys2.org/mingw/mingw64/mingw-w64-x86_64-cfitsio-1~4.3.1-1-any.pkg.tar.zst
+pacman --noconfirm -U mingw-w64-x86_64-cfitsio-1~4.3.1-1-any.pkg.tar.zst
+cd ..
+
+# Build LibRaw from github
 git clone --depth 1 https://github.com/LibRaw/LibRaw.git
 cd LibRaw
 autoreconf -fi && \
 ./configure --disable-examples --disable-static && \
 make install -j$(nproc) || exit 1
 cd ..
-
-# Fetch older version of cfitsio (4.3.1-1)
-wget https://repo.msys2.org/mingw/mingw64/mingw-w64-x86_64-cfitsio-1~4.3.1-1-any.pkg.tar.zst
-pacman -U mingw-w64-x86_64-cfitsio-1~4.3.1-1-any.pkg.tar.zst
 
