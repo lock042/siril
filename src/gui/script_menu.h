99426eda255795440241f3565f07f7ea61af88ed
Adrian Knagg-Baugh <aje.baugh@gmail.com>
2024-07-03 21:55:30 +0000

    Enforce proper control over interactions during script execution (fixes #1326)

--- src/gui/script_menu.h
+++ src/gui/script_menu.h
@@ -9,5 +9,7 @@ int initialize_script_menu(gboolean verbose);
 int refresh_script_menu(gboolean verbose);
 int refresh_scripts(gboolean update_list, gchar **error);
 void siril_get_on_script_pages();
+void script_widgets_enable(gboolean status);
+gboolean script_widgets_idle(gpointer user_data);
 
 #endif /* SRC_GUI_SCRIPT_MENU_H_ */
