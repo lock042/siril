effd57d15539ddb3a717793b49890f2194e47384
aje.baugh <aje.baugh@gmail.com>
2024-10-17 22:45:00 +0100

    Revert experiments with appimage and flatpak

--- build/appimage/generate.sh
+++ build/appimage/generate.sh
@@ -1,49 +1,65 @@
 #!/bin/bash
+
 ########################################################################
 # Install build-time and run-time dependencies
 ########################################################################
+
 export DEBIAN_FRONTEND=noninteractive
+
 ########################################################################
 # Build Siril and install to appdir/
 ########################################################################
+
 BUILDDIR=build/appimage/build
 PREFIX=/usr
+
 meson ${BUILDDIR} \
     --prefix=${PREFIX} \
     --buildtype=release \
     -Drelocatable-bundle=yes
+
+
 ninja -C ${BUILDDIR} -j$(nproc)
 DESTDIR=$PWD/${BUILDDIR}/appdir ninja -C ${BUILDDIR} -j$(nproc) install; find ${BUILDDIR}/appdir/
 cd ${BUILDDIR}
 cp ../AppRun appdir/AppRun ; chmod +x appdir/AppRun
 cp ./appdir/usr/share/icons/hicolor/scalable/apps/org.siril.Siril.svg ./appdir/org.siril.Siril.svg
+
 cd appdir/
+
 ########################################################################
-# Bundle everything
+# Bundle everyhting
 # to allow the AppImage to run on older systems as well
 ########################################################################
+
 apt_bundle() {
     apt-get download "$@"
     find *.deb -exec dpkg-deb -x {} . \;
     find *.deb -delete
 }
+
 # Bundle all of glibc; this should eventually be done by linuxdeployqt
 apt update
 apt_bundle libc6
+
 # Make absolutely sure it will not load stuff from /lib or /usr
 sed -i -e 's|/usr|/xxx|g' lib/x86_64-linux-gnu/ld-linux-x86-64.so.2
+
 # Bundle Gdk pixbuf loaders without which the bundled Gtk does not work;
 # this should eventually be done by linuxdeployqt
 apt_bundle librsvg2-common libgdk-pixbuf2.0-0 heif-gdk-pixbuf heif-thumbnailer
 cp /usr/lib/x86_64-linux-gnu/gdk-pixbuf-*/*/loaders/* usr/lib/x86_64-linux-gnu/gdk-pixbuf-*/*/loaders/
 cp /usr/lib/x86_64-linux-gnu/gdk-pixbuf-*/*/loaders.cache usr/lib/x86_64-linux-gnu/gdk-pixbuf-*/*/
 sed -i -e 's|/usr/lib/x86_64-linux-gnu/gdk-pixbuf-.*/.*/loaders/||g' usr/lib/x86_64-linux-gnu/gdk-pixbuf-*/*/loaders.cache
+
 # Bundle fontconfig settings
 mkdir -p etc/fonts/
 cp /etc/fonts/fonts.conf etc/fonts/
+
 # Bundle ssl certificates
 mkdir -p etc/ssl/
 cp -rf /etc/ssl/* etc/ssl/
+
 # Compile GLib schemas if the subdirectory is present in the AppImage
 # AppRun has to export GSETTINGS_SCHEMA_DIR for this to work
 apt_bundle gnome-settings-daemon-common
@@ -53,44 +69,26 @@ if [ -d usr/share/glib-2.0/schemas/ ] ; then
   ( cd usr/share/glib-2.0/schemas/ ; glib-compile-schemas . )
 fi
 
-# Install Python3 and pip
-apt_bundle python3 python3-pip
-
-# Ensure Python3 binary exists before creating symlink
-if [ -f usr/bin/python3.9 ]; then
-    ln -s python3.9 usr/bin/python3
-else
-    echo "Error: Python3.9 binary not found in usr/bin"
-    exit 1
-fi
-
-# Create symlink for pip3 if necessary
-if [ -f usr/bin/pip3 ]; then
-    ln -s pip3 usr/bin/pip
-else
-    echo "Error: pip3 binary not found in usr/bin"
-    exit 1
-fi
-
-# Ensure pip is up to date
-./usr/bin/python3 -m pip install --upgrade pip
-
 cd -
+
 ########################################################################
 # Generate AppImage
 ########################################################################
+
 wget -c -nv "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
 chmod a+x linuxdeployqt-continuous-x86_64.AppImage
+
 linuxdeployqtargs=()
+
 for so in $(find \
     appdir/usr/lib/x86_64-linux-gnu/gdk-pixbuf-*/*/loaders \
     -name \*.so); do
     linuxdeployqtargs+=("-executable=$(readlink -f "$so")")
 done
-# Add Python to the AppImage using linuxdeploy
-linuxdeployqtargs+=("-executable=appdir/usr/bin/python3")
-linuxdeployqtargs+=("-executable=appdir/usr/bin/pip3")
+
 ./linuxdeployqt-continuous-x86_64.AppImage --appimage-extract-and-run appdir/usr/share/applications/org.siril.Siril.desktop \
   -appimage -unsupported-bundle-everything \
   "${linuxdeployqtargs[@]}"
+
 mv Siril*.AppImage* ../
+
