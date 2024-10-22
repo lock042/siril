728837139db61c32ffd4cac1c91fef60f997f23c
cec m <cissou8@gmail.com>
2024-10-22 21:38:03 +0000

    Fixes windows native build

--- build/windows/native-gitlab-ci/siril-package.sh
+++ build/windows/native-gitlab-ci/siril-package.sh
@@ -15,8 +15,6 @@ cp -fr ${W64_OUT}/bin/gspawn-win64-helper-console.exe ${INSTALL_PREFIX}/bin/
 cp build/windows/crossbuild-gitlab-ci/siril-wrapper.cmd ${INSTALL_PREFIX}/bin/
 # Package lib
 cp -fr ${W64_OUT}/lib/gdk-pixbuf-2.0 ${INSTALL_PREFIX}/lib/
-cp build/windows/loaders.cache ${INSTALL_PREFIX}/lib/gdk-pixbuf-2.0/2.10.0/
-glib-compile-schemas --targetdir=${INSTALL_PREFIX}/share/glib-2.0/schemas ${W64_OUT}/share/glib-2.0/schemas
 # Package dll with a Python script using objdump
 python3 build/windows/dll_link.py ${INSTALL_PREFIX}/bin/siril.exe ${W64_OUT}/ ${INSTALL_PREFIX}
 python3 build/windows/dll_link.py ${INSTALL_PREFIX}/bin/gdbus.exe ${W64_OUT}/ ${INSTALL_PREFIX}
@@ -26,4 +24,7 @@ python3 build/windows/dll_link.py ${W64_OUT}/bin/gdk-pixbuf-thumbnailer.exe ${W6
 python3 build/windows/dll_link.py ${W64_OUT}/bin/gspawn-win64-helper.exe ${W64_OUT}/ ${INSTALL_PREFIX}
 python3 build/windows/dll_link.py ${W64_OUT}/bin/gspawn-win64-helper-console.exe ${W64_OUT}/ ${INSTALL_PREFIX}
 python3 build/windows/dll_link.py ${W64_OUT}/bin/glib-compile-schemas.exe ${W64_OUT}/ ${INSTALL_PREFIX}
-python3 build/windows/dll_link.py ${W64_OUT}/lib/gdk-pixbuf-2.0/2.10.0/loaders/libpixbufloader-svg.dll ${W64_OUT}/ ${INSTALL_PREFIX}
+python3 build/windows/dll_link.py ${W64_OUT}/lib/gdk-pixbuf-2.0/2.10.0/loaders/pixbufloader_svg.dll ${W64_OUT}/ ${INSTALL_PREFIX}
+cd "${INSTALL_PREFIX}/bin"
+./gdk-pixbuf-query-loaders.exe --update-cache
+./glib-compile-schemas.exe --targetdir="../share/glib-2.0/schemas" "../share/glib-2.0/schemas"
