b144c826a35f79b1aaade8c5b5feaabe3eaf212f
aje.baugh <aje.baugh@gmail.com>
2024-10-26 23:37:01 +0100

    Revert "Remove an #ifdef used during testing"
    This reverts commit 8fe2006b522c6e6edce7a211a7d0f916a817a57b.

--- src/io/image_formats_libraries.c
+++ src/io/image_formats_libraries.c
@@ -17,6 +17,7 @@
  * You should have received a copy of the GNU General Public License
  * along with Siril. If not, see <http://www.gnu.org/licenses/>.
 */
+#define HAVE_LIBTIFF
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
