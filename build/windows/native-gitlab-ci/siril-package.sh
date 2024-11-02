8f3338aa469cd2271cf6f8ab24d8a8c2576929ad
cissou8 <cissou8@gmail.com>
2024-11-02 13:32:44 +0100

    try to fix paths

--- build/windows/native-gitlab-ci/siril-package.sh
+++ build/windows/native-gitlab-ci/siril-package.sh
@@ -15,12 +15,12 @@ cp -fr ${W64_OUT}/bin/gspawn-win64-helper-console.exe ${INSTALL_PREFIX}/bin/
 # Package lib
 cp -fr ${W64_OUT}/lib/gdk-pixbuf-2.0 ${INSTALL_PREFIX}/lib/
 # Package dll with a Python script using objdump
-python3 build/windows/dll_link.py ${INSTALL_PREFIX}/bin/siril.exe ${W64_OUT}/ ${INSTALL_PREFIX} -l ${INSTALL_PREFIX}/DLLcache/DLLlist_${SDW_VERSION}_siril.txt
-python3 build/windows/dll_link.py ${INSTALL_PREFIX}/bin/gdbus.exe ${W64_OUT}/ ${INSTALL_PREFIX} -l ${INSTALL_PREFIX}/DLLcache/DLLlist_${SDW_VERSION}_gdbus.txt
-python3 build/windows/dll_link.py ${W64_OUT}/bin/gdk-pixbuf-query-loaders.exe ${W64_OUT}/ ${INSTALL_PREFIX} -l ${INSTALL_PREFIX}/DLLcache/DLLlist_${SDW_VERSION}_gdk-pixbuf-query-loaders.txt
-python3 build/windows/dll_link.py ${W64_OUT}/bin/gdk-pixbuf-pixdata.exe ${W64_OUT}/ ${INSTALL_PREFIX} -l ${INSTALL_PREFIX}/DLLcache/DLLlist_${SDW_VERSION}_gdk-pixbuf-pixdata.txt
-python3 build/windows/dll_link.py ${W64_OUT}/bin/gdk-pixbuf-thumbnailer.exe ${W64_OUT}/ ${INSTALL_PREFIX} -l ${INSTALL_PREFIX}/DLLcache/DLLlist_${SDW_VERSION}_gdk-pixbuf-thumbnailer.txt
-python3 build/windows/dll_link.py ${W64_OUT}/bin/gspawn-win64-helper.exe ${W64_OUT}/ ${INSTALL_PREFIX} -l ${INSTALL_PREFIX}/DLLcache/DLLlist_${SDW_VERSION}_gspawn-win64-helper.txt
-python3 build/windows/dll_link.py ${W64_OUT}/bin/gspawn-win64-helper-console.exe ${W64_OUT}/ ${INSTALL_PREFIX} -l ${INSTALL_PREFIX}/DLLcache/DLLlist_${SDW_VERSION}_gspawn-win64-helper-console.txt
-python3 build/windows/dll_link.py ${W64_OUT}/bin/glib-compile-schemas.exe ${W64_OUT}/ ${INSTALL_PREFIX} -l ${INSTALL_PREFIX}/DLLcache/DLLlist_${SDW_VERSION}_glib-compile-schemas.txt
-python3 build/windows/dll_link.py ${W64_OUT}/lib/gdk-pixbuf-2.0/2.10.0/loaders/pixbufloader_svg.dll ${W64_OUT}/ ${INSTALL_PREFIX} -l ${INSTALL_PREFIX}/DLLcache/DLLlist_${SDW_VERSION}_pixbufloader_svg.txt
+python3 build/windows/dll_link.py ${INSTALL_PREFIX}/bin/siril.exe ${W64_OUT}/ ${INSTALL_PREFIX} -l ${CI_PROJECT_DIR}/DLLcache/DLLlist_${SDW_NAME}_siril.txt
+python3 build/windows/dll_link.py ${INSTALL_PREFIX}/bin/gdbus.exe ${W64_OUT}/ ${INSTALL_PREFIX} -l ${CI_PROJECT_DIR}/DLLcache/DLLlist_${SDW_NAME}_gdbus.txt
+python3 build/windows/dll_link.py ${W64_OUT}/bin/gdk-pixbuf-query-loaders.exe ${W64_OUT}/ ${INSTALL_PREFIX} -l ${CI_PROJECT_DIR}/DLLcache/DLLlist_${SDW_NAME}_gdk-pixbuf-query-loaders.txt
+python3 build/windows/dll_link.py ${W64_OUT}/bin/gdk-pixbuf-pixdata.exe ${W64_OUT}/ ${INSTALL_PREFIX} -l ${CI_PROJECT_DIR}/DLLcache/DLLlist_${SDW_NAME}_gdk-pixbuf-pixdata.txt
+python3 build/windows/dll_link.py ${W64_OUT}/bin/gdk-pixbuf-thumbnailer.exe ${W64_OUT}/ ${INSTALL_PREFIX} -l ${CI_PROJECT_DIR}/DLLcache/DLLlist_${SDW_NAME}_gdk-pixbuf-thumbnailer.txt
+python3 build/windows/dll_link.py ${W64_OUT}/bin/gspawn-win64-helper.exe ${W64_OUT}/ ${INSTALL_PREFIX} -l ${CI_PROJECT_DIR}/DLLcache/DLLlist_${SDW_NAME}_gspawn-win64-helper.txt
+python3 build/windows/dll_link.py ${W64_OUT}/bin/gspawn-win64-helper-console.exe ${W64_OUT}/ ${INSTALL_PREFIX} -l ${CI_PROJECT_DIR}/DLLcache/DLLlist_${SDW_NAME}_gspawn-win64-helper-console.txt
+python3 build/windows/dll_link.py ${W64_OUT}/bin/glib-compile-schemas.exe ${W64_OUT}/ ${INSTALL_PREFIX} -l ${CI_PROJECT_DIR}/DLLcache/DLLlist_${SDW_NAME}_glib-compile-schemas.txt
+python3 build/windows/dll_link.py ${W64_OUT}/lib/gdk-pixbuf-2.0/2.10.0/loaders/pixbufloader_svg.dll ${W64_OUT}/ ${INSTALL_PREFIX} -l ${CI_PROJECT_DIR}/DLLcache/DLLlist_${SDW_NAME}_pixbufloader_svg.txt
