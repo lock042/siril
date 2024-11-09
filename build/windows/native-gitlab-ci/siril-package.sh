8be8d7137ca53988287dfb8751d01abd7e43cfbc
cissou8 <cissou8@gmail.com>
2024-11-09 12:38:59 +0100

    fixing path again again

--- build/windows/native-gitlab-ci/siril-package.sh
+++ build/windows/native-gitlab-ci/siril-package.sh
@@ -5,7 +5,7 @@ cp -fr ${W64_OUT}/etc/ssl ${INSTALL_PREFIX}/etc
 cp -fr ${W64_OUT}/share/glib-2.0/ ${INSTALL_PREFIX}/share
 cp -fr ${W64_OUT}/share/icons/ ${INSTALL_PREFIX}/share
 cp -fr ${W64_OUT}/share/locale/ ${INSTALL_PREFIX}/share
-cp -fr ${PY_OUT}/siril-python/ ${INSTALL_PREFIX}/python
+cp -fr ${PY_OUT}/* ${INSTALL_PREFIX}/python
 # Package executable
 cp -fr ${W64_OUT}/bin/gdbus.exe ${INSTALL_PREFIX}/bin/
 cp -fr ${W64_OUT}/bin/glib-compile-schemas.exe ${INSTALL_PREFIX}/bin/
