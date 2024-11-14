6a1f75ccf1fed3db968fae527cda617fb88804de
cissou8 <cissou8@gmail.com>
2024-11-14 20:25:53 +0100

    final fix

--- src/io/siril_pythonmodule.c
+++ src/io/siril_pythonmodule.c
@@ -1407,10 +1407,14 @@ static gboolean check_or_create_venv(const gchar *project_path, GError **error) 
 		if (!sys_python_exe) {
 			const gchar *sirilrootpath = get_siril_bundle_path();
 			printf("Siril bundle path: %s\n", sirilrootpath);
-			bundle_python_exe = g_build_filename(G_DIR_SEPARATOR_S, sirilrootpath, "python", PYTHON_EXE, NULL);
+			bundle_python_exe = g_build_filename(sirilrootpath, "python", PYTHON_EXE, NULL);
 			printf("Bundle python path: %s\n", bundle_python_exe);
 			if (g_file_test(bundle_python_exe, G_FILE_TEST_IS_EXECUTABLE))
 				printf("Python found in bundle: %s\n", bundle_python_exe);
+			else {
+				g_free(bundle_python_exe);
+				bundle_python_exe = NULL;
+			}
 		}
 
 		if (!sys_python_exe && !bundle_python_exe) {
