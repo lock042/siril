5e93683460009379e3753409833f50ccb11fdc0e
aje.baugh <aje.baugh@gmail.com>
2024-11-14 22:31:55 +0000

    Add header

--- src/io/siril_git.c
+++ src/io/siril_git.c
@@ -31,6 +31,7 @@
 #include "core/siril_networking.h"
 #include "core/siril_update.h" // for the version_number struct
 #include "gui/message_dialog.h"
+#include "gui/photometric_cc.h"
 #include "gui/script_menu.h" // for SCRIPT_EXT TODO: after python3 is merged, move this out of src/gui
 #include "io/siril_git.h"
 #include <assert.h>
