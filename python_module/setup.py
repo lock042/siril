75eed11712e78f9e62edd783f9c1d8fe62dadbff
cissou8 <cissou8@gmail.com>
2024-11-19 00:06:13 +0100

    import metadata from setup.py instead of redefining

--- python_module/setup.py
+++ python_module/setup.py
@@ -11,6 +11,7 @@ setup(
         "pywin32>=300; platform_system=='Windows'"
     ],
     author="Team free-astro",
+    license="GPLv3+",
     description="Python interface for Siril astronomical image processing",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
@@ -20,6 +21,7 @@ setup(
         "Intended Audience :: Science/Research",
         "Topic :: Scientific/Engineering :: Astronomy",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Copyright :: (c) Team free-astro 2024",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
