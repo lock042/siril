3168e69b34cb66a5169a8c8d8c981e715db29d92
aje.baugh <aje.baugh@gmail.com>
2024-12-03 13:18:06 +0000

    Bump module version

--- python_module/setup.py
+++ python_module/setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="siril",
-    version="0.3.46",
+    version="0.3.47",
     packages=find_packages(),
     install_requires=[
         "numpy>=1.20.0",
