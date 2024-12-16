lock042/siril

commit 0add5f552cf74097ca7db9ed528704410da4479c

Fix an issue with false positives when identifying a CFA image

src/gui/callbacks.c
@@ -367,15 +367,17 @@ gpointer on_set_roi() {
 }
 
 gpointer on_clear_roi() {
-	g_mutex_lock(&roi_mutex); // Wait until any thread previews are finished
-	cancel_pending_update();
-	copy_backup_to_gfit();
-	clearfits(&gui.roi.fit);
-	memset(&gui.roi, 0, sizeof(roi_t));
-	redraw(REDRAW_OVERLAY);
-	// Call any callbacks that need calling
-	call_roi_callbacks();
-	g_mutex_unlock(&roi_mutex);
+	if (is_preview_active()) {
+		g_mutex_lock(&roi_mutex); // Wait until any thread previews are finished
+		cancel_pending_update();
+		copy_backup_to_gfit();
+		clearfits(&gui.roi.fit);
+		memset(&gui.roi, 0, sizeof(roi_t));
+		redraw(REDRAW_OVERLAY);
+		// Call any callbacks that need calling
+		call_roi_callbacks();
+		g_mutex_unlock(&roi_mutex);
+	}
 	return GINT_TO_POINTER(0);
 }
 

src/algos/background_extraction.c
@@ -1317,7 +1317,7 @@ void apply_background_extraction_to_sequence(struct background_data *background_
 		siril_log_color_message(_("Error reading reference metadata.\n"), "red");
 		return;
 	}
-	background_args->is_cfa = (!strncmp(metadata.keywords.bayer_pattern, "RGGB", 4) ||
+	background_args->is_cfa = background_args->seq->nb_layers == 1 && (!strncmp(metadata.keywords.bayer_pattern, "RGGB", 4) ||
 							!strncmp(metadata.keywords.bayer_pattern, "BGGR", 4) ||
 							!strncmp(metadata.keywords.bayer_pattern, "GBRG", 4) ||
 							!strncmp(metadata.keywords.bayer_pattern, "GRBG", 4));

src/core/command.c
@@ -5875,7 +5875,7 @@ int process_subsky(int nb) {
 		args->fit = &gfit;
 
 		// Check if the image has a Bayer CFA pattern
-		gboolean is_cfa = (!strncmp(gfit.keywords.bayer_pattern, "RGGB", 4) ||
+		gboolean is_cfa = gfit.naxes[2] == 1 && (!strncmp(gfit.keywords.bayer_pattern, "RGGB", 4) ||
 						  !strncmp(gfit.keywords.bayer_pattern, "BGGR", 4) ||
 						  !strncmp(gfit.keywords.bayer_pattern, "GBRG", 4) ||
 						  !strncmp(gfit.keywords.bayer_pattern, "GRBG", 4));

src/gui/background_extraction.c
@@ -157,7 +157,7 @@ void on_bkg_compute_bkg_clicked(GtkButton *button, gpointer user_data) {
 	args->fit = &gfit;
 
 	// Check if the image has a Bayer CFA pattern
-	gboolean is_cfa = (!strncmp(gfit.keywords.bayer_pattern, "RGGB", 4) ||
+	gboolean is_cfa = gfit.naxes[2] == 1 && (!strncmp(gfit.keywords.bayer_pattern, "RGGB", 4) ||
 					  !strncmp(gfit.keywords.bayer_pattern, "BGGR", 4) ||
 					  !strncmp(gfit.keywords.bayer_pattern, "GBRG", 4) ||
 					  !strncmp(gfit.keywords.bayer_pattern, "GRBG", 4));

commit 42e0b1aba3ae8776374df9ff5896977411b9e460

Fixes a crash when cropping an image with a ROI set (regression caused by 0add5f552cf74097ca7db9ed528704410da4479c)

src/gui/callbacks.c
@@ -366,7 +366,7 @@ gpointer on_set_roi() {
 }
 
 gpointer on_clear_roi() {
-	if (is_preview_active()) {
+	if (gui.roi.active) {
 		g_mutex_lock(&roi_mutex); // Wait until any thread previews are finished
 		cancel_pending_update();
 		copy_backup_to_gfit();
@@ -922,8 +922,8 @@ static void update_roi_from_selection() {
 		copy_roi_into_gfit();
 	}
 	memcpy(&gui.roi.selection, &com.selection, sizeof(rectangle));
-	gui.roi.active = (gui.roi.selection.w > 0 && gui.roi.selection.h > 0);
-	if (gui.roi.active)
+	gboolean active = (gui.roi.selection.w > 0 && gui.roi.selection.h > 0);
+	if (active)
 		on_set_roi();
 	else
 		on_clear_roi();