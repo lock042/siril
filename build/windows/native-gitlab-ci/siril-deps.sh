db7938e02eaadfdafdef4e5e215acffb4a9899d0
Adrian Knagg-Baugh <aje.baugh@gmail.com>
2025-01-13 00:49:09 +0000

    Revert "Remove glib-json from CI build deps"
    This reverts commit ae1ecc713d04ed1b3759e19a13011f8f652f162f.

--- build/windows/native-gitlab-ci/siril-deps.sh
+++ build/windows/native-gitlab-ci/siril-deps.sh
@@ -16,6 +16,7 @@ mingw-w64-x86_64-glib2 \
 mingw-w64-x86_64-gtk3 \
 mingw-w64-x86_64-gsl \
 mingw-w64-x86_64-lcms2 \
+mingw-w64-x86_64-json-glib \
 mingw-w64-x86_64-fftw \
 mingw-w64-x86_64-cfitsio \
 mingw-w64-x86_64-opencv \
