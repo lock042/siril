48c8f46bd860559cdc2d27f92de796050c619732
Adrian Knagg-Baugh <aje.baugh@gmail.com>
2025-01-13 11:37:33 +0000

    Remove the function for handling a photometric catalogue This will be reworked and included in a separate MR

--- src/io/healpix/healpix_cat.h
+++ src/io/healpix/healpix_cat.h
@@ -6,7 +6,6 @@ extern "C" {
 #endif
 
 int get_raw_stars_from_local_gaia_astro_catalogue(double ra, double dec, double radius, double limitmag, gboolean phot, deepStarData **stars, uint32_t *nb_stars);
-int get_raw_stars_from_local_gaia_photo_catalogue(double ra, double dec, double radius, double limitmag, SourceEntryPhoto **stars, uint32_t *nb_stars);
 
 #ifdef __cplusplus
 }
