7907b0443453de41f5c58acd15c275612f058c77
Adrian Knagg-Baugh <aje.baugh@gmail.com>
2025-01-29 00:06:06 +0000

    Script menu improvements

--- src/gui/script_menu.h
+++ src/gui/script_menu.h
@@ -1,8 +1,9 @@
 #ifndef SRC_GUI_SCRIPT_MENU_H_
 #define SRC_GUI_SCRIPT_MENU_H_
 
-#define SCRIPT_EXT ".ssf"
-#define PYSCRIPT_EXT ".py"
+#define SCRIPT_EXT "ssf"
+#define PYSCRIPT_EXT "py"
+#define PYCSCRIPT_EXT "pyc"
 
 GSList *get_list_from_preferences_dialog();
 GSList *set_list_to_preferences_dialog(GSList *list);
@@ -13,5 +14,6 @@ void siril_get_on_script_pages();
 void script_widgets_enable(gboolean status);
 gboolean script_widgets_idle(gpointer user_data);
 gboolean accept_script_warning_dialog();
+gboolean test_last_subdir(const gchar *path, const gchar *expected_subdir);
 
 #endif /* SRC_GUI_SCRIPT_MENU_H_ */
