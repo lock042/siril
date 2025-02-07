255727e89fa7b1c6af0b26bdbb12e847133b25dd
Adrian Knagg-Baugh <aje.baugh@gmail.com>
2025-02-07 23:31:23 +0000

    Remove unused variable

--- src/filters/banding.c
+++ src/filters/banding.c
@@ -132,7 +132,8 @@ void apply_banding_to_sequence(struct banding_data *banding_args) {
 	banding_args->fit = NULL;	// not used here
 
 	if (start_in_new_thread(generic_sequence_worker, args)) {
-		free(args->user);
+		free(banding_args->seqEntry);
+		free(banding_args);
 		free_generic_seq_args(args);
 	}
 }
