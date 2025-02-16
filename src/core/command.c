3ce609b686ee28d2fa3f2beded85760f9f5ce256
Adrian Knagg-Baugh <aje.baugh@gmail.com>
2025-02-16 00:09:19 +0000

    Add missing arg for compilation without LIBCURL

--- src/core/command.c
+++ src/core/command.c
@@ -10013,7 +10013,7 @@ int process_pcc(int nb) {
 
 int process_spcc(int nb) {
 #ifndef HAVE_LIBCURL
-	siril_log_color_message(_("Siril has been compiled without libcurl support for network operations; SPCC is therefore not available. Recompile with libcurl support to enable SPCC.\n"));
+	siril_log_color_message(_("Siril has been compiled without libcurl support for network operations; SPCC is therefore not available. Recompile with libcurl support to enable SPCC.\n"), "red");
 	return CMD_GENERIC_ERROR;
 #else
 	return do_pcc(nb, TRUE);
