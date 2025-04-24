344916e313664dfa447a93a8caea0050158b1ce6
Cyril Richard <cyril.richard42@gmail.com>
2025-04-24 09:18:50 +0200

    Implmenting a better linear regression algorithm

--- src/algos/fitting.h
+++ src/algos/fitting.h
@@ -3,6 +3,7 @@
 
 int robust_polynomial_fit(double *xdata, double *ydata, int n, int degree, double *coeffs, double *uncertainties, gboolean *mask, double *sigma);
 int robust_linear_fit(double *xdata, double *ydata, int n, double *a, double *b, double *sigma, gboolean *mask);
+int repeated_median_fit(double *xdata, double *ydata, int n, double *a, double *b, double *sigma, gboolean *mask);
 double evaluate_polynomial(double *coeffs, int degree, double x);
 
 int find_linear_coeff(fits *target_fit, fits *reference_fit, double low,
