d21c043d9d944775609f87dbd74f949a66e6397c
Adrian Knagg-Baugh <aje.baugh@gmail.com>
2025-04-30 18:36:55 +0100

    Fixes #1566

--- src/gui/script_menu.h
+++ src/gui/script_menu.h
@@ -10,6 +10,7 @@ GSList *set_list_to_preferences_dialog(GSList *list);
 int initialize_script_menu(gboolean verbose);
 gpointer initialize_script_menu_in_thread(gpointer data);
 int refresh_script_menu(gboolean verbose);
+gpointer refresh_scripts_menu_in_thread(gpointer data);
 int refresh_scripts(gboolean update_list, gchar **error);
 void script_widgets_enable(gboolean status);
 gboolean script_widgets_idle(gpointer user_data);
