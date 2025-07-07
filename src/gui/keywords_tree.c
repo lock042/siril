0b7b20a95a4b7698c9ef02dfe3dedeaed62d88cd
Cyril Richard <cyril.richard42@gmail.com>
2025-07-07 19:24:45 +0000

    Make SIP and WCS protected keywords

--- src/gui/keywords_tree.c
+++ src/gui/keywords_tree.c
@@ -144,7 +144,7 @@ static int listFITSKeywords(fits *fit, gboolean editable) {
 		fits_parse_value(card, value, comment, &status);
 		fits_get_keytype(value, &dtype, &status);
 		status = 0;
-		add_key_to_tree(keyname, value, comment, dtype, keyword_is_protected(card), editable);
+		add_key_to_tree(keyname, value, comment, dtype, keyword_is_protected(card, fit), editable);
 	}
 
 
