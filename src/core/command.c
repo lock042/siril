lock042/siril

commit dd2b31f734c499b9b50fadaa6234140f17d5a7af

Fix lack of file extension in com.uniq->filename on saving a file
(Affects saving using the filedialog)

src/gui/save_dialog.c
@@ -659,7 +659,10 @@ static gpointer mini_save_dialog(gpointer p) {
 			gfit.checksum = args->checksum;
 			args->retval = savefits(args->filename, &gfit);
 			if (!args->retval && single_image_is_loaded()) {
-				com.uniq->filename = strdup(args->filename);
+				// com.uniq->filename is handled by libc functions so we can't use g_strdup_printf directly
+				gchar* tempfilename = g_strdup_printf("%s%s", args->filename, com.pref.ext);
+				com.uniq->filename = strdup(tempfilename);
+				free(tempfilename);
 				com.uniq->fileexist = TRUE;
 			}
 			break;

commit 2935da3bdf1748c4f16e7ac4a34e1498ac87bf57

Fixing bug introduced in dd2b31f7 and 8af97f5a

src/core/command.c
@@ -335,9 +335,13 @@ int process_save(int nb){
 		set_cursor_waiting(FALSE);
 	}
 	if (com.uniq) {
-		gchar* tempfilename = g_strdup_printf("%s%s", savename, com.pref.ext);
-		com.uniq->filename = strdup(tempfilename);
-		g_free(tempfilename);
+		if (!g_str_has_suffix(savename, com.pref.ext)) {
+			gchar* tempfilename = g_strdup_printf("%s%s", savename, com.pref.ext);
+			com.uniq->filename = strdup(tempfilename);
+			g_free(tempfilename);
+		} else {
+			com.uniq->filename = strdup(savename);
+		}
 		com.uniq->fileexist = TRUE;
 		if (!com.headless) {
 			display_filename();

src/gui/save_dialog.c
@@ -659,10 +659,14 @@ static gpointer mini_save_dialog(gpointer p) {
 			gfit.checksum = args->checksum;
 			args->retval = savefits(args->filename, &gfit);
 			if (!args->retval && single_image_is_loaded()) {
+				com.uniq->filename = strdup(args->filename);
 				// com.uniq->filename is handled by libc functions so we can't use g_strdup_printf directly
-				gchar* tempfilename = g_strdup_printf("%s%s", args->filename, com.pref.ext);
-				com.uniq->filename = strdup(tempfilename);
-				g_free(tempfilename);
+				if (!g_str_has_suffix(args->filename, com.pref.ext)) {
+					gchar* tempfilename = g_strdup_printf("%s%s", args->filename, com.pref.ext);
+					free(com.uniq->filename);  // Free the previously allocated memory
+					com.uniq->filename = strdup(tempfilename);
+					g_free(tempfilename);  // Use g_free for GLib allocated memory
+				}
 				com.uniq->fileexist = TRUE;
 			}
 			break;