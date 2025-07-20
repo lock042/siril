caac240559dc49c12dd974753180fd9daa206d09
cissou8 <cissou8@gmail.com>
2025-07-20 17:47:02 +0200

    make sure sequence basename is used to create the cache name

--- src/io/sequence.c
+++ src/io/sequence.c
@@ -2427,8 +2427,8 @@ gchar *get_sequence_cache_filename(sequence *seq, int index, const gchar *ext, c
 		cache_filename = g_strdup_printf("%s%s.%s", prefix, root, ext);
 	else
 		cache_filename = g_strdup_printf("%s.%s", root, ext);
-
-	gchar *cache_path = g_build_path(G_DIR_SEPARATOR_S, com.wd, "cache", cache_filename, NULL);
+	const gchar *base_cache_filename = g_path_get_basename(cache_filename);
+	gchar *cache_path = g_build_path(G_DIR_SEPARATOR_S, com.wd, "cache", base_cache_filename, NULL);
 	g_free(cache_filename);
 	return cache_path;
 }
