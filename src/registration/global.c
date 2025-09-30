3b751e1feb32686e8cc8ab0db26c3d2a927cc0e2
cissou8 <cissou8@gmail.com>
2025-09-30 23:51:09 +0200

    fix wrong conditions in star_align_image_hook

--- src/registration/global.c
+++ src/registration/global.c
@@ -378,7 +378,7 @@ int star_align_image_hook(struct generic_seq_args *args, int out_index, int in_i
 		sadata->current_regdata[in_index].H = H;
 
 		if (!regargs->no_output) {
-			if (regargs->interpolation <= OPENCV_LANCZOS4) {
+			if (regargs->driz || regargs->interpolation <= OPENCV_LANCZOS4) {
 				if (apply_reg_image_hook(args, out_index, in_index, fit, _, threads)) {
 					args->seq->imgparam[in_index].incl = !SEQUENCE_DEFAULT_INCLUDE;
 					return 1;
@@ -394,7 +394,7 @@ int star_align_image_hook(struct generic_seq_args *args, int out_index, int in_i
 		// reference image
 		cvGetEye(&H);
 		sadata->current_regdata[in_index].H = H;
-		if (!regargs->no_output && (regargs->output_scale != 1.f || regargs->driz || regargs->undistort)) {
+		if (!regargs->no_output) {
 			if (apply_reg_image_hook(args, out_index, in_index, fit, _, threads)) {
 				args->seq->imgparam[in_index].incl = !SEQUENCE_DEFAULT_INCLUDE;
 				return 1;
