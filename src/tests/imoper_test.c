62d3cfd6d8fbaf9437e39382cb9f6e1f28e9cf2c
Adrian Knagg-Baugh <aje.baugh@gmail.com>
2025-11-07 15:10:36 +0000

    Fix test suite

--- src/tests/imoper_test.c
+++ src/tests/imoper_test.c
@@ -28,7 +28,7 @@
 
 cominfo com;	// the core data struct
 guiinfo gui;	// the gui data struct
-fits gfit;	// currently loaded image
+fits *gfit;	// currently loaded image
 
 /* required links:
  * new_fit_image (io/image_format_fits.o),
