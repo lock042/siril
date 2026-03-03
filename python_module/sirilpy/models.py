1d84f2c91c65795cccc0e3f600e5dabb5cc8d188
Adrian Knagg-Baugh <aje.baugh@gmail.com>
2026-03-03 09:52:38 +0000

    Fix incorrect constraint on ndarray shape when setting FFit.data

--- python_module/sirilpy/models.py
+++ python_module/sirilpy/models.py
@@ -354,7 +354,7 @@ class FFit:
     bitpix: Optional[BitpixType] = None #: FITS header specification of the image data type.
     orig_bitpix: Optional[BitpixType] = None #: FITS header specification of the original image data type.
     naxis: int = 0 #: The number of axes (2 for a mono image, 3 for a RGB image). Corresponds to the FITS kwyword NAXIS.
-    _naxes: Tuple[int, int, int] = (0, 0, 0) #: A tuple holding the image dimensions.
+    _naxes: Tuple[int, int, int] = (0, 0, 0) #: A tuple holding the image dimensions in Siril order (w, h, c).
 
     keywords: FKeywords = field(default_factory=FKeywords) #: A FKeywords object containing FITS header keywords.
     checksum: bool = False #: Whether Siril will write FITS data checksums for this file.
@@ -410,8 +410,8 @@ class FFit:
             if len(shape) == 2:
                 self._naxes = (shape[1], shape[0], 1)  # width, height, 1 channel
             elif len(shape) == 3:
-                if shape[2] not in (1, 3):
-                    raise ValueError(_("Third dimension must be 1 or 3"))
+                if shape[0] not in (1, 3):
+                    raise ValueError(_("First dimension must be 1 or 3"))
                 self._naxes = (shape[2], shape[1], shape[0])  # width, height, channels
             else:
                 raise ValueError(_("Data must be 2D or 3D"))
