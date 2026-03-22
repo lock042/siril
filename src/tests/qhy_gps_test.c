6c07c6975f52baee6d008ab1e12cb2c12203a1df
Vincent Hourdin <vh@free-astro.org>
2026-03-22 20:40:18 +0000

    more unit tests

--- src/tests/qhy_gps_test.c
+++ src/tests/qhy_gps_test.c
@@ -28,9 +28,10 @@
 #if defined(HAVE_LIBCURL)
 #include <curl/curl.h>
 #endif
-//#include "core/siril_date.h"
+#include "core/siril_date.h"
 #include "io/gps_parser.h"
 #include "io/image_format_fits.h"
+#include "algos/geometry.h"
 
 /************ download test files **************/
 
@@ -110,5 +111,131 @@ void test_rsgps_data_loading() {
 	cr_assert(!fit.keywords.gps_data);
 }
 
+void test_metadata_reading() {
+	fits fit = {0};
+	gchar *file_path = check_or_download_test_file("gps_global_shutter_not_locked.fit");
+	cr_assert(file_path);
+	cr_assert(readfits(file_path, &fit, NULL, FALSE) == 0);
+	g_free(file_path);
+	GDateTime *original_dateobs = g_date_time_ref(fit.keywords.date_obs);
+	double original_exposure = fit.keywords.exposure;
+	long original_naxes1 = fit.naxes[1];
+	int original_ry = fit.ry;
+
+	int crop_rows = 1;
+	struct generic_seq_args args = { 0 };
+	args.user = GINT_TO_POINTER(crop_rows);
+	int retval = gps_extract_image_hook(&args, 0, 0, &fit, NULL, MULTI_THREADED);
+	cr_assert(!retval);
+
+	double new_exposure = fit.keywords.exposure;
+	GDateTime *new_dateobs = g_date_time_ref(fit.keywords.date_obs);
+	gchar *new_dateobs_str = date_time_to_FITS_date(new_dateobs);
+	siril_debug_print("exp %f -> %f\n", original_exposure, new_exposure);
+	siril_debug_print("date-obs %s -> %s\n", date_time_to_FITS_date(original_dateobs), new_dateobs_str);
+	cr_assert(fabs(original_exposure - new_exposure) < original_exposure * 0.001);
+	cr_assert(timediff_in_s(original_dateobs, new_dateobs) < 1.0);
+	cr_assert(!strcmp("2023-08-31T20:20:12.854125", new_dateobs_str));
+	// image metadata has been modified using the GPS metadata
+	cr_assert(fit.ry == original_ry - 1);
+	cr_assert(fit.naxes[1] == original_naxes1 - 1);
+	clearfits(&fit);
+}
+
+void test_rsgps_timestamp_computation() {
+	fits fit = {0};
+	gchar *file_path = check_or_download_test_file("gps_target_rate_tracking_rolling_shutter_1.fit");
+	cr_assert(file_path);
+	cr_assert(readfits(file_path, &fit, NULL, FALSE) == 0);
+	g_free(file_path);
+
+	int row = 0;
+	enum timestamp_type moment = EXP_START;
+	GDateTime *date_start0 = get_timestamp_for_pixel(fit.keywords.gps_data, moment, 0, row);
+	//date_str = date_time_to_FITS_date(date_start);
+	//cr_assert(!strcmp("", date_str));
+
+	moment = EXP_MIDDLE;
+	GDateTime *date_middle0 = get_timestamp_for_pixel(fit.keywords.gps_data, moment, 0, row);
+	cr_assert(fabs(timediff_in_s(date_start0, date_middle0) - fit.keywords.exposure * 0.5) < 1e-3);
+
+	moment = EXP_END;
+	GDateTime *date_end0 = get_timestamp_for_pixel(fit.keywords.gps_data, moment, 0, row);
+	cr_assert(fabs(timediff_in_s(date_start0, date_end0) - fit.keywords.exposure) < 1e-3);
+
+	row = 3193;	// this is on-screen row, so effectively the top of the image
+	moment = EXP_START;
+	GDateTime *date_start1 = get_timestamp_for_pixel(fit.keywords.gps_data, moment, 0, row);
+	//date_str = date_time_to_FITS_date(date_start);
+	//cr_assert(!strcmp("", date_str));
+
+	moment = EXP_MIDDLE;
+	GDateTime *date_middle1 = get_timestamp_for_pixel(fit.keywords.gps_data, moment, 0, row);
+	cr_assert(fabs(timediff_in_s(date_start1, date_middle1) - fit.keywords.exposure * 0.5) < 1e-3);
+
+	moment = EXP_END;
+	GDateTime *date_end1 = get_timestamp_for_pixel(fit.keywords.gps_data, moment, 0, row);
+	cr_assert(fabs(timediff_in_s(date_start1, date_end1) - fit.keywords.exposure) < 1e-3);
+
+	double rolling_shutter_duration = timediff_in_s(date_start1, date_start0);
+	siril_debug_print("rolling shutter duration: %f\n", rolling_shutter_duration);
+	cr_assert(rolling_shutter_duration > 0.2);
+	cr_assert(rolling_shutter_duration < 0.3);
+
+	clearfits(&fit);
+}
+
+void test_rsgps_binning() {
+	fits fit = {0};
+	gchar *file_path = check_or_download_test_file("gps_target_rate_tracking_rolling_shutter_1.fit");
+	cr_assert(file_path);
+	cr_assert(readfits(file_path, &fit, NULL, FALSE) == 0);
+	g_free(file_path);
+
+	cr_assert(fit.keywords.gps_data->binning == 2);
+	fits_binning(&fit, 2, TRUE);
+	cr_assert(fit.keywords.gps_data->binning == 4);
+	// TODO: compare timestamps before and after
+
+	clearfits(&fit);
+}
+
+void test_rsgps_flip() {
+	fits fit = {0};
+	gchar *file_path = check_or_download_test_file("gps_target_rate_tracking_rolling_shutter_1.fit");
+	cr_assert(file_path);
+	cr_assert(readfits(file_path, &fit, NULL, FALSE) == 0);
+	g_free(file_path);
+
+	cr_assert(fit.keywords.gps_data->top_down);
+	mirrorx(&fit, FALSE);
+	cr_assert(!fit.keywords.gps_data->top_down);
+	// TODO: compare timestamps before and after
+
+	clearfits(&fit);
+}
+
+void test_rsgps_crop() {
+	fits fit = {0};
+	gchar *file_path = check_or_download_test_file("gps_target_rate_tracking_rolling_shutter_1.fit");
+	cr_assert(file_path);
+	cr_assert(readfits(file_path, &fit, NULL, FALSE) == 0);
+	g_free(file_path);
+
+	cr_assert(fit.keywords.gps_data->crop_offset_y == 0);
+	rectangle area = { 300, 500, 200, 200 };
+	crop(&fit, &area);
+	siril_debug_print("new offset: %d\n", fit.keywords.gps_data->crop_offset_y);
+	cr_assert(fit.keywords.gps_data->crop_offset_y == 3194 - 500 - 200);
+	// TODO: compare timestamps before and after
+
+	clearfits(&fit);
+}
+
 Test(qhy_gps, load) { test_rsgps_data_loading(); }
+Test(qhy_gps, metadata) { test_metadata_reading(); }
+Test(qhy_gps, rs_timestamp) { test_rsgps_timestamp_computation(); }
+Test(qhy_gps, binning) { test_rsgps_binning(); }
+Test(qhy_gps, flip) { test_rsgps_flip(); }
+Test(qhy_gps, crop) { test_rsgps_crop(); }
 
