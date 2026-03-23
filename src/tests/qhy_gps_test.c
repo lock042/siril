d9cda8cb7c87fa788e8202557487fdedf9a19fac
Vincent Hourdin <vh@free-astro.org>
2026-03-23 21:42:33 +0000

    final test

--- src/tests/qhy_gps_test.c
+++ src/tests/qhy_gps_test.c
@@ -41,6 +41,18 @@
 #define TEST_FILES_DOWNLOAD_DIR "siril/tests" // in user cache dir
 #define LOCK_FILE_FOR_DOWNLOAD "/tmp/siril-test-download.lock"
 
+#define LOCK(retval) \
+	int fd_lock = open(LOCK_FILE_FOR_DOWNLOAD, O_RDWR | O_CREAT, 0640); \
+	if (fd_lock < 0) { \
+		siril_debug_print("could not create lock file\n"); \
+		return retval; \
+	} \
+	flock(fd_lock, LOCK_EX)
+
+#define UNLOCK \
+	flock(fd_lock, LOCK_UN); \
+	close(fd_lock);
+
 static size_t write_data(void *ptr, size_t size, size_t nmemb, FILE *stream) {
     size_t written = fwrite(ptr, size, nmemb, stream);
     return written;
@@ -52,12 +64,7 @@ static gboolean directory_created = FALSE;
  * we use a file lock instead, which may not be portable. The other solution would be to make a function
  * with all the test function and call this one instead in the single Test() */
 static void init_download() {
-	int fd_lock = open(LOCK_FILE_FOR_DOWNLOAD, O_RDWR | O_CREAT, 0640);
-	if (fd_lock < 0) {
-		siril_debug_print("could not create lock file\n");
-		return;
-	}
-	flock(fd_lock, LOCK_EX);
+	LOCK();
 	const gchar *cache_dir_path = g_get_user_cache_dir();
 	gchar *test_files_dir_path = g_strdup_printf("%s/%s", cache_dir_path, TEST_FILES_DOWNLOAD_DIR);
 	if (g_mkdir_with_parents(test_files_dir_path, 0700)) {
@@ -66,25 +73,21 @@ static void init_download() {
 		directory_created = TRUE;
 	}
 	g_free(test_files_dir_path);
-	flock(fd_lock, LOCK_UN);
-	close(fd_lock);
+	UNLOCK;
 }
 
 static gchar *check_or_download_test_file(const char *filename) {
-	int fd_lock = open(LOCK_FILE_FOR_DOWNLOAD, O_RDWR | O_CREAT, 0640);
-	flock(fd_lock, LOCK_EX);
+	LOCK(NULL);
 	const gchar *cache_dir_path = g_get_user_cache_dir();
 	gchar *file_path = g_strdup_printf("%s/%s/%s", cache_dir_path, TEST_FILES_DOWNLOAD_DIR, filename);
 	if (is_readable_file(file_path)) {
-		flock(fd_lock, LOCK_UN);
-		close(fd_lock);
+		UNLOCK;
 		return file_path;
 	}
 	if (!directory_created) {
 		siril_debug_print("cannot download to expected directory which creation failed\n");
 		g_free(file_path);
-		flock(fd_lock, LOCK_UN);
-		close(fd_lock);
+		UNLOCK;
 		return NULL;
 	}
 #ifdef HAVE_LIBCURL
@@ -93,8 +96,7 @@ static gchar *check_or_download_test_file(const char *filename) {
 	if (!dest_file) {
 		perror("fopen");
 		siril_debug_print("failed to create file %s for downloads\n", file_path);
-		flock(fd_lock, LOCK_UN);
-		close(fd_lock);
+		UNLOCK;
 		return NULL;
 	}
 	gchar *file_url = g_strdup_printf("%s/%s", TEST_FILES_BUCKET_URL, filename);
@@ -121,8 +123,7 @@ static gchar *check_or_download_test_file(const char *filename) {
 #else
 	siril_debug_print("test file %s is missing and cannot be doawnloaded (libcurl missing)\n", file_path);
 #endif
-	flock(fd_lock, LOCK_UN);
-	close(fd_lock);
+	UNLOCK;
 	return file_path;
 }
 
@@ -284,6 +285,25 @@ void test_rsgps_crop() {
 	clearfits(&fit);
 }
 
+void test_non_gps_images() {
+	fits fit = {0};
+	gchar *file_path = check_or_download_test_file("B_stacked.fit");
+	cr_assert(file_path);
+	cr_assert(readfits(file_path, &fit, NULL, FALSE) == 0);
+	cr_assert(!fit.keywords.gps_data);
+
+        struct _qhy_struct qhy_header = { 0 };
+        int retval = parse_gps_image(&fit, &qhy_header);
+	cr_assert(retval);
+
+	retval = parse_gps_from_header(&fit, file_path, &qhy_header);
+	cr_assert(retval);
+
+	g_free(file_path);
+	clearfits(&fit);
+}
+
+
 TestSuite(qhy_gps, .init = init_download);
 Test(qhy_gps, load) { test_rsgps_data_loading(); }
 Test(qhy_gps, metadata) { test_metadata_reading(); }
@@ -291,4 +311,5 @@ Test(qhy_gps, rs_timestamp) { test_rsgps_timestamp_computation(); }
 Test(qhy_gps, binning) { test_rsgps_binning(); }
 Test(qhy_gps, flip) { test_rsgps_flip(); }
 Test(qhy_gps, crop) { test_rsgps_crop(); }
+Test(qhy_gps, normal_image) { test_non_gps_images(); }
 
